7f0bff12c0b04d219349e1006018232fef4567e2
Douglas Lane <laned@ecs.csus.edu>
2007-03-22 23:23:42 +0000

    bug 11: added pc2ver scripts
    svn path=/trunk/; revision=9

---
+++ pc2ver.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Purpose: start pc2 Server and Client version 9
+rem Author : pc2@ecs.csus.edu
+rem Mon Sep 11 13:29:58 PDT 2006
+
+java -Djava.security.policy=policy.ini edu.csus.ecs.pc2.VersionInfo
+
+rem eof startpc2.bat 
