ff1edf6fbd2d5d6d991efa9df974f20a728ddf4c
Douglas Lane <laned@ecs.csus.edu>
2007-03-22 23:20:07 +0000

    bug 11: Add Log class into logging package
    svn path=/trunk/; revision=7

---
+++ src/edu/csus/ecs/pc2/logging/Log.java
@@ -0,0 +1,15 @@
+package src.edu.csus.ecs.pc2.logging;
+
+/**
+ * 
+ * @author pc2@ecs.csus.edu.
+ */
+// $HeadURL: http://pc2.ecs.csus.edu/repos/v9wip/trunk/src/edu/csus/ecs/pc2/VersionInfo.java$
+public class Log {
+
+    public void info(String string) {
+        System.out.println(string);
+
+    }
+
+}
