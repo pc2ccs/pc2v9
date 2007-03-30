6d97ddd8fb89746192d09a96214e56a50157c16b
Douglas Lane <laned@ecs.csus.edu>
2007-03-30 00:29:10 +0000

    bug 11: added three JUnit tests put them under test.test.edu... to avoid javadoc clash with actual core classes
    svn path=/trunk/; revision=63

---
+++ test/test/edu/csus/ecs/pc2/core/ElementIdTest.java
@@ -0,0 +1,48 @@
+package test.edu.csus.ecs.pc2.core;
+
+import junit.framework.TestCase;
+import edu.csus.ecs.pc2.core.model.ElementId;
+
+/**
+ * 
+ * @author pc2@ecs.csus.edu
+ * 
+ */
+// $HeadURL$
+public class ElementIdTest extends TestCase {
+
+    /**
+     * svn id.
+     */
+    public static final String SVN_ID = "$Id$";
+
+    /*
+     * Test method for 'edu.csus.ecs.pc2.core.ElementId.hashCode()'
+     */
+    public void testHashCode() {
+        ElementId element1 = new ElementId("test Case");
+        ElementId element2 = new ElementId("test Case");
+        assertFalse("hashCode match", element1.hashCode() == element2
+                .hashCode());
+    }
+
+    /*
+     * Test method for 'edu.csus.ecs.pc2.core.ElementId.toString()'
+     */
+    public void testToString() {
+        ElementId element1 = new ElementId("test Case");
+        ElementId element2 = new ElementId("test Case");
+        assertFalse("toStrings match", element1.toString().equals(
+                element2.toString()));
+    }
+
+    /*
+     * Test method for 'edu.csus.ecs.pc2.core.ElementId.equals(Object)'
+     */
+    public void testEqualsObject() {
+        ElementId element1 = new ElementId("test Case");
+        ElementId element2 = new ElementId("test Case");
+        assertFalse("equals match", element1.equals(element2));
+    }
+
+}
