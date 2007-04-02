97c0edd69572edce28be70d00b7cd41104d9f4ee
Douglas Lane <laned@ecs.csus.edu>
2007-04-02 01:18:48 +0000

    bug 11: added GUIPlugin interface
    svn path=/trunk/; revision=100

---
+++ src/edu/csus/ecs/pc2/ui/GUIPlugin.java
@@ -0,0 +1,30 @@
+package edu.csus.ecs.pc2.ui;
+
+import edu.csus.ecs.pc2.core.IController;
+import edu.csus.ecs.pc2.core.model.IModel;
+
+/**
+ * Interface for plugin UI or GUI.
+ * 
+ * @author pc2@ecs.csus.edu
+ */
+
+// $HeadURL$
+public interface GUIPlugin {
+
+    /**
+     * set the Model and Controller.
+     * 
+     * @param model
+     *            contest data
+     * @param controller
+     *            contest controller
+     */
+    void setModelController(IModel model, IController controller);
+
+    /**
+     * @return name of this plugin, used in choosing plugin.
+     */
+    String pluginTitle();
+
+}
