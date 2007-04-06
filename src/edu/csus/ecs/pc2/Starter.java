fbe4ed51d775a2707c39c9eeaeb66d9b760214d4
Douglas Lane <laned@ecs.csus.edu>
2007-04-06 00:44:58 +0000

    bug 11: updated class java doc, various fixed per checkstyle.
    svn path=/trunk/; revision=147

--- src/edu/csus/ecs/pc2/Starter.java
+++ src/edu/csus/ecs/pc2/Starter.java
@@ -10,29 +10,25 @@ import edu.csus.ecs.pc2.core.model.Model;
  * 
  * The Starter class is the main driver for all PC<sup>2</sup> modules.
  * <P>
- * The class presents the user with a {@link edu.csus.ecs.pc2.ui.LoginFrame}, upon selecting login the LoginFrame class invokes the
- * {@link #run()} method and uses {@link edu.csus.ecs.pc2.core.Controller#login}, that method returns a
- * {@link edu.csus.ecs.pc2.core.model.IModel}, a model on succesful login.
- * <P>
- * Upon a Exception or invalid login a message will appear on the LoginFrame message area.
- * <P>
- * Upon a successful login, based on {@link edu.csus.ecs.pc2.core.model.ClientType.Type} a class name will be looked up in a {@link java.util.Properties} file.
- * The class will be instanciated and passed a {@link edu.csus.ecs.pc2.core.model.IModel} and
- * {@link edu.csus.ecs.pc2.core.IController}.
- * <P>
- * At this point the LoginFrame is set not visible and the class that was loaded takes over focus.
+ * This class creates a contest data {@link edu.csus.ecs.pc2.core.model.IModel}, then
+ * a  controller {@link edu.csus.ecs.pc2.core.IController}.   Then it passes the
+ * command line arguments to {@link edu.csus.ecs.pc2.core.Controller#start(String[])} and
+ * that starts a Login Frame. 
  * 
  * @author pc2@ecs.csus.edu
  */
 
-// TODO write code for a command line login
 // $HeadURL$
-public class Starter  {
+public final class Starter  {
 
     public static final String SVN_ID = "$Id$";
+    
+    private Starter(){
+        // constructor per checkstyle suggestion.
+    }
 
     /**
-     * Show version info and start Login window.
+     * Start a contest module.
      * 
      * @param args
      */
