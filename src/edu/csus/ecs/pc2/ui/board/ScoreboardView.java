5c0ff496ce85ce5cb0eac47fcd85502aaac4074c
Douglas Lane <laned@ecs.csus.edu>
2007-04-18 01:24:48 +0000

    bug 11: Scoreboard View added prompt before exit added setVisible(true) unused warnings now gone (put in TODOs)
    svn path=/trunk/; revision=254

--- src/edu/csus/ecs/pc2/ui/board/ScoreboardView.java
+++ src/edu/csus/ecs/pc2/ui/board/ScoreboardView.java
@@ -1,52 +1,82 @@
 package edu.csus.ecs.pc2.ui.board;
 
-
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import edu.csus.ecs.pc2.core.IController;
 import edu.csus.ecs.pc2.core.model.IModel;
+import edu.csus.ecs.pc2.ui.FrameUtilities;
 import edu.csus.ecs.pc2.ui.UIPlugin;
+
 /**
  * This class is the default scoreboard view (frame).
+ * 
  * @author pc2@ecs.csus.edu
- *
+ * 
  */
-//$HeadURL: http://pc2.ecs.csus.edu/repos/pc2v9/trunk/src/edu/csus/ecs/pc2/ui/ClarificationsPane.java $
+// $HeadURL: http://pc2.ecs.csus.edu/repos/pc2v9/trunk/src/edu/csus/ecs/pc2/ui/ClarificationsPane.java $
 public class ScoreboardView extends JFrame implements UIPlugin {
 
     /**
      * 
      */
     private static final long serialVersionUID = -8071477348056424178L;
+
+    // TODO remove @SuppressWarnings for model
+    @SuppressWarnings("unused")
     private IModel model;
+
+    // TODO remove @SuppressWarnings for controller
+    @SuppressWarnings("unused")
     private IController controller;
 
     /**
-	 * This method initializes 
-	 * 
-	 */
-	public ScoreboardView() {
-		super();
-		initialize();
-	}
+     * This method initializes
+     * 
+     */
+    public ScoreboardView() {
+        super();
+        initialize();
+    }
 
-	/**
-	 * This method initializes this
-	 * 
-	 */
-	private void initialize() {
-        this.setSize(new java.awt.Dimension(500,209));
+    /**
+     * This method initializes this
+     * 
+     */
+    private void initialize() {
+        this.setSize(new java.awt.Dimension(405, 227));
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         this.setTitle("Scoreboard");
-			
-	}
 
-	public void setModelAndController(IModel model, IController controller) {
-        this.model = model;
-        this.controller = controller;
+        this.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent e) {
+                promptAndExit();
+            }
+        });
+
+        FrameUtilities.centerFrame(this);
+    }
+
+    protected void promptAndExit() {
+        int result = FrameUtilities.yesNoCancelDialog("Are you sure you want to exit PC^2?", "Exit PC^2");
+
+        if (result == JOptionPane.YES_OPTION) {
+            System.err.println("Server halting");
+            System.exit(0);
+        }
+    }
+
+    public void setModelAndController(IModel inModel, IController inController) {
+        this.model = inModel;
+        this.controller = inController;
+
+        setTitle("PC^2 Server (Site " + model.getSiteNumber() + ")");
+
+        setVisible(true);
     }
 
     public String getPluginTitle() {
-        return "ScoreboardView";
+        return "Scoreboard View";
     }
 
-}  //  @jve:decl-index=0:visual-constraint="10,10"
+} // @jve:decl-index=0:visual-constraint="10,10"
