a814cb4e9f3560ffa2a1f5456a72bd3462bcd392
Douglas Lane <laned@ecs.csus.edu>
2007-04-21 08:08:27 +0000

    bug 11: Add AccountComparator (site, type, client number).
    svn path=/trunk/; revision=308

---
+++ src/edu/csus/ecs/pc2/core/list/AccountComparator.java
@@ -0,0 +1,46 @@
+package edu.csus.ecs.pc2.core.list;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import edu.csus.ecs.pc2.core.model.Account;
+import edu.csus.ecs.pc2.core.model.ClientId;
+
+/**
+ * Account Comparator, Order the accounts by site #, client Type, then client  #.
+ * 
+ * @author pc2@ecs.csus.edu
+ */
+
+// $HeadURL$
+public class AccountComparator implements Comparator<Account>, Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6254851774817221276L;
+    public static final String SVN_ID = "$Id$";
+
+    public int compare(Account account1, Account account2) {
+
+        // sort by site id, clientType, clientNumber
+
+        int site1 = account1.getSiteNumber();
+        int site2 = account2.getSiteNumber();
+
+        if (site1 == site2) {
+            
+            ClientId clientId1 = account1.getClientId();
+            ClientId clientId2 = account2.getClientId();
+            
+            if (clientId1.getClientType().equals(clientId2.getClientType())){
+                return clientId1.getClientNumber() - clientId2.getClientNumber();
+            } else {
+                return clientId1.getClientType().compareTo(clientId1.getClientType());
+            }
+            
+        } else {
+            return site1 - site2;
+        }
+    }
+}
