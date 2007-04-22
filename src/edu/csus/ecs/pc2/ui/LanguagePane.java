115ea7678f13d747a90f5ff8613c828379407612
Douglas Lane <laned@ecs.csus.edu>
2007-04-22 21:18:47 +0000

    bug 11: created LanguagePane to add/edit a single language def autopopulate done.
    svn path=/trunk/; revision=334

---
+++ src/edu/csus/ecs/pc2/ui/LanguagePane.java
@@ -0,0 +1,388 @@
+package edu.csus.ecs.pc2.ui;
+
+import edu.csus.ecs.pc2.core.IController;
+import edu.csus.ecs.pc2.core.model.IModel;
+import edu.csus.ecs.pc2.core.model.LanguageAutoFill;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+
+/**
+ * Add/Edit Language Pane
+ * 
+ * @author pc2@ecs.csus.edu
+ * 
+ */
+
+// $HeadURL$
+public class LanguagePane extends JPanePlugin {
+
+    private JPanel messagePane = null;
+
+    private JPanel buttonPane = null;
+
+    private JButton addButton = null;
+
+    private JButton updateButton = null;
+
+    private JButton cancelButton = null;
+
+    private JLabel messageLabel = null;
+
+    private JPanel jPanel = null;
+
+    private JLabel jLabel = null;
+
+    private JLabel jLabel1 = null;
+
+    private JLabel jLabel2 = null;
+
+    private JTextField displayNameTextField = null;
+
+    private JTextField compileCommandLineTextField = null;
+
+    private JTextField executableFilenameTextField = null;
+
+    private JLabel jLabel3 = null;
+
+    private JTextField jTextField3 = null;
+
+    private JLabel jLabel4 = null;
+
+    private JTextField programExecutionCommandLineTextField = null;
+
+    private JLabel jLabel5 = null;
+
+    private JComboBox autoPopulateLanguageComboBox = null;
+    
+    private static final String NO_CHANGE_TITLE = "No Change";
+
+    /**
+     * This method initializes
+     * 
+     */
+    public LanguagePane() {
+        super();
+        initialize();
+    }
+
+    /**
+     * This method initializes this
+     * 
+     */
+    private void initialize() {
+        this.setLayout(new BorderLayout());
+        this.setSize(new java.awt.Dimension(517, 239));
+
+        this.add(getJPanel(), java.awt.BorderLayout.CENTER);
+        this.add(getMessagePane(), java.awt.BorderLayout.NORTH);
+        this.add(getButtonPane(), java.awt.BorderLayout.SOUTH);
+        
+        loadComboBox();
+    }
+
+    private void loadComboBox() {
+  
+        getAutoPopulateLanguageComboBox().addItem(NO_CHANGE_TITLE);
+        for (String languageName : LanguageAutoFill.getLanguageList()){
+            getAutoPopulateLanguageComboBox().addItem(languageName);
+        }
+    }
+
+    public void setModelAndController(IModel inModel, IController inController) {
+        super.setModelAndController(inModel, inController);
+    }
+
+    public String getPluginTitle() {
+        return "Edit Language Pane";
+    }
+
+    /**
+     * This method initializes messagePane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getMessagePane() {
+        if (messagePane == null) {
+            messageLabel = new JLabel();
+            messageLabel.setText("");
+            messageLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+            messagePane = new JPanel();
+            messagePane.setLayout(new BorderLayout());
+            messagePane.setPreferredSize(new java.awt.Dimension(25, 25));
+            messagePane.add(messageLabel, java.awt.BorderLayout.CENTER);
+        }
+        return messagePane;
+    }
+
+    /**
+     * This method initializes buttonPane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getButtonPane() {
+        if (buttonPane == null) {
+            FlowLayout flowLayout = new FlowLayout();
+            flowLayout.setHgap(15);
+            buttonPane = new JPanel();
+            buttonPane.setLayout(flowLayout);
+            buttonPane.add(getAddButton(), null);
+            buttonPane.add(getUpdateButton(), null);
+            buttonPane.add(getCancelButton(), null);
+        }
+        return buttonPane;
+    }
+
+    /**
+     * This method initializes addButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getAddButton() {
+        if (addButton == null) {
+            addButton = new JButton();
+            addButton.setText("Add");
+            addButton.setEnabled(false);
+            addButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return addButton;
+    }
+
+    /**
+     * This method initializes updateButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getUpdateButton() {
+        if (updateButton == null) {
+            updateButton = new JButton();
+            updateButton.setText("Update");
+            updateButton.setEnabled(false);
+            updateButton.setMnemonic(java.awt.event.KeyEvent.VK_U);
+            updateButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return updateButton;
+    }
+
+    /**
+     * This method initializes cancelButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getCancelButton() {
+        if (cancelButton == null) {
+            cancelButton = new JButton();
+            cancelButton.setText("Cancel");
+            cancelButton.setMnemonic(java.awt.event.KeyEvent.VK_C);
+            cancelButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return cancelButton;
+    }
+
+    /**
+     * This method initializes jPanel
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getJPanel() {
+        if (jPanel == null) {
+            jLabel5 = new JLabel();
+            jLabel5.setBounds(new java.awt.Rectangle(67, 13, 129, 20));
+            jLabel5.setHorizontalAlignment(SwingConstants.RIGHT);
+            jLabel5.setText("Auto Populate with");
+            jLabel5.setName("AutoPopulateLabel");
+            jLabel4 = new JLabel();
+            jLabel4.setBounds(new java.awt.Rectangle(13, 145, 244, 20));
+            jLabel4.setName("ProgramExLabel");
+            jLabel4.setMaximumSize(new Dimension(2147483647, 2147483647));
+            jLabel4.setHorizontalAlignment(SwingConstants.RIGHT);
+            jLabel4.setText("Program Execution Command Line");
+            jLabel4.setForeground(new Color(0, 0, 0));
+            jLabel3 = new JLabel();
+            jLabel3.setBounds(new java.awt.Rectangle(14, 112, 182, 20));
+            jLabel3.setName("ExeFilenameLabel");
+            jLabel3.setMaximumSize(new Dimension(2147483647, 2147483647));
+            jLabel3.setHorizontalAlignment(SwingConstants.RIGHT);
+            jLabel3.setText("Executable Filename");
+            jLabel3.setForeground(Color.black);
+            jLabel2 = new JLabel();
+            jLabel2.setBounds(new java.awt.Rectangle(0, 0, 0, 0));
+            jLabel2.setName("SourceExtLabel");
+            jLabel2.setForeground(Color.black);
+            jLabel2.setHorizontalAlignment(SwingConstants.RIGHT);
+            jLabel2.setText("Source Extensions");
+            jLabel2.setVisible(false);
+            jLabel1 = new JLabel();
+            jLabel1.setBounds(new java.awt.Rectangle(14, 46, 182, 20));
+            jLabel1.setName("DisplayNameLabel");
+            jLabel1.setMaximumSize(new Dimension(2147483647, 2147483647));
+            jLabel1.setHorizontalAlignment(SwingConstants.RIGHT);
+            jLabel1.setText("Display Name");
+            jLabel1.setForeground(Color.black);
+            jLabel = new JLabel();
+            jLabel.setBounds(new java.awt.Rectangle(14, 79, 182, 20));
+            jLabel.setName("CompileCmdLineLabel");
+            jLabel.setMaximumSize(new Dimension(2147483647, 2147483647));
+            jLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+            jLabel.setText("Compile Cmd Line");
+            jLabel.setForeground(Color.black);
+            jPanel = new JPanel();
+            jPanel.setLayout(null);
+            jPanel.setName("advancedEdit");
+            jPanel.add(jLabel, jLabel.getName());
+            jPanel.add(jLabel1, jLabel1.getName());
+            jPanel.add(jLabel2, jLabel2.getName());
+            jPanel.add(getDisplayNameTextField(), getDisplayNameTextField().getName());
+            jPanel.add(getCompileCommandLineTextField(), getCompileCommandLineTextField().getName());
+            jPanel.add(getExecutableFilenameTextField(), getExecutableFilenameTextField().getName());
+            jPanel.add(jLabel3, jLabel3.getName());
+            jPanel.add(getJTextField3(), getJTextField3().getName());
+            jPanel.add(jLabel4, jLabel4.getName());
+            jPanel.add(getProgramExecutionCommandLineTextField(), getProgramExecutionCommandLineTextField().getName());
+            jPanel.add(jLabel5, jLabel5.getName());
+            jPanel.add(getAutoPopulateLanguageComboBox(), getAutoPopulateLanguageComboBox().getName());
+        }
+        return jPanel;
+    }
+
+    /**
+     * This method initializes jTextField
+     * 
+     * @return javax.swing.JTextField
+     */
+    private JTextField getDisplayNameTextField() {
+        if (displayNameTextField == null) {
+            displayNameTextField = new JTextField();
+            displayNameTextField.setBounds(new java.awt.Rectangle(209, 46, 263, 20));
+            displayNameTextField.setToolTipText("Name to display to users");
+            displayNameTextField.setName("displayNameTextField");
+        }
+        return displayNameTextField;
+    }
+
+    /**
+     * This method initializes jTextField1
+     * 
+     * @return javax.swing.JTextField
+     */
+    private JTextField getCompileCommandLineTextField() {
+        if (compileCommandLineTextField == null) {
+            compileCommandLineTextField = new JTextField();
+            compileCommandLineTextField.setBounds(new java.awt.Rectangle(208, 79, 264, 20));
+            compileCommandLineTextField.setToolTipText("Command Line for compiler");
+            compileCommandLineTextField.setName("commandLineTextField");
+        }
+        return compileCommandLineTextField;
+    }
+
+    /**
+     * This method initializes jTextField2
+     * 
+     * @return javax.swing.JTextField
+     */
+    private JTextField getExecutableFilenameTextField() {
+        if (executableFilenameTextField == null) {
+            executableFilenameTextField = new JTextField();
+            executableFilenameTextField.setBounds(new java.awt.Rectangle(208, 112, 264, 20));
+            executableFilenameTextField.setToolTipText("Form: exe");
+            executableFilenameTextField.setName("programExeTextField");
+        }
+        return executableFilenameTextField;
+    }
+
+    /**
+     * This method initializes jTextField3
+     * 
+     * @return javax.swing.JTextField
+     */
+    private JTextField getJTextField3() {
+        if (jTextField3 == null) {
+            jTextField3 = new JTextField();
+            jTextField3.setBounds(new java.awt.Rectangle(0, 0, 0, 0));
+            jTextField3.setName("sourceExtTextField");
+            jTextField3.setToolTipText("Form: *.cpp;*.c");
+            jTextField3.setVisible(false);
+        }
+        return jTextField3;
+    }
+
+    /**
+     * This method initializes jTextField4
+     * 
+     * @return javax.swing.JTextField
+     */
+    private JTextField getProgramExecutionCommandLineTextField() {
+        if (programExecutionCommandLineTextField == null) {
+            programExecutionCommandLineTextField = new JTextField();
+            programExecutionCommandLineTextField.setBounds(new java.awt.Rectangle(274, 145, 198, 20));
+            programExecutionCommandLineTextField.setToolTipText("Form: exe");
+            programExecutionCommandLineTextField.setName("programCommandLine");
+        }
+        return programExecutionCommandLineTextField;
+    }
+
+    /**
+     * This method initializes jComboBox
+     * 
+     * @return javax.swing.JComboBox
+     */
+    private JComboBox getAutoPopulateLanguageComboBox() {
+        if (autoPopulateLanguageComboBox == null) {
+            autoPopulateLanguageComboBox = new JComboBox();
+            autoPopulateLanguageComboBox.setBounds(new java.awt.Rectangle(209, 12, 259, 23));
+            autoPopulateLanguageComboBox.setName("LangPopulateComboBox");
+            autoPopulateLanguageComboBox.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    autoFillFields();
+                }
+            });
+        }
+        return autoPopulateLanguageComboBox;
+    }
+
+    protected void autoFillFields() {
+        
+        String languageToFill = (String) getAutoPopulateLanguageComboBox().getSelectedItem();
+        
+        if (NO_CHANGE_TITLE.equals(languageToFill)){
+            return;
+        }
+        
+        /**
+         * From LanguageAutoFill
+         * <li>Title for Language
+         * <li>Compiler Command Line
+         * <li>Executable Identifier Mask
+         * <li>Execute command line
+         */
+        
+        String [] values = LanguageAutoFill.getAutoFillValues(languageToFill);
+        
+        displayNameTextField.setText(values[0]);
+        compileCommandLineTextField.setText(values[1]);
+        executableFilenameTextField.setText(values[2]);
+        programExecutionCommandLineTextField.setText(values[3]);
+        
+    }
+
+} // @jve:decl-index=0:visual-constraint="10,10"
