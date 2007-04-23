92e3eda9b5a1588eaa8ec02cfdc3969fb81754c9
Douglas Lane <laned@ecs.csus.edu>
2007-04-23 03:54:15 +0000

    bug 11: add edit poroblem pane
    svn path=/trunk/; revision=337

---
+++ src/edu/csus/ecs/pc2/ui/ProblemPane.java
@@ -0,0 +1,604 @@
+package edu.csus.ecs.pc2.ui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import edu.csus.ecs.pc2.core.IController;
+import edu.csus.ecs.pc2.core.log.Log;
+import edu.csus.ecs.pc2.core.model.IModel;
+import edu.csus.ecs.pc2.core.model.Problem;
+
+/**
+ * Add/Edit Problem Pane
+ * 
+ * @author pc2@ecs.csus.edu
+ * 
+ */
+
+// $HeadURL$
+public class ProblemPane extends JPanePlugin {
+
+    private JPanel messagePane = null;
+
+    private JPanel buttonPane = null;
+
+    private JButton addButton = null;
+
+    private JButton updateButton = null;
+
+    private JButton cancelButton = null;
+
+    private JLabel messageLabel = null;
+
+    private Problem problem = null;
+
+    private JTabbedPane mainTabbedPane = null;
+
+    private JPanel generalPane = null;
+
+    private JTextField problemNameTextField = null;
+
+    private JTextField timeOutSecondTextField = null;
+
+    private JCheckBox problemRequiresDataCheckBox = null;
+
+    private JPanel dataProblemPane = null;
+
+    private JPanel readsFromPane = null;
+
+    private JPanel inputDataFilePane = null;
+
+    private JRadioButton stdinRadioButton = null;
+
+    private JRadioButton fileRadioButton = null;
+
+    private JPanel fileNamePane = null;
+
+    private JButton selectFileButton = null;
+
+    private JCheckBox judgesHaveDataFiles = null;
+
+    private JPanel answerFilePane = null;
+
+    private JPanel answerFilenamePane = null;
+
+    private JButton answerBrowseButton = null;
+
+    private JLabel inputDataFileLabel = null;
+
+    private JLabel answerFileNameLabel = null;
+
+    private JLabel problemNameLabel = null;
+
+    private JLabel timeoutLabel = null;
+    
+    private Log log = null;
+
+    /**
+     * This method initializes
+     * 
+     */
+    public ProblemPane() {
+        super();
+        initialize();
+    }
+
+    /**
+     * This method initializes this
+     * 
+     */
+    private void initialize() {
+        this.setLayout(new BorderLayout());
+        this.setSize(new java.awt.Dimension(536, 413));
+
+        this.add(getMessagePane(), java.awt.BorderLayout.NORTH);
+        this.add(getButtonPane(), java.awt.BorderLayout.SOUTH);
+        this.add(getMainTabbedPane(), java.awt.BorderLayout.EAST);
+        this.add(getGeneralPane(), java.awt.BorderLayout.CENTER);
+    }
+
+    public void setModelAndController(IModel inModel, IController inController) {
+        super.setModelAndController(inModel, inController);
+        
+        log = getController().getLog();
+    }
+
+    public String getPluginTitle() {
+        return "Edit Problem Pane";
+    }
+
+    /**
+     * This method initializes messagePane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getMessagePane() {
+        if (messagePane == null) {
+            messageLabel = new JLabel();
+            messageLabel.setText("");
+            messageLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+            messagePane = new JPanel();
+            messagePane.setLayout(new BorderLayout());
+            messagePane.setPreferredSize(new java.awt.Dimension(25, 25));
+            messagePane.add(messageLabel, java.awt.BorderLayout.CENTER);
+        }
+        return messagePane;
+    }
+
+    /**
+     * This method initializes buttonPane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getButtonPane() {
+        if (buttonPane == null) {
+            FlowLayout flowLayout = new FlowLayout();
+            flowLayout.setHgap(15);
+            buttonPane = new JPanel();
+            buttonPane.setLayout(flowLayout);
+            buttonPane.add(getAddButton(), null);
+            buttonPane.add(getUpdateButton(), null);
+            buttonPane.add(getCancelButton(), null);
+        }
+        return buttonPane;
+    }
+
+    /**
+     * This method initializes addButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getAddButton() {
+        if (addButton == null) {
+            addButton = new JButton();
+            addButton.setText("Add");
+            addButton.setEnabled(false);
+            addButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    addProblem();
+                }
+            });
+        }
+        return addButton;
+    }
+
+    protected void addProblem() {
+
+        Problem newProblem = getProblemFromFields();
+
+        // TODO update problem
+        // getController().addNewProblem(newProblem);
+
+        cancelButton.setText("Close");
+        addButton.setEnabled(false);
+        updateButton.setEnabled(false);
+    }
+
+    private Problem getProblemFromFields() {
+        if (problem == null) {
+            problem = new Problem(problemNameTextField.getText());
+        } else {
+            problem.setDisplayName(problemNameTextField.getText());
+        }
+
+        try {
+            int secs = new Integer(timeOutSecondTextField.getText()).intValue();
+            problem.setTimeOutInSeconds(secs);
+        } catch (Exception e) {
+            // TODO: log handle exception
+            log.log(Log.WARNING, "Exception logged ", e);
+            showMessage("Invalid time out");
+        }
+        
+        if (problemRequiresDataCheckBox.isSelected()){
+            String s = inputDataFileLabel.getText();
+        } 
+//        else {
+//            // TODO clear input data file
+//        }
+        
+        if (judgesHaveDataFiles.isSelected()){
+            String s = answerFileNameLabel.getText();
+        } 
+//        else {
+//            // TODO clear judges data file 
+//        }
+
+        // TODO load problem data files with input file and answer file
+        
+        return problem;
+    }
+
+    /**
+     * This method initializes updateButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getUpdateButton() {
+        if (updateButton == null) {
+            updateButton = new JButton();
+            updateButton.setText("Update");
+            updateButton.setEnabled(false);
+            updateButton.setMnemonic(java.awt.event.KeyEvent.VK_U);
+            updateButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    updateProblem();
+                }
+            });
+        }
+        return updateButton;
+    }
+
+    protected void updateProblem() {
+
+        Problem newProblem = getProblemFromFields();
+
+        // TODO update problem
+        // getController().updateProblem(newProblem);
+
+        cancelButton.setText("Close");
+        addButton.setEnabled(false);
+        updateButton.setEnabled(false);
+    }
+
+    /**
+     * This method initializes cancelButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getCancelButton() {
+        if (cancelButton == null) {
+            cancelButton = new JButton();
+            cancelButton.setText("Cancel");
+            cancelButton.setMnemonic(java.awt.event.KeyEvent.VK_C);
+            cancelButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    handleCancelButton();
+                }
+            });
+        }
+        return cancelButton;
+    }
+
+    protected void handleCancelButton() {
+
+        if (getAddButton().isEnabled() || getUpdateButton().isEnabled()) {
+
+            // Something changed, are they sure ?
+
+            int result = FrameUtilities.yesNoCancelDialog("Problem modified, save changes?", "Confirm Choice");
+
+            if (result == JOptionPane.YES_OPTION) {
+                if (getAddButton().isEnabled()) {
+                    addProblem();
+                } else {
+                    updateProblem();
+                }
+            }
+        }
+    }
+
+    public Problem getProblem() {
+        return problem;
+    }
+
+    public void setProblem(final Problem problem) {
+
+        this.problem = problem;
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                populateGUI(problem);
+                enableUpdateButtons(false);
+            }
+        });
+    }
+
+    private void populateGUI(Problem problem2) {
+
+        if (problem2 != null) {
+
+            getAddButton().setVisible(false);
+            getUpdateButton().setVisible(true);
+
+        } else {
+
+            getAddButton().setVisible(true);
+            getUpdateButton().setVisible(false);
+        }
+    }
+
+    protected void enableUpdateButtons(boolean editedText) {
+        if (editedText) {
+            cancelButton.setText("Cancel");
+        } else {
+            cancelButton.setText("Close");
+        }
+        addButton.setEnabled(editedText);
+        updateButton.setEnabled(editedText);
+    }
+
+    /**
+     * This method initializes mainTabbedPane
+     * 
+     * @return javax.swing.JTabbedPane
+     */
+    private JTabbedPane getMainTabbedPane() {
+        if (mainTabbedPane == null) {
+            mainTabbedPane = new JTabbedPane();
+        }
+        return mainTabbedPane;
+    }
+
+    /**
+     * This method initializes generalPane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getGeneralPane() {
+        if (generalPane == null) {
+            timeoutLabel = new JLabel();
+            timeoutLabel.setBounds(new java.awt.Rectangle(23, 46, 175, 16));
+            timeoutLabel.setText("Run Timeout Limit (Secs)");
+            problemNameLabel = new JLabel();
+            problemNameLabel.setBounds(new java.awt.Rectangle(23, 14, 179, 16));
+            problemNameLabel.setText("Problem name");
+            generalPane = new JPanel();
+            generalPane.setLayout(null);
+            generalPane.add(getProblemNameTextField(), null);
+            generalPane.add(getJTextField(), null);
+            generalPane.add(getProblemRequiresDataTextField(), null);
+            generalPane.add(getDataProblemPane(), null);
+            generalPane.add(getJudgesHaveDataFiles(), null);
+            generalPane.add(getAnswerFilePane(), null);
+            generalPane.add(problemNameLabel, null);
+            generalPane.add(timeoutLabel, null);
+        }
+        return generalPane;
+    }
+
+    /**
+     * This method initializes problemNameTextField
+     * 
+     * @return javax.swing.JTextField
+     */
+    private JTextField getProblemNameTextField() {
+        if (problemNameTextField == null) {
+            problemNameTextField = new JTextField();
+            problemNameTextField.setPreferredSize(new java.awt.Dimension(120, 20));
+            problemNameTextField.setSize(new java.awt.Dimension(273, 20));
+            problemNameTextField.setLocation(new java.awt.Point(220, 12));
+        }
+        return problemNameTextField;
+    }
+
+    /**
+     * This method initializes jTextField
+     * 
+     * @return javax.swing.JTextField
+     */
+    private JTextField getJTextField() {
+        if (timeOutSecondTextField == null) {
+            timeOutSecondTextField = new JTextField();
+            timeOutSecondTextField.setBounds(new java.awt.Rectangle(220, 44, 120, 20));
+            timeOutSecondTextField.setPreferredSize(new java.awt.Dimension(120, 20));
+            timeOutSecondTextField.setDocument(new IntegerDocument());
+        }
+        return timeOutSecondTextField;
+    }
+
+    /**
+     * This method initializes problemRequiresDataTextField
+     * 
+     * @return javax.swing.JCheckBox
+     */
+    private JCheckBox getProblemRequiresDataTextField() {
+        if (problemRequiresDataCheckBox == null) {
+            problemRequiresDataCheckBox = new JCheckBox();
+            problemRequiresDataCheckBox.setBounds(new java.awt.Rectangle(23, 76, 257, 26));
+            problemRequiresDataCheckBox.setText("Problem Requires Input Data");
+        }
+        return problemRequiresDataCheckBox;
+    }
+
+    /**
+     * This method initializes DataProblemPane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getDataProblemPane() {
+        if (dataProblemPane == null) {
+            GridLayout gridLayout = new GridLayout();
+            gridLayout.setRows(2);
+            gridLayout.setHgap(5);
+            gridLayout.setVgap(6);
+            dataProblemPane = new JPanel();
+            dataProblemPane.setLayout(gridLayout);
+            dataProblemPane.setBounds(new java.awt.Rectangle(53, 114, 423, 113));
+            dataProblemPane.add(getReadsFromPane(), null);
+            dataProblemPane.add(getInputDataFilePane(), null);
+        }
+        return dataProblemPane;
+    }
+
+    /**
+     * This method initializes readsFromPane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getReadsFromPane() {
+        if (readsFromPane == null) {
+            FlowLayout flowLayout1 = new FlowLayout();
+            flowLayout1.setHgap(35);
+            flowLayout1.setAlignment(java.awt.FlowLayout.LEFT);
+            flowLayout1.setVgap(0);
+            readsFromPane = new JPanel();
+            readsFromPane.setLayout(flowLayout1);
+            readsFromPane.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Team Reads From", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+                    javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12), new java.awt.Color(51, 51, 51)));
+            readsFromPane.add(getFileRadioButton(), null);
+            readsFromPane.add(getStdinRadioButton(), null);
+        }
+        return readsFromPane;
+    }
+
+    /**
+     * This method initializes inputDataFilePane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getInputDataFilePane() {
+        if (inputDataFilePane == null) {
+            BorderLayout borderLayout = new BorderLayout();
+            borderLayout.setHgap(15);
+            borderLayout.setVgap(5);
+            inputDataFilePane = new JPanel();
+            inputDataFilePane.setLayout(borderLayout);
+            inputDataFilePane.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Input Data File", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+                    javax.swing.border.TitledBorder.DEFAULT_POSITION, null, null));
+            inputDataFilePane.setPreferredSize(new java.awt.Dimension(98, 45));
+            inputDataFilePane.add(getFileNamePane(), java.awt.BorderLayout.CENTER);
+            inputDataFilePane.add(getSelectFileButton(), java.awt.BorderLayout.EAST);
+        }
+        return inputDataFilePane;
+    }
+
+    /**
+     * This method initializes stdinRadioButton
+     * 
+     * @return javax.swing.JRadioButton
+     */
+    private JRadioButton getStdinRadioButton() {
+        if (stdinRadioButton == null) {
+            stdinRadioButton = new JRadioButton();
+            stdinRadioButton.setText("Stdin");
+        }
+        return stdinRadioButton;
+    }
+
+    /**
+     * This method initializes fileRadioButton
+     * 
+     * @return javax.swing.JRadioButton
+     */
+    private JRadioButton getFileRadioButton() {
+        if (fileRadioButton == null) {
+            fileRadioButton = new JRadioButton();
+            fileRadioButton.setText("File");
+        }
+        return fileRadioButton;
+    }
+
+    /**
+     * This method initializes fileNamePane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getFileNamePane() {
+        if (fileNamePane == null) {
+            inputDataFileLabel = new JLabel();
+            inputDataFileLabel.setText("");
+            fileNamePane = new JPanel();
+            fileNamePane.setLayout(new BorderLayout());
+            fileNamePane.add(inputDataFileLabel, java.awt.BorderLayout.CENTER);
+        }
+        return fileNamePane;
+    }
+
+    /**
+     * This method initializes selectFileButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getSelectFileButton() {
+        if (selectFileButton == null) {
+            selectFileButton = new JButton();
+            selectFileButton.setText("Browse");
+        }
+        return selectFileButton;
+    }
+
+    /**
+     * This method initializes jCheckBox
+     * 
+     * @return javax.swing.JCheckBox
+     */
+    private JCheckBox getJudgesHaveDataFiles() {
+        if (judgesHaveDataFiles == null) {
+            judgesHaveDataFiles = new JCheckBox();
+            judgesHaveDataFiles.setBounds(new java.awt.Rectangle(23, 239, 302, 24));
+            judgesHaveDataFiles.setText("Judges Have Provided Data File");
+        }
+        return judgesHaveDataFiles;
+    }
+
+    /**
+     * This method initializes answerFilePane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getAnswerFilePane() {
+        if (answerFilePane == null) {
+            BorderLayout borderLayout1 = new BorderLayout();
+            borderLayout1.setHgap(15);
+            borderLayout1.setVgap(5);
+            answerFilePane = new JPanel();
+            answerFilePane.setLayout(borderLayout1);
+            answerFilePane.setBounds(new java.awt.Rectangle(53, 275, 423, 52));
+            answerFilePane.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Answer File", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+                    javax.swing.border.TitledBorder.DEFAULT_POSITION, null, null));
+            answerFilePane.add(getAnswerFilenamePane(), java.awt.BorderLayout.CENTER);
+            answerFilePane.add(getAnswerBrowseButton(), java.awt.BorderLayout.EAST);
+        }
+        return answerFilePane;
+    }
+
+    /**
+     * This method initializes answerFilenamePane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getAnswerFilenamePane() {
+        if (answerFilenamePane == null) {
+            answerFileNameLabel = new JLabel();
+            answerFileNameLabel.setText("");
+            answerFilenamePane = new JPanel();
+            answerFilenamePane.setLayout(new BorderLayout());
+            answerFilenamePane.add(answerFileNameLabel, java.awt.BorderLayout.CENTER);
+        }
+        return answerFilenamePane;
+    }
+
+    /**
+     * This method initializes answerBrowseButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getAnswerBrowseButton() {
+        if (answerBrowseButton == null) {
+            answerBrowseButton = new JButton();
+            answerBrowseButton.setText("Browse");
+        }
+        return answerBrowseButton;
+    }
+    
+    public void showMessage(final String message){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                messageLabel.setText(message);
+            }
+        });
+    }
+
+} // @jve:decl-index=0:visual-constraint="10,10"
