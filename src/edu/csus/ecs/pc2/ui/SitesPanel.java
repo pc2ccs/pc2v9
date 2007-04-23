81eae5c753df32d10f73c618472b156f8c9d5517
Douglas Lane <laned@ecs.csus.edu>
2007-04-23 04:26:40 +0000

    bug 11: add message label, now no longer uses JOptionPane.showMessage
    svn path=/trunk/; revision=339

--- src/edu/csus/ecs/pc2/ui/SitesPanel.java
+++ src/edu/csus/ecs/pc2/ui/SitesPanel.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Properties;
 
 import javax.swing.JButton;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -21,6 +20,9 @@ import edu.csus.ecs.pc2.core.model.ISiteListener;
 import edu.csus.ecs.pc2.core.model.Site;
 import edu.csus.ecs.pc2.core.model.SiteEvent;
 import edu.csus.ecs.pc2.core.model.SiteList;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Dimension;
 
 /**
  * View, Add and change site's settings.
@@ -45,8 +47,12 @@ public class SitesPanel extends JPanePlugin {
     private JButton updateSiteButton = null;
 
     private JButton cancelSiteEditButton = null;
-    
+
     private SiteList siteList = new SiteList();
+
+    private JPanel messagePane = null;
+
+    private JLabel messageLabel = null;
 
     /**
      * This method initializes
@@ -64,6 +70,7 @@ public class SitesPanel extends JPanePlugin {
     private void initialize() {
         this.setLayout(new BorderLayout());
         this.setSize(new java.awt.Dimension(536, 217));
+        this.add(getMessagePane(), java.awt.BorderLayout.NORTH);
         this.add(getSiteButtonPanel(), java.awt.BorderLayout.SOUTH);
         this.add(getSiteListBox(), java.awt.BorderLayout.CENTER);
 
@@ -108,29 +115,27 @@ public class SitesPanel extends JPanePlugin {
             siteListBox.addColumns(cols);
 
             /**
-             * No sorters for this frame, keep it in order.  If you
-             * want sorters, be sure to change update methods.
+             * No sorters for this frame, keep it in order. If you want sorters, be sure to change update methods.
              */
-//            // Sorters
-//            HeapSorter sorter = new HeapSorter();
-//            HeapSorter numericStringSorter = new HeapSorter();
-//            numericStringSorter.setComparator(new NumericStringComparator());
-//
-//            // Site Number
-//            siteListBox.setColumnSorter(0, numericStringSorter, 1);
-//
-//            // Site Title
-//            siteListBox.setColumnSorter(1, sorter, 2);
-//
-//            // Password
-//            siteListBox.setColumnSorter(2, sorter, 3);
-//
-//            // IP
-//            siteListBox.setColumnSorter(3, sorter, 4);
-//
-//            // Port
-//            siteListBox.setColumnSorter(4, sorter, 5);
-
+            // // Sorters
+            // HeapSorter sorter = new HeapSorter();
+            // HeapSorter numericStringSorter = new HeapSorter();
+            // numericStringSorter.setComparator(new NumericStringComparator());
+            //
+            // // Site Number
+            // siteListBox.setColumnSorter(0, numericStringSorter, 1);
+            //
+            // // Site Title
+            // siteListBox.setColumnSorter(1, sorter, 2);
+            //
+            // // Password
+            // siteListBox.setColumnSorter(2, sorter, 3);
+            //
+            // // IP
+            // siteListBox.setColumnSorter(3, sorter, 4);
+            //
+            // // Port
+            // siteListBox.setColumnSorter(4, sorter, 5);
             siteListBox.autoSizeAllColumns();
 
         }
@@ -160,7 +165,7 @@ public class SitesPanel extends JPanePlugin {
     protected void addNewSite() {
         Site newSite = createSite(siteListBox.getRowCount() + 1);
         addSiteRow(newSite);
-        enableUpdateButtons (true);
+        enableUpdateButtons(true);
     }
 
     /**
@@ -185,6 +190,7 @@ public class SitesPanel extends JPanePlugin {
 
     protected void updateSitesData() {
 
+        showMessage("");
         String message = validateSiteListBox();
 
         if (message == null) {
@@ -203,10 +209,10 @@ public class SitesPanel extends JPanePlugin {
             }
             enableUpdateButtons(false);
         } else {
-            JOptionPane.showMessageDialog(this, message);
+            showMessage(message);
         }
     }
-    
+
     /**
      * Return true if input string contains a valid port number.
      * 
@@ -224,7 +230,7 @@ public class SitesPanel extends JPanePlugin {
 
         return portNum > 0 && portNum < 65536;
     }
-    
+
     /**
      * Checks for invalid site settings.
      * 
@@ -298,42 +304,44 @@ public class SitesPanel extends JPanePlugin {
     private String validateSiteListBox() {
 
         SiteList siteListToValidate = new SiteList();
-        
+
         for (int i = 0; i < siteListBox.getRowCount(); i++) {
             Site newSite = createSiteFromRow(i);
             siteListToValidate.add(newSite);
         }
-        
+
         return validateSites(siteListToValidate);
     }
 
     /**
      * Create a Site from hst site list box.
-     * @param i - index in the list box.
+     * 
+     * @param i -
+     *            index in the list box.
      * @return
      */
     private Site createSiteFromRow(int i) {
 
-        Object [] objects = siteListBox.getRow(i);
-        
-//        Object[] cols = { "Site Number", "Site Title", "Password", "IP", "Port" };
-//        Integer siteNumberInteger = ((Integer) objects[0]);
+        Object[] objects = siteListBox.getRow(i);
+
+        // Object[] cols = { "Site Number", "Site Title", "Password", "IP", "Port" };
+        // Integer siteNumberInteger = ((Integer) objects[0]);
         String siteTitle = ((JTextField) objects[1]).getText();
         String password = ((JTextField) objects[2]).getText();
         String hostName = ((JTextField) objects[3]).getText();
         String portString = ((JTextField) objects[4]).getText();
         int port = Integer.parseInt(portString);
-        
+
         ElementId siteId = (ElementId) siteListBox.getKeys()[i];
         Site site = (Site) siteList.get(siteId);
         site.setDisplayName(siteTitle);
         site.setPassword(password);
-        
+
         Properties props = new Properties();
         props.put(Site.IP_KEY, hostName);
         props.put(Site.PORT_KEY, "" + port);
         site.setConnectionInfo(props);
-        
+
         return site;
     }
 
@@ -387,7 +395,7 @@ public class SitesPanel extends JPanePlugin {
             addSiteRow(site);
         }
 
-        enableUpdateButtons (false);
+        enableUpdateButtons(false);
     }
 
     private void enableUpdateButtons(boolean enableButtons) {
@@ -421,19 +429,20 @@ public class SitesPanel extends JPanePlugin {
         site.setPassword("site" + nextSiteNumber);
         return site;
     }
-   
+
     /**
      * Add or update a site row
+     * 
      * @param site
      */
-    private void updateSiteRow (Site site) {
+    private void updateSiteRow(Site site) {
         int row = siteListBox.getIndexByKey(site.getElementId());
-        if (row == -1){
-            Object [] objects = buildSiteRow(site);
+        if (row == -1) {
+            Object[] objects = buildSiteRow(site);
             siteListBox.addRow(objects, site.getElementId());
             siteList.add(site);
-        }else {
-            Object [] objects = buildSiteRow(site);
+        } else {
+            Object[] objects = buildSiteRow(site);
             siteListBox.replaceRow(objects, row);
             siteList.update(site);
         }
@@ -442,6 +451,7 @@ public class SitesPanel extends JPanePlugin {
 
     /**
      * Add a site row.
+     * 
      * @param site
      */
     private void addSiteRow(Site site) {
@@ -450,16 +460,15 @@ public class SitesPanel extends JPanePlugin {
 
     public void setModelAndController(IModel inModel, IController inController) {
         super.setModelAndController(inModel, inController);
-        
+
         getModel().addSiteListener(new SiteListenerImplementation());
-        
+
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 reloadListBox();
             }
         });
     }
-    
 
     /**
      * Site Listener for use by ServerView.
@@ -471,7 +480,7 @@ public class SitesPanel extends JPanePlugin {
 
         public void siteAdded(SiteEvent event) {
             StaticLog.unclassified("Site " + event.getAction() + " " + event.getSite());
-            updateSiteList (event.getSite());
+            updateSiteList(event.getSite());
         }
 
         public void siteRemoved(SiteEvent event) {
@@ -488,24 +497,26 @@ public class SitesPanel extends JPanePlugin {
 
         public void siteChanged(SiteEvent event) {
             StaticLog.unclassified("Site " + event.getAction() + " " + event.getSite());
-            updateSiteList (event.getSite());
+            updateSiteList(event.getSite());
         }
     }
-    
+
     /**
      * Reset to model existing.
-     *
+     * 
      */
     protected void undoEdit() {
+        showMessage("");
         reloadListBox();
     }
 
     /**
      * Update site list with input site.
+     * 
      * @param site
      */
     public void updateSiteList(final Site site) {
-        
+
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 updateSiteRow(site);
@@ -513,4 +524,30 @@ public class SitesPanel extends JPanePlugin {
         });
     }
 
+    /**
+     * This method initializes jPanel
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getMessagePane() {
+        if (messagePane == null) {
+            messageLabel = new JLabel();
+            messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            messageLabel.setText("");
+            messagePane = new JPanel();
+            messagePane.setLayout(new BorderLayout());
+            messagePane.setPreferredSize(new Dimension(25, 25));
+            messagePane.add(messageLabel, java.awt.BorderLayout.CENTER);
+        }
+        return messagePane;
+    }
+
+    private void showMessage(final String string) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                messageLabel.setText(string);
+            }
+        });
+    }
 } // @jve:decl-index=0:visual-constraint="10,10"
