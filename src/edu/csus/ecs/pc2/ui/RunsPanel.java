1da1d224b26c474e2a7cb934ae7ee0455b8922b4
Douglas Lane <laned@ecs.csus.edu>
2007-04-24 03:58:00 +0000

    bug 11: added buttons to RunsPanel
    svn path=/trunk/; revision=360

--- src/edu/csus/ecs/pc2/ui/RunsPanel.java
+++ src/edu/csus/ecs/pc2/ui/RunsPanel.java
@@ -1,7 +1,10 @@
 package edu.csus.ecs.pc2.ui;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import com.ibm.webrunner.j2mclb.util.HeapSorter;
@@ -25,7 +28,7 @@ import edu.csus.ecs.pc2.core.model.Run.RunStates;
 
 /**
  * View runs.
- *
+ * 
  * @author pc2@ecs.csus.edu
  */
 
@@ -36,7 +39,28 @@ public class RunsPanel extends JPanePlugin {
      * 
      */
     private static final long serialVersionUID = 114647004580210428L;
+
     private MCLB runListBox = null;
+
+    private JPanel messagePanel = null;
+
+    private JPanel buttonPanel = null;
+
+    private JButton requestRunButton = null;
+
+    private JButton filterButton = null;
+
+    private JButton editRunButton = null;
+
+    private JButton extractButton = null;
+
+    private JButton giveButton = null;
+
+    private JButton takeButton = null;
+
+    private JButton rejudgeRunButton = null;
+
+    private JButton viewJudgementsButton = null;
 
     /**
      * This method initializes
@@ -54,14 +78,16 @@ public class RunsPanel extends JPanePlugin {
     private void initialize() {
         this.setLayout(new BorderLayout());
         this.setSize(new java.awt.Dimension(625, 216));
+        this.add(getButtonPanel(), java.awt.BorderLayout.SOUTH);
         this.add(getRunsListBox(), java.awt.BorderLayout.CENTER);
+        this.add(getMessagePanel(), java.awt.BorderLayout.NORTH);
     }
 
     @Override
     public String getPluginTitle() {
         return "Runs Panel";
     }
-    
+
     protected Object[] buildRunRow(Run run, ClientId judgeId) {
         // Object[] cols = {"Site","Team","Run Id","Time","Status", "Judge",
         // "Problem","Language","OS"};
@@ -91,10 +117,9 @@ public class RunsPanel extends JPanePlugin {
         return null;
     }
 
-    
     private String getLanguageTitle(ElementId languageId) {
         // TODO Auto-generated method stub
-        for (Language language : getModel().getLanguages()){
+        for (Language language : getModel().getLanguages()) {
             if (language.getElementId().equals(languageId)) {
                 return language.toString();
             }
@@ -104,8 +129,8 @@ public class RunsPanel extends JPanePlugin {
 
     private String getProblemTitle(ElementId problemId) {
         Problem problem = getModel().getProblem(problemId);
-        System.out.println("debug getProblemTitle - "+problemId+" "+problem);
-        if (problem != null){
+        System.out.println("debug getProblemTitle - " + problemId + " " + problem);
+        if (problem != null) {
             return problem.toString();
         }
         return "Problem ?";
@@ -113,42 +138,33 @@ public class RunsPanel extends JPanePlugin {
 
     private String getSiteTitle(String string) {
         // TODO Auto-generated method stub
-        return "Site "+string;
+        return "Site " + string;
     }
 
     private String getTeamDisplayName(Run run) {
         Account account = getModel().getAccount(run.getSubmitter());
-        if (account != null ){
+        if (account != null) {
             return account.getDisplayName();
         }
-        
-        return run.getSubmitter().getName(); 
-    }
 
-    private Object getJudgementTitle(ElementId judgementId) {
-        Judgement judgement = getModel().getJudgement(judgementId);
-        if (judgement != null){
-            return judgement.toString();
-        }
-        return "Judgement ?";
+        return run.getSubmitter().getName();
     }
-
 
     /**
      * 
-     *
+     * 
      * @author pc2@ecs.csus.edu
      */
-    
+
     // $HeadURL$
-    public class RunListenerImplementation implements IRunListener{
+    public class RunListenerImplementation implements IRunListener {
 
         public void runAdded(RunEvent event) {
-            updateRunRow (event.getRun(), event.getWhoModifiedRun());
+            updateRunRow(event.getRun(), event.getWhoModifiedRun());
         }
 
         public void runChanged(RunEvent event) {
-            updateRunRow (event.getRun(), event.getWhoModifiedRun());
+            updateRunRow(event.getRun(), event.getWhoModifiedRun());
         }
 
         public void runRemoved(RunEvent event) {
@@ -224,32 +240,31 @@ public class RunsPanel extends JPanePlugin {
             }
         });
     }
-    
-    public void reloadRunList () {
-        
-        Run [] runs = getModel().getRuns();
-        
+
+    public void reloadRunList() {
+
+        Run[] runs = getModel().getRuns();
+
         // TODO bulk load these record
-        
-        for (Run run : runs){
-            
+
+        for (Run run : runs) {
+
             ClientId clientId = null;
-            
-            RunStates runStates =  run.getStatus();
-            if ( ! (runStates.equals(RunStates.NEW) || run.isDeleted())){
+
+            RunStates runStates = run.getStatus();
+            if (!(runStates.equals(RunStates.NEW) || run.isDeleted())) {
                 JudgementRecord judgementRecord = run.getJudgementRecord();
                 if (judgementRecord != null) {
-                    clientId = judgementRecord.getJudgerClientId(); 
+                    clientId = judgementRecord.getJudgerClientId();
                 }
             }
             updateRunRow(run, clientId);
         }
     }
-    
 
     public void setModelAndController(IModel inModel, IController inController) {
         super.setModelAndController(inModel, inController);
-        
+
         getModel().addRunListener(new RunListenerImplementation());
 
         SwingUtilities.invokeLater(new Runnable() {
@@ -257,6 +272,192 @@ public class RunsPanel extends JPanePlugin {
                 reloadRunList();
             }
         });
+    }
+
+    /**
+     * This method initializes messagePanel
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getMessagePanel() {
+        if (messagePanel == null) {
+            messagePanel = new JPanel();
+        }
+        return messagePanel;
+    }
+
+    /**
+     * This method initializes buttonPanel
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getButtonPanel() {
+        if (buttonPanel == null) {
+            FlowLayout flowLayout = new FlowLayout();
+            flowLayout.setHgap(25);
+            buttonPanel = new JPanel();
+            buttonPanel.setLayout(flowLayout);
+            buttonPanel.setPreferredSize(new java.awt.Dimension(70, 70));
+            buttonPanel.add(getGiveButton(), null);
+            buttonPanel.add(getTakeButton(), null);
+            buttonPanel.add(getEditRunButton(), null);
+            buttonPanel.add(getViewJudgementsButton(), null);
+            buttonPanel.add(getRequestRunButton(), null);
+            buttonPanel.add(getRejudgeRunButton(), null);
+            buttonPanel.add(getFilterButton(), null);
+            buttonPanel.add(getExtractButton(), null);
+        }
+        return buttonPanel;
+    }
+
+    /**
+     * This method initializes requestRunButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getRequestRunButton() {
+        if (requestRunButton == null) {
+            requestRunButton = new JButton();
+            requestRunButton.setText("Request Run");
+            requestRunButton.setMnemonic(java.awt.event.KeyEvent.VK_R);
+            requestRunButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return requestRunButton;
+    }
+
+    /**
+     * This method initializes filterButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getFilterButton() {
+        if (filterButton == null) {
+            filterButton = new JButton();
+            filterButton.setText("Filter");
+            filterButton.setMnemonic(java.awt.event.KeyEvent.VK_F);
+            filterButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return filterButton;
+    }
+
+    /**
+     * This method initializes editRunButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getEditRunButton() {
+        if (editRunButton == null) {
+            editRunButton = new JButton();
+            editRunButton.setText("Edit");
+            editRunButton.setMnemonic(java.awt.event.KeyEvent.VK_E);
+            editRunButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return editRunButton;
+    }
+
+    /**
+     * This method initializes extractButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getExtractButton() {
+        if (extractButton == null) {
+            extractButton = new JButton();
+            extractButton.setText("Extract");
+            extractButton.setMnemonic(java.awt.event.KeyEvent.VK_X);
+            extractButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return extractButton;
+    }
+
+    /**
+     * This method initializes giveButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getGiveButton() {
+        if (giveButton == null) {
+            giveButton = new JButton();
+            giveButton.setText("Give");
+            giveButton.setMnemonic(java.awt.event.KeyEvent.VK_G);
+            giveButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return giveButton;
+    }
+
+    /**
+     * This method initializes takeButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getTakeButton() {
+        if (takeButton == null) {
+            takeButton = new JButton();
+            takeButton.setText("Take");
+            takeButton.setMnemonic(java.awt.event.KeyEvent.VK_T);
+            takeButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return takeButton;
+    }
+
+    /**
+     * This method initializes rejudgeRunButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getRejudgeRunButton() {
+        if (rejudgeRunButton == null) {
+            rejudgeRunButton = new JButton();
+            rejudgeRunButton.setText("Rejudge");
+            rejudgeRunButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return rejudgeRunButton;
+    }
+
+    /**
+     * This method initializes viewJudgementsButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getViewJudgementsButton() {
+        if (viewJudgementsButton == null) {
+            viewJudgementsButton = new JButton();
+            viewJudgementsButton.setText("View Judgements");
+            viewJudgementsButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+                }
+            });
+        }
+        return viewJudgementsButton;
     }
 
 } // @jve:decl-index=0:visual-constraint="10,10"
