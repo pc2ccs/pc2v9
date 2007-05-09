e55226ec718228d9c649cfa205b7c74ae37f9501
Troy Boudreau <boudreat@ecs.csus.edu>
2007-05-09 22:44:27 +0000

    bug 10: add test to detect failure in alpha sorting
    svn path=/trunk/; revision=622

--- test/edu/csus/ecs/pc2/core/TestAccountNameComparator.java
+++ test/edu/csus/ecs/pc2/core/TestAccountNameComparator.java
@@ -44,4 +44,44 @@ public class TestAccountNameComparator extends TestCase {
             assertTrue("Sort failed on display name " + names[j] + " expecting " + (j + 1), names[j].equalsIgnoreCase("team" + (j + 1)));
         }
     }
+    public void testOrderAlpha() {
+
+        int numberAccounts = 10;
+
+        AccountList accountList = new AccountList();
+
+        accountList.generateNewAccounts(ClientType.Type.TEAM, numberAccounts, 1, PasswordType.JOE, 12, true);
+
+        Account[] accounts = accountList.getList();
+
+        String[] names = new String[accounts.length];
+        String[] namesAlpha = new String[accounts.length];
+
+        for (int j = 0; j < accounts.length; j++) {
+            if (accounts[j].getClientId().getClientNumber() == 3) {
+                accounts[j].setDisplayName("teal3");
+            }
+            if (accounts[j].getClientId().getClientNumber() == 7) {
+                accounts[j].setDisplayName("teal7");
+            }
+            if (accounts[j].getClientId().getClientNumber() == 9) {
+                accounts[j].setDisplayName("teams9");
+            }
+            if (accounts[j].getClientId().getClientNumber() == 10) {
+                accounts[j].setDisplayName("tea3");
+            }
+            names[j] = accounts[j].getDisplayName();
+            namesAlpha[j] = accounts[j].getDisplayName();
+        }
+
+        // Regular string sort
+        Arrays.sort(namesAlpha);
+
+        // sort by display name
+        Arrays.sort(names, new AccountNameComparator());
+
+        for (int j = 0; j < names.length; j++) {
+            assertTrue("Alpha sort failed on display name " + names[j] + " expecting " + namesAlpha[j], names[j].equals(namesAlpha[j]));
+        }
+    }
 }
