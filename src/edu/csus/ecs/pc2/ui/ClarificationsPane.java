4d6ffe61d18a412139ef24a26ffca4697f4e13cb
Douglas Lane <laned@ecs.csus.edu>
2007-05-10 04:15:39 +0000

    bug 11: Added Question and Answer preview/view.
    svn path=/trunk/; revision=629

--- src/edu/csus/ecs/pc2/ui/ClarificationsPane.java
+++ src/edu/csus/ecs/pc2/ui/ClarificationsPane.java
@@ -31,6 +31,12 @@ import edu.csus.ecs.pc2.core.model.ProblemEvent;
 import edu.csus.ecs.pc2.core.model.Clarification.ClarificationStates;
 import edu.csus.ecs.pc2.core.security.Permission;
 import edu.csus.ecs.pc2.core.security.PermissionList;
+import java.awt.GridLayout;
+import javax.swing.JSplitPane;
+import java.awt.Dimension;
+import javax.swing.JTextArea;
+import javax.swing.BorderFactory;
+import javax.swing.border.TitledBorder;
 
 /**
  * Shows clarifications in a list box.
@@ -67,9 +73,21 @@ public class ClarificationsPane extends JPanePlugin {
     private JPanel messagePane = null;
 
     private JLabel messageLabel = null;
-    
+
     private AnswerClarificationFrame answerClarificationFrame;
-    
+
+    private JPanel centerPane = null;
+
+    private JSplitPane clarificationSplitPane = null;
+
+    private JPanel clarificationPane = null;
+
+    private JTextArea questionTextArea = null;
+
+    private JPanel answerPane = null;
+
+    private JTextArea answerTextArea = null;
+
     /**
      * This method initializes
      * 
@@ -81,7 +99,7 @@ public class ClarificationsPane extends JPanePlugin {
 
     /**
      * @author pc2@ecs.csus.edu
-     *
+     * 
      */
     public class LanguageListenerImplementation implements ILanguageListener {
 
@@ -100,11 +118,12 @@ public class ClarificationsPane extends JPanePlugin {
         public void languageRemoved(LanguageEvent event) {
             // ignore, does not affect this pane
         }
-        
+
     }
+
     /**
      * @author pc2@ecs.csus.edu
-     *
+     * 
      */
     public class ProblemListenerImplementation implements IProblemListener {
 
@@ -123,20 +142,20 @@ public class ClarificationsPane extends JPanePlugin {
         public void problemRemoved(ProblemEvent event) {
             // ignore, does not affect this pane
         }
-        
+
     }
-    
+
     /**
      * This method initializes this
      * 
      */
     private void initialize() {
         this.setLayout(new BorderLayout());
-        this.setSize(new java.awt.Dimension(622,229));
-        this.add(getClarificationListBox(), java.awt.BorderLayout.CENTER);
+        this.setSize(new java.awt.Dimension(622, 229));
+        this.add(getCenterPane(), java.awt.BorderLayout.CENTER);
         this.add(getMessagePane(), java.awt.BorderLayout.NORTH);
         this.add(getClarificationButtonPane(), java.awt.BorderLayout.SOUTH);
-        
+
         answerClarificationFrame = new AnswerClarificationFrame();
 
     }
@@ -176,6 +195,15 @@ public class ClarificationsPane extends JPanePlugin {
     private MCLB getClarificationListBox() {
         if (clarificationListBox == null) {
             clarificationListBox = new MCLB();
+
+            clarificationListBox.addListboxListener(new com.ibm.webrunner.j2mclb.event.ListboxListener() {
+                public void rowSelected(com.ibm.webrunner.j2mclb.event.ListboxEvent e) {
+                    showSelectedClarification();
+                }
+                public void rowDeselected(com.ibm.webrunner.j2mclb.event.ListboxEvent e) {
+                    showSelectedClarification();
+                }
+            });
 
             Object[] cols = { "Site", "Team", "Clar Id", "Time", "Status", "Judge", "Sent to", "Problem", "Question", "Answer" };
             clarificationListBox.addColumns(cols);
@@ -220,6 +248,46 @@ public class ClarificationsPane extends JPanePlugin {
         }
         return clarificationListBox;
     }
+    
+
+    protected void showSelectedClarification() {
+        
+        int selectedClarificationIndex = getClarificationListBox().getSelectedIndex();
+        
+        if (selectedClarificationIndex == -1){
+            getAnswerPane().setVisible(false);
+            getAnswerTextArea().setText("");
+            getQuestionTextArea().setText("");
+            String clarificationTitle = "Clarification ";
+            clarificationPane.setBorder(BorderFactory.createTitledBorder(null, clarificationTitle, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
+            return;
+        }
+
+        ElementId clarificationId = (ElementId) getClarificationListBox().getKeys()[selectedClarificationIndex];
+        
+        Clarification clarification = getContest().getClarification(clarificationId);
+        
+        if (clarification != null){
+            
+            String clarificationTitle = "Clarification "+clarification.getNumber()+"  from "+getTeamDisplayName(clarification.getSubmitter())+" (Site "+clarification.getSiteNumber()+")";
+            clarificationPane.setBorder(BorderFactory.createTitledBorder(null, clarificationTitle, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
+            
+            getQuestionTextArea().setText(clarification.getQuestion());
+            
+            if (clarification.getAnswer() != null){
+                showMessage("debug Do show answer");
+                getAnswerPane().setVisible(true);
+                getAnswerTextArea().setText(clarification.getAnswer());
+            } else {
+                // Don't show answer pane if no answer
+                showMessage("debug Do not show answer");
+                getAnswerPane().setVisible(false);
+            }
+        }
+
+        // Show preview pane
+        getClarificationSplitPane().setVisible(true);
+    }
 
     public void updateClarificationRow(final Clarification clarification, final ClientId whoChangedId) {
         SwingUtilities.invokeLater(new Runnable() {
@@ -248,25 +316,25 @@ public class ClarificationsPane extends JPanePlugin {
         obj[1] = getTeamDisplayName(clar.getSubmitter());
         obj[2] = clar.getNumber();
         obj[3] = clar.getElapsedMins();
-        
+
         boolean isTeam = getContest().getClientId().getClientType().equals(ClientType.Type.TEAM);
-        
+
         if (isTeam) {
             obj[4] = "New";
-            if (clar.isAnswered()){
+            if (clar.isAnswered()) {
                 obj[4] = "Answered";
             }
         } else {
             obj[4] = clar.getState();
         }
-        
+
         obj[5] = "";
         if (clar.isAnswered()) {
-            
+
             if (clar.getWhoJudgedItId() == null || isTeam) {
                 obj[5] = "";
             } else {
-                obj[5] = getTeamDisplayName(clar.getWhoJudgedItId());
+                obj[5] = clar.getWhoJudgedItId().getName();
             }
         }
 
@@ -326,7 +394,7 @@ public class ClarificationsPane extends JPanePlugin {
         answerClarificationFrame.setContestAndController(inContest, inController);
 
         initializePermissions();
-        
+
         getContest().addClarificationListener(new ClarificationListenerImplementation());
         getContest().addAccountListener(new AccountListenerImplementation());
         getContest().addProblemListener(new ProblemListenerImplementation());
@@ -354,11 +422,13 @@ public class ClarificationsPane extends JPanePlugin {
     }
 
     private String getTeamDisplayName(ClientId clientId) {
-        Account account = getContest().getAccount(clientId);
-        if (account != null) {
-            return account.getDisplayName();
-        }
-
+        // TODO code change this depending on how to display team names
+        
+//        Account account = getContest().getAccount(clientId);
+//        if (account != null) {
+//            return account.getDisplayName();
+//        }
+        
         return clientId.getName();
     }
 
@@ -477,7 +547,7 @@ public class ClarificationsPane extends JPanePlugin {
         giveButton.setVisible(isAllowed(Permission.Type.GIVE_CLARIFICATION));
         takeButton.setVisible(isAllowed(Permission.Type.TAKE_CLARIFICATION));
         generateClarificationButton.setVisible(isAllowed(Permission.Type.GENERATE_NEW_CLARIFICATION));
-        
+
     }
 
     /**
@@ -538,19 +608,18 @@ public class ClarificationsPane extends JPanePlugin {
 
     protected void requestSelectedClarification() {
 
-        int [] selectedIndexes = clarificationListBox.getSelectedIndexes();
-        
-        if (selectedIndexes.length < 1){
+        int[] selectedIndexes = clarificationListBox.getSelectedIndexes();
+
+        if (selectedIndexes.length < 1) {
             showMessage("Please select a run ");
             return;
         }
-        
+
         try {
             ElementId elementId = (ElementId) clarificationListBox.getKeys()[selectedIndexes[0]];
-            Clarification clarificationToAnswer  = getContest().getClarification(elementId);
-            
-            
-            if ((! clarificationToAnswer.getState().equals(ClarificationStates.NEW)) || clarificationToAnswer.isDeleted()){
+            Clarification clarificationToAnswer = getContest().getClarification(elementId);
+
+            if ((!clarificationToAnswer.getState().equals(ClarificationStates.NEW)) || clarificationToAnswer.isDeleted()) {
                 showMessage("Not allowed to request run, already judged");
                 return;
             }
@@ -560,7 +629,7 @@ public class ClarificationsPane extends JPanePlugin {
         } catch (Exception e) {
             getController().getLog().log(Log.WARNING, "Exception logged ", e);
             showMessage("Unable to answer clarification, check log");
-        }   
+        }
     }
 
     /**
@@ -575,12 +644,12 @@ public class ClarificationsPane extends JPanePlugin {
             messageLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
             messagePane = new JPanel();
             messagePane.setLayout(new BorderLayout());
-            messagePane.setPreferredSize(new java.awt.Dimension(32,32));
+            messagePane.setPreferredSize(new java.awt.Dimension(32, 32));
             messagePane.add(messageLabel, java.awt.BorderLayout.CENTER);
         }
         return messagePane;
     }
-    
+
     private void showMessage(final String string) {
 
         SwingUtilities.invokeLater(new Runnable() {
@@ -590,6 +659,93 @@ public class ClarificationsPane extends JPanePlugin {
             }
         });
     }
-    
-    
+
+    /**
+     * This method initializes centerPane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getCenterPane() {
+        if (centerPane == null) {
+            GridLayout gridLayout = new GridLayout();
+            gridLayout.setRows(2);
+            centerPane = new JPanel();
+            centerPane.setLayout(gridLayout);
+            centerPane.add(getClarificationListBox(), null);
+            centerPane.add(getClarificationSplitPane(), null);
+        }
+        return centerPane;
+    }
+
+    /**
+     * This method initializes jSplitPane
+     * 
+     * @return javax.swing.JSplitPane
+     */
+    private JSplitPane getClarificationSplitPane() {
+        if (clarificationSplitPane == null) {
+            clarificationSplitPane = new JSplitPane();
+            clarificationSplitPane.setPreferredSize(new Dimension(120, 120));
+            clarificationSplitPane.setTopComponent(getClarificationPane());
+            clarificationSplitPane.setBottomComponent(getAnswerPane());
+            clarificationSplitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        }
+        return clarificationSplitPane;
+    }
+
+    /**
+     * This method initializes jPanel
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getClarificationPane() {
+        if (clarificationPane == null) {
+            clarificationPane = new JPanel();
+            clarificationPane.setLayout(new BorderLayout());
+            clarificationPane.setPreferredSize(new Dimension(10, 40));
+            clarificationPane.setBorder(BorderFactory.createTitledBorder(null, "Clarification", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
+            clarificationPane.add(getQuestionTextArea(), java.awt.BorderLayout.CENTER);
+        }
+        return clarificationPane;
+    }
+
+    /**
+     * This method initializes jTextArea
+     * 
+     * @return javax.swing.JTextArea
+     */
+    private JTextArea getQuestionTextArea() {
+        if (questionTextArea == null) {
+            questionTextArea = new JTextArea();
+        }
+        return questionTextArea;
+    }
+
+    /**
+     * This method initializes jPanel1
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getAnswerPane() {
+        if (answerPane == null) {
+            answerPane = new JPanel();
+            answerPane.setLayout(new BorderLayout());
+            answerPane.setBorder(BorderFactory.createTitledBorder(null, "Answer", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
+            answerPane.add(getAnswerTextArea(), java.awt.BorderLayout.CENTER);
+        }
+        return answerPane;
+    }
+
+    /**
+     * This method initializes jTextArea1
+     * 
+     * @return javax.swing.JTextArea
+     */
+    private JTextArea getAnswerTextArea() {
+        if (answerTextArea == null) {
+            answerTextArea = new JTextArea();
+        }
+        return answerTextArea;
+    }
+
 } // @jve:decl-index=0:visual-constraint="10,10"
