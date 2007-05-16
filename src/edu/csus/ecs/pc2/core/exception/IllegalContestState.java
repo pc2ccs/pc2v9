22c0cc168f01c32fb4ce3467728ea213f242a23a
Troy Boudreau <boudreat@ecs.csus.edu>
2007-05-16 01:53:40 +0000

    bug 10: add new exceptions (to be thrown by DefaultScoringAlgorithm)
    svn path=/trunk/; revision=656

---
+++ src/edu/csus/ecs/pc2/core/exception/IllegalContestState.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package edu.csus.ecs.pc2.core.exception;
+
+/**
+ * Signals that a contest is in an illegal state.
+ * 
+ * @author pc2@ecs.csus.edu
+ *
+ */
+public class IllegalContestState extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -8484343906357576436L;
+
+    public IllegalContestState(String message){
+        super(message);
+    }
+
+}
---
+++ src/edu/csus/ecs/pc2/core/exception/IllegalRunState.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.csus.ecs.pc2.core.exception;
+
+/**
+ * Signals that a Run is in an illegal state.
+ * 
+ * @author pc2@ecs.csus.edu
+ *
+ */
+public class IllegalRunState extends IllegalContestState {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 4354597345499633383L;
+
+    public IllegalRunState(String message){
+        super(message);
+    }
+}
