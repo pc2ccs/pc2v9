65f1ebf05d5b00f564ddd22a9e21daa84452d911
Douglas Lane <laned@ecs.csus.edu>
2007-10-06 08:04:32 +0000

    bug 11: add Frame for Reports Pane
    svn path=/trunk/; revision=798

---
+++ src/edu/csus/ecs/pc2/ui/ReportFrame.java
@@ -0,0 +1,66 @@
+package edu.csus.ecs.pc2.ui;
+
+import javax.swing.JFrame;
+
+import edu.csus.ecs.pc2.core.IController;
+import edu.csus.ecs.pc2.core.model.IContest;
+
+/**
+ * Report Frame.s
+ * 
+ * @author pc2@ecs.csus.edu
+ * @version $Id$
+ */
+
+// $HeadURL$
+public class ReportFrame extends JFrame implements UIPlugin {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 4445138164153522572L;
+
+    private ReportPane reportPane = null;
+
+    /**
+     * This method initializes
+     * 
+     */
+    public ReportFrame() {
+        super();
+        initialize();
+    }
+
+    /**
+     * This method initializes this
+     * 
+     */
+    private void initialize() {
+        this.setSize(new java.awt.Dimension(496, 230));
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        this.setTitle("Contest Reports");
+        this.setContentPane(getReportPane());
+
+    }
+
+    public void setContestAndController(IContest inContest, IController inController) {
+        reportPane.setContestAndController(inContest, inController);
+    }
+
+    public String getPluginTitle() {
+        return "Report Frame";
+    }
+
+    /**
+     * This method initializes reportPane
+     * 
+     * @return edu.csus.ecs.pc2.ui.ReportPane
+     */
+    private ReportPane getReportPane() {
+        if (reportPane == null) {
+            reportPane = new ReportPane();
+        }
+        return reportPane;
+    }
+
+} // @jve:decl-index=0:visual-constraint="10,10"
