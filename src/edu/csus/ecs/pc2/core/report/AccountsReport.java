157bdca3a96674b21f62d16a1c470f348c1350af
Troy Boudreau <boudreat@ecs.csus.edu>
2007-10-12 01:52:31 +0000

    bug 10: add alias & group to report
    svn path=/trunk/; revision=894

--- src/edu/csus/ecs/pc2/core/report/AccountsReport.java
+++ src/edu/csus/ecs/pc2/core/report/AccountsReport.java
@@ -15,7 +15,9 @@ import edu.csus.ecs.pc2.core.log.Log;
 import edu.csus.ecs.pc2.core.model.Account;
 import edu.csus.ecs.pc2.core.model.ClientId;
 import edu.csus.ecs.pc2.core.model.ClientType;
+import edu.csus.ecs.pc2.core.model.ElementId;
 import edu.csus.ecs.pc2.core.model.Filter;
+import edu.csus.ecs.pc2.core.model.Group;
 import edu.csus.ecs.pc2.core.model.IContest;
 import edu.csus.ecs.pc2.core.model.Site;
 import edu.csus.ecs.pc2.core.model.ClientType.Type;
@@ -137,7 +139,21 @@ public class AccountsReport implements IReport {
                     if (account.isAllowed(type)){
                         printWriter.print(type+" ");
                     }
-                    
+                    printWriter.println();
+
+                    printWriter.format("%22s"," ");
+                    printWriter.print("alias '" + account.getAliasName()+"' ");
+                    ElementId groupId = account.getGroupId();
+                    if (groupId != null) {
+                        Group group = contest.getGroup(groupId);
+                        if (group != null) {
+                            printWriter.print("group '"+group+"' ("+groupId+")");
+                        } else {
+                            printWriter.print("group invalid ("+groupId+")");
+                        }
+                    } else {
+                        printWriter.print("group ''");
+                    }
                     printWriter.println();
                 }
             }
