0fb788d8e4ee762707d2d54664412b82d9fc0682
Troy Boudreau <boudreat@ecs.csus.edu>
2007-10-13 22:23:55 +0000

    bug 10: when we edit a BalloonSettings, put the site in the titleBar
    svn path=/trunk/; revision=932

--- src/edu/csus/ecs/pc2/ui/EditBalloonSettingsFrame.java
+++ src/edu/csus/ecs/pc2/ui/EditBalloonSettingsFrame.java
@@ -76,6 +76,11 @@ public class EditBalloonSettingsFrame extends JFrame implements UIPlugin {
 
     public void setBalloonSettings(BalloonSettings balloonSettings) {
         getBalloonSettingPane().setBalloonSettings(balloonSettings);
+        if (balloonSettings != null) {
+            this.setTitle("Edit Balloon Settings for Site " + balloonSettings.getSiteNumber());
+        } else {
+            this.setTitle("Edit Balloon Settings");
+        }
 
     }
 
