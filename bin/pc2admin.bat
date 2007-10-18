908787629ffa21fe67788e8c22ba5dcd779f209e
Douglas Lane <laned@ecs.csus.edu>
2007-10-18 04:36:34 +0000

    bug 11: add admin, board and judge scripts
    svn path=/trunk/; revision=949

---
+++ bin/pc2admin.bat
@@ -0,0 +1,11 @@
+@echo off
+
+rem Purpose: start pc2
+rem Author : pc2@ecs.csus.edu
+rem $HeadURL$
+
+call %0\..\pc2env.bat
+
+java -cp %libdir%\pc2.jar;%mclbdir%\mclb.jar edu.csus.ecs.pc2.Starter %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+rem eof pc2admin.bat $Id$
