40e570eb47b60ae4e3cbdce29920738e4ba45c77
Douglas Lane <laned@ecs.csus.edu>
2007-10-20 04:03:02 +0000

    bug 11: add ClientId comparator
    svn path=/trunk/; revision=959

---
+++ src/edu/csus/ecs/pc2/core/list/ClientIdComparator.java
@@ -0,0 +1,37 @@
+package edu.csus.ecs.pc2.core.list;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import edu.csus.ecs.pc2.core.model.ClientId;
+
+/**
+ * Sort ClientIds by site, type and id.
+ * @author pc2@ecs.csus.edu
+ * @version $Id$
+ */
+
+public class ClientIdComparator implements Comparator<ClientId>, Serializable {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6684993280342683013L;
+
+    public int compare(ClientId clientId1, ClientId clientId2) {
+
+        int site1 = clientId1.getSiteNumber();
+        int site2 = clientId1.getSiteNumber();
+
+        if (site1 == site2) {
+
+            if (clientId1.getClientType().equals(clientId2.getClientType())) {
+                return clientId1.getClientNumber() - clientId2.getClientNumber();
+            } else {
+                return clientId1.getClientType().compareTo(clientId2.getClientType());
+            }
+
+        } else {
+            return site1 - site2;
+        }
+    }
+}
