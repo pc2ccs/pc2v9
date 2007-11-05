65b767a44aacdc97d57a44f1e7a9fe1c5b534903
Douglas Lane <laned@ecs.csus.edu>
2007-11-05 05:01:24 +0000

    bug 11: added contest clock to top pane for admin
    svn path=/trunk/; revision=1042

--- src/edu/csus/ecs/pc2/ui/admin/AdministratorView.java
+++ src/edu/csus/ecs/pc2/ui/admin/AdministratorView.java
@@ -19,6 +19,7 @@ import edu.csus.ecs.pc2.ui.AutoJudgesPane;
 import edu.csus.ecs.pc2.ui.BalloonSettingsPane;
 import edu.csus.ecs.pc2.ui.ClarificationsPane;
 import edu.csus.ecs.pc2.ui.ConnectionsPane;
+import edu.csus.ecs.pc2.ui.ContestClockDisplay;
 import edu.csus.ecs.pc2.ui.ContestClockPane;
 import edu.csus.ecs.pc2.ui.ContestInformationPane;
 import edu.csus.ecs.pc2.ui.ContestTimesPane;
@@ -40,6 +41,9 @@ import edu.csus.ecs.pc2.ui.StandingsHTMLPane;
 import edu.csus.ecs.pc2.ui.StandingsPane;
 import edu.csus.ecs.pc2.ui.TeamStatusPane;
 import edu.csus.ecs.pc2.ui.UIPlugin;
+import edu.csus.ecs.pc2.ui.ContestClockDisplay.DisplayTimes;
+
+import javax.swing.JLabel;
 
 /**
  * Administrator GUI.
@@ -49,7 +53,6 @@ import edu.csus.ecs.pc2.ui.UIPlugin;
 
 // $HeadURL$
 // $Id$
-
 public class AdministratorView extends JFrame implements UIPlugin {
 
     private static final long serialVersionUID = 1L;
@@ -69,7 +72,17 @@ public class AdministratorView extends JFrame implements UIPlugin {
     private JButton exitButton = null;
 
     private LogWindow logWindow = null;
-    
+
+    private JPanel clockPane = null;
+
+    private JPanel exitButtonPane = null;
+
+    private JLabel clockLabel = null;
+
+    private JPanel padPane = null;
+
+    private ContestClockDisplay contestClockDisplay = null;
+
     /**
      * This method initializes
      * 
@@ -84,7 +97,7 @@ public class AdministratorView extends JFrame implements UIPlugin {
      * 
      */
     private void initialize() {
-        this.setSize(new java.awt.Dimension(754,387));
+        this.setSize(new java.awt.Dimension(754, 387));
         this.setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         this.setContentPane(getJPanel());
         this.setTitle("PC^2 Administrator");
@@ -96,101 +109,103 @@ public class AdministratorView extends JFrame implements UIPlugin {
 
         FrameUtilities.centerFrame(this);
 
-
     }
 
     public void setContestAndController(IContest inContest, IController inController) {
         this.contest = inContest;
         this.controller = inController;
-        
-        
+
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 if (logWindow == null) {
                     logWindow = new LogWindow();
                 }
                 logWindow.setContestAndController(contest, controller);
-                logWindow.setTitle("Log "+contest.getClientId().toString());
+                logWindow.setTitle("Log " + contest.getClientId().toString());
 
                 RunsPanel runsPane = new RunsPanel();
                 addUIPlugin(getMainTabbedPanel(), "Runs", runsPane);
-                
+
                 ClarificationsPane clarificationsPane = new ClarificationsPane();
                 addUIPlugin(getMainTabbedPanel(), "Clarifications", clarificationsPane);
-                
+
                 StandingsPane standingsPane = new StandingsPane();
                 addUIPlugin(getMainTabbedPanel(), "Standings", standingsPane);
-                
+
                 ReportPane reportPane = new ReportPane();
                 addUIPlugin(getMainTabbedPanel(), "Reports", reportPane);
-                
+
                 GenerateAccountsPane generateAccountsPane = new GenerateAccountsPane();
                 addUIPlugin(getMainTabbedPanel(), "Generate", generateAccountsPane);
-                
+
                 AccountsPane accountsPane = new AccountsPane();
                 addUIPlugin(getMainTabbedPanel(), "Accounts", accountsPane);
-                
+
                 LanguagesPane languagesPane = new LanguagesPane();
                 addUIPlugin(getMainTabbedPanel(), "Languages", languagesPane);
-                
+
                 ProblemsPane problemsPane = new ProblemsPane();
                 addUIPlugin(getMainTabbedPanel(), "Problems", problemsPane);
-                
+
                 ContestTimesPane contestTimesPane = new ContestTimesPane();
                 addUIPlugin(getMainTabbedPanel(), "Times", contestTimesPane);
-                
+
                 SitesPanel sitesPanel = new SitesPanel();
                 addUIPlugin(getMainTabbedPanel(), "Sites", sitesPanel);
-                
+
                 LoginsPane loginsPane = new LoginsPane();
                 addUIPlugin(getMainTabbedPanel(), "Logins", loginsPane);
-                
+
                 ConnectionsPane connectionsPane = new ConnectionsPane();
                 addUIPlugin(getMainTabbedPanel(), "Connections", connectionsPane);
-                
+
                 GroupsPane groupsPane = new GroupsPane();
                 addUIPlugin(getMainTabbedPanel(), "Groups", groupsPane);
-                
+
                 OptionsPanel optionsPanel = new OptionsPanel();
                 addUIPlugin(getMainTabbedPanel(), "Options", optionsPanel);
                 optionsPanel.setLogWindow(logWindow);
-                
+
                 InfoPane infoPane = new InfoPane();
                 addUIPlugin(getMainTabbedPanel(), "Dev", infoPane);
-                
+
                 TeamStatusPane teamStatusPane = new TeamStatusPane();
                 addUIPlugin(getMainTabbedPanel(), "Team Status", teamStatusPane);
-                
+
                 ContestInformationPane contestInformationPane = new ContestInformationPane();
                 addUIPlugin(getMainTabbedPanel(), "Title", contestInformationPane);
-                
+
                 JudgementsPanel judgementsPanel = new JudgementsPanel();
                 addUIPlugin(getMainTabbedPanel(), "Judgements", judgementsPanel);
-                
+
                 ContestClockPane contestClockPane = new ContestClockPane();
                 addUIPlugin(getMainTabbedPanel(), "Clock", contestClockPane);
 
                 BalloonSettingsPane balloonSettingsPane = new BalloonSettingsPane();
                 addUIPlugin(getMainTabbedPanel(), "Notifications", balloonSettingsPane);
-              
+
                 AutoJudgesPane autoJudgesPane = new AutoJudgesPane();
                 addUIPlugin(getMainTabbedPanel(), "Auto Judge", autoJudgesPane);
-                
-//                full.xsl
-//                fullnums.xsl
-//                sumatt.xsl
-//                summary.xsl
-//                sumtime.xsl
-                
+
+                // full.xsl
+                // fullnums.xsl
+                // sumatt.xsl
+                // summary.xsl
+                // sumtime.xsl
+
                 StandingsHTMLPane standingsHTMLPane = new StandingsHTMLPane("full.xsl");
                 addUIPlugin(getMainTabbedPanel(), "Standings HTML", standingsHTMLPane);
-                
+
+                contestClockDisplay = new ContestClockDisplay(controller.getLog(), contest.getContestTime(), contest.getSiteNumber(), false, null);
+                contestClockDisplay.setContestAndController(contest, controller);
+                contestClockDisplay.addLabeltoUpdateList(clockLabel, DisplayTimes.REMAINING_TIME, contest.getSiteNumber());
+
                 setTitle("PC^2 " + contest.getTitle() + " Build " + new VersionInfo().getBuildNumber());
                 setVisible(true);
             }
         });
     }
-    
+
     public String getPluginTitle() {
         return "Admin GUI";
     }
@@ -243,12 +258,12 @@ public class AdministratorView extends JFrame implements UIPlugin {
      */
     private JPanel getTopPanel() {
         if (topPanel == null) {
-            FlowLayout flowLayout = new FlowLayout();
-            flowLayout.setAlignment(java.awt.FlowLayout.RIGHT);
             topPanel = new JPanel();
-            topPanel.setLayout(flowLayout);
+            topPanel.setLayout(new BorderLayout());
             topPanel.setPreferredSize(new java.awt.Dimension(45, 45));
-            topPanel.add(getExitButton(), null);
+            topPanel.add(getClockPane(), java.awt.BorderLayout.CENTER);
+            topPanel.add(getExitButtonPane(), java.awt.BorderLayout.EAST);
+            topPanel.add(getPadPane(), java.awt.BorderLayout.WEST);
         }
         return topPanel;
     }
@@ -283,23 +298,70 @@ public class AdministratorView extends JFrame implements UIPlugin {
     protected void addUIPlugin(JTabbedPane tabbedPane, String tabTitle, JPanePlugin plugin) {
 
         // TODO this should throw an exception
-        if (plugin == null){
+        if (plugin == null) {
             return;
         }
-        
+
         try {
             plugin.setContestAndController(contest, controller);
             tabbedPane.add(plugin, tabTitle);
-            
+
         } catch (Exception e) {
             controller.getLog().log(Log.WARNING, "Exception loading plugin ", e);
-            JOptionPane.showMessageDialog(this, "Error loading "+plugin.getPluginTitle());
+            JOptionPane.showMessageDialog(this, "Error loading " + plugin.getPluginTitle());
         }
 
     }
 
     protected void showLog(boolean showLogWindow) {
         logWindow.setVisible(showLogWindow);
+    }
+
+    /**
+     * This method initializes clockPane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getClockPane() {
+        if (clockPane == null) {
+            clockLabel = new JLabel();
+            clockLabel.setText("JLabel");
+            clockLabel.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 18));
+            clockPane = new JPanel();
+            clockPane.setLayout(new BorderLayout());
+            clockPane.add(clockLabel, java.awt.BorderLayout.CENTER);
+        }
+        return clockPane;
+    }
+
+    /**
+     * This method initializes exitButtonPane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getExitButtonPane() {
+        if (exitButtonPane == null) {
+            FlowLayout flowLayout = new FlowLayout();
+            flowLayout.setHgap(10);
+            flowLayout.setVgap(10);
+            exitButtonPane = new JPanel();
+            exitButtonPane.setLayout(flowLayout);
+            exitButtonPane.add(getExitButton(), null);
+        }
+        return exitButtonPane;
+    }
+
+    /**
+     * This method initializes padPane
+     * 
+     * @return javax.swing.JPanel
+     */
+    private JPanel getPadPane() {
+        if (padPane == null) {
+            padPane = new JPanel();
+            padPane.setPreferredSize(new java.awt.Dimension(10, 10));
+        }
+        return padPane;
     }
 
     public static void main(String[] args) {
