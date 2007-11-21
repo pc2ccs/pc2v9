03cfc6c2a014b528fc72382bd8904830fea1f564
Troy Boudreau <boudreat@ecs.csus.edu>
2007-11-21 09:00:26 +0000

    bug 10: setRun on the pane before requesting the checkoutRun
    svn path=/trunk/; revision=1094

--- src/edu/csus/ecs/pc2/ui/SelectJudgementFrame.java
+++ src/edu/csus/ecs/pc2/ui/SelectJudgementFrame.java
@@ -77,6 +77,7 @@ public class SelectJudgementFrame extends JFrame implements UIPlugin {
     }
 
     public void setRun(Run theRun) {
+        getSelectJudgementPane().setRun(theRun);
         if (theRun == null) {
             setTitle("Run not loaded");
         } else {
@@ -85,7 +86,6 @@ public class SelectJudgementFrame extends JFrame implements UIPlugin {
             getSelectJudgementPane().setRunFiles(null);
             controller.checkOutRun(theRun, false);
         }
-        getSelectJudgementPane().setRun(theRun);
     }
 
     public String getPluginTitle() {
