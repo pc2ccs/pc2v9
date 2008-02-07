6daee033a2595817977ab30f94115e43f8e5ec1a
John Clevenger <clevenger@csus.edu>
2008-02-07 09:14:57 +0000

    bug 12:  Completed javadoc updates; removed unused import.
    svn path=/trunk/; revision=1267

--- src/edu/csus/ecs/pc2/api/listener/IRunEventListener.java
+++ src/edu/csus/ecs/pc2/api/listener/IRunEventListener.java
@@ -1,15 +1,15 @@
 package edu.csus.ecs.pc2.api.listener;
 
-import edu.csus.ecs.pc2.api.IContest;
 import edu.csus.ecs.pc2.api.IRun;
 
 /**
- * Set of methods that any Run Listener must implement.
- *  
- * These are the events/methods invoked when a run has been added, removed
- * or updated.
- * <P>
- * See {@link IContest#getRuns()} for an example of use.
+ * This interface describes the set of methods that any Run Listener must implement.
+ *
+ * These methods are invoked when a run has been added, removed (marked as deleted), judged,
+ * or updated in the contest.
+ *
+ * <p>
+ * This documentation describes the current <I>draft</i> of the PC<sup>2</sup> API, which is subject to change.
  *
  * @author pc2@ecs.csus.edu
  * @version $Id$
@@ -18,45 +18,50 @@ import edu.csus.ecs.pc2.api.IRun;
 public interface IRunEventListener {
 
     /**
-     * A run has been added to the system.
+     * Invoked when a new run has been added to the contest.
      * <P>
-     * Typically this means that a run has been submitted by a team. Though it also may be caused by a remote server sending its runs to the local server.
+     * Typically this means that a run has been submitted by a team;
+     * it may also may be caused by a remote server in a multi-site contest sending its run(s) to the local server.
      * <P>
-     * The run may be added on the local server or a remote server.
+     * The added run may have been originally entered into the contest on either the local server (the server to which 
+     * this client is connected) or on a remote server.
      * 
      * @param run
-     *            the run that has changed
+     *            the {@link IRun} that has been added to the contest
      */
     void runAdded(IRun run);
 
     /**
-     * Run has been removed (marked as deleted).
-     * <P>
-     * Typically this happens when an Admin marks a run as deleted.
+     * Invoked when an existing run has been removed from the contest (marked as deleted by the Contest Administrator).
      * 
      * @param run
-     *            deleted run
+     *            the deleted {@link IRun}
      */
     void runRemoved(IRun run);
 
     /**
-     * Run judged.
+     * Invoked when an existing run has been judged; that is, has had a Judgement applied to it.
      * 
-     * Typically this happens when a judge judges a run.
+     * Typically this happens when a judge assigns a Judgement, but it can also occur
+     * as a result of an &quot;automated judger&quot; (also known as a &quot;validator&quot;)
+     * applying a judgement automatically.  
      * 
      * @param run
-     *            the judged run
+     *            the judged {@link IRun}
      */
     void runJudged(IRun run);
 
     /**
-     * Run updated or rejudged.
+     * Invoked when an existing run has been updated (modified) in some way.
      * <P>
-     * Triggered when a judge re-judges a run.
-     * Triggered when the admin changes a run (like changes the elapsed time) or changes a judgement.
+     * Typically a <code>runUpdated()</code> invocation occurs when either
+     * <ul>
+     *      <li>A judge re-judges a run; or </li>
+     *      <li>The Contest Administrator changes a setting (such as the elapsed time or the assigned {@link IJudgement}) in the run.</li>
+     * </ul>
      * 
      * @param run
-     *            the changed run
+     *            the {@link IRun} which has been changed
      */
     void runUpdated(IRun run);
 
