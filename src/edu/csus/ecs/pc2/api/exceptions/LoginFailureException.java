d7ea49648001f83211b8b05956a7dbbe80e95b63
John Clevenger <clevenger@csus.edu>
2008-02-07 08:51:39 +0000

    bug 12:  Completed javadoc updates for the api.exceptions package
    svn path=/trunk/; revision=1266

--- src/edu/csus/ecs/pc2/api/exceptions/LoginFailureException.java
+++ src/edu/csus/ecs/pc2/api/exceptions/LoginFailureException.java
@@ -1,10 +1,26 @@
 package edu.csus.ecs.pc2.api.exceptions;
 
 /**
- * Exception when login failure occurs.
+ * This class defines the Exception which is thrown when a failure occurs during
+ * a client's attempt to connect to a PC<sup>2</sup> server through the PC<sup>2</sup> API.
+ * <P>
+ * There are many conditions which can cause connection/login failure, including but not limited
+ * to 
+ * <ul>
+ *      <li> lack of proper configuration information identifying the network connection to
+ *              be used in contacting the server; </li>
+ *      <li> inability of the underlying PC<sup>2</sup> transport
+ *              mechanism to accomplish the network connection; </li>
+ *      <li> incorrect authentication (login name/password) credentials. </li>
+ * </ul>
+ * <P>
+ * Information regarding the specific type of failure is accessible using the standard
+ * <code>getMessage()</code> method associated with the Exception. 
  * 
- * Use getMesssage() to find the why the login failed.
  * 
+ * <p>
+ * This documentation describes the current <I>draft</i> of the PC<sup>2</sup> API, which is subject to change.
+ *
  * @author pc2@ecs.csus.edu
  * @version $Id$
  */
