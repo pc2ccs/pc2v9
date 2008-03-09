58e6d973f97427ff4cb7cc93a832390d6105a2b7
Douglas Lane <laned@ecs.csus.edu>
2008-03-09 04:27:39 +0000

    bug	11: fixed api AllTests Test Suite
    svn path=/trunk/; revision=1348

--- test/edu/csus/ecs/pc2/api/AllTests.java
+++ test/edu/csus/ecs/pc2/api/AllTests.java
@@ -3,7 +3,6 @@ package edu.csus.ecs.pc2.api;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-
 /**
  * Test Suite for api package.
  *  
@@ -12,7 +11,11 @@ import junit.framework.TestSuite;
  */
 
 // $HeadURL$
-public class AllTests {
+public final class AllTests {
+    
+    private AllTests() {
+
+    }
 
     public static Test suite() {
         TestSuite suite = new TestSuite("Test for edu.csus.ecs.pc2.api");
