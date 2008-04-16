03377dad0df8a1d77a5d852656bf1bdd9981620e
Troy Boudreau <boudreat@ecs.csus.edu>
2008-04-16 01:28:09 +0000

    bug 110:  do not allow permissions (login/display) to be modified for administrator 1 (any site).
    svn path=/trunk/; revision=1475

--- src/edu/csus/ecs/pc2/core/imports/LoadAccounts.java
+++ src/edu/csus/ecs/pc2/core/imports/LoadAccounts.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import edu.csus.ecs.pc2.core.exception.IllegalTSVFormatException;
 import edu.csus.ecs.pc2.core.model.Account;
 import edu.csus.ecs.pc2.core.model.ClientId;
+import edu.csus.ecs.pc2.core.model.ClientType;
 import edu.csus.ecs.pc2.core.model.Group;
 import edu.csus.ecs.pc2.core.model.ClientType.Type;
 import edu.csus.ecs.pc2.core.security.Permission;
@@ -96,18 +97,21 @@ public class LoadAccounts {
                 }
             }
         }
-        if (permDisplayColumn != -1 && values.length > permDisplayColumn && values[permDisplayColumn].length() > 0) {
-            if (Boolean.parseBoolean(values[permDisplayColumn])) {
-                account.addPermission(Permission.Type.DISPLAY_ON_SCOREBOARD);
-            } else {
-                account.removePermission(Permission.Type.DISPLAY_ON_SCOREBOARD);
+        // do not allow permission changes for root
+        if (!(clientId.getClientType().equals(ClientType.Type.ADMINISTRATOR) && clientId.getClientNumber() == 1)) {
+            if (permDisplayColumn != -1 && values.length > permDisplayColumn && values[permDisplayColumn].length() > 0) {
+                if (Boolean.parseBoolean(values[permDisplayColumn])) {
+                    account.addPermission(Permission.Type.DISPLAY_ON_SCOREBOARD);
+                } else {
+                    account.removePermission(Permission.Type.DISPLAY_ON_SCOREBOARD);
+                }
             }
-        }
-        if (permLoginColumn != -1 && values.length > permLoginColumn && values[permLoginColumn].length() > 0) {
-            if (Boolean.parseBoolean(values[permLoginColumn])) {
-                account.addPermission(Permission.Type.LOGIN);
-            } else {
-                account.removePermission(Permission.Type.LOGIN);
+            if (permLoginColumn != -1 && values.length > permLoginColumn && values[permLoginColumn].length() > 0) {
+                if (Boolean.parseBoolean(values[permLoginColumn])) {
+                    account.addPermission(Permission.Type.LOGIN);
+                } else {
+                    account.removePermission(Permission.Type.LOGIN);
+                }
             }
         }
         return account;
