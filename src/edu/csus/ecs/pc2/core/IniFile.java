6e2a6e1ab94fcdf8dc7635cb86c36a2b6942cfb6
Troy Boudreau <boudreat@ecs.csus.edu>
2008-12-01 21:14:58 +0000

    bug 327: satisfy parameters for raw types.
    svn path=/trunk/; revision=1699

--- src/edu/csus/ecs/pc2/core/IniFile.java
+++ src/edu/csus/ecs/pc2/core/IniFile.java
@@ -106,7 +106,7 @@ public class IniFile {
     public static void dump(PrintStream ps) {
         ps.println("Dumping: " + getIniFileURL());
         // String dump=new String("Dumping IniFile");
-        Enumeration enumeration = nameValueHash.keys();
+        Enumeration<String> enumeration = nameValueHash.keys();
         Object key;
         while (enumeration.hasMoreElements()) {
             key = enumeration.nextElement();
