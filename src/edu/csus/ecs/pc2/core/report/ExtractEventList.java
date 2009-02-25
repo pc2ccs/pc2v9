72f9cbdcbf30d95ef8a1062077cc3848f3f6f665
Douglas Lane <laned@ecs.csus.edu>
2009-02-25 22:32:54 +0000

    bug 215: replay extract report now extracts file contents too.
    svn path=/trunk/; revision=1784

--- src/edu/csus/ecs/pc2/core/report/ExtractEventList.java
+++ src/edu/csus/ecs/pc2/core/report/ExtractEventList.java
@@ -1,10 +1,12 @@
 package edu.csus.ecs.pc2.core.report;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Vector;
 
 import edu.csus.ecs.pc2.VersionInfo;
 import edu.csus.ecs.pc2.core.IInternalController;
@@ -15,6 +17,7 @@ import edu.csus.ecs.pc2.core.model.Filter;
 import edu.csus.ecs.pc2.core.model.IInternalContest;
 import edu.csus.ecs.pc2.core.model.Run;
 import edu.csus.ecs.pc2.core.model.RunFiles;
+import edu.csus.ecs.pc2.core.model.SerializedFile;
 import edu.csus.ecs.pc2.core.model.playback.PlaybackEvent;
 import edu.csus.ecs.pc2.core.model.playback.PlaybackManager;
 
@@ -38,12 +41,19 @@ public class ExtractEventList implements IReport {
     private IInternalContest contest;
 
     private IInternalController controller;
+    
+    private String extractDirectory;
+    
+    private String reportFilename;
 
     private Log log;
 
     private Filter filter;
+    
+    private String reportDirectory = "reports";
 
-    private void writeRow(PrintWriter printWriter, Run run) {
+
+    private void writeRow(PrintWriter printWriter, Run run) throws Exception {
 
         writeValues(printWriter, PlaybackManager.ACTION_KEY, PlaybackEvent.Action.RUN_SUBMIT.toString());
         writeValues(printWriter, PlaybackManager.ID_KEY, run.getNumber());
@@ -56,7 +66,22 @@ public class ExtractEventList implements IReport {
         printWriter.flush();
         
         RunFiles runFiles = contest.getRunFiles(run);
-        writeValues(printWriter, PlaybackManager.MAINFILE_KEY, runFiles.getMainFile().getName());
+        SerializedFile mainfile = runFiles.getMainFile();
+        String mainFileName = mainfile.getName();
+        
+        String targetDirectory = extractDirectory + File.separator + "site" + run.getSiteNumber() + "run" + run.getNumber();
+        if (!new File(targetDirectory).isDirectory()) {
+            new File(targetDirectory).mkdirs();
+        }
+        
+        String outputFileName = targetDirectory + File.separator + mainFileName; 
+        
+        mainfile.writeFile(outputFileName);
+
+        // remove report/ from targetdirectory to make it relative to the load list file (report file)
+        outputFileName = removeUpTo(targetDirectory, File.separator).substring(1) + File.separator + mainFileName;
+        
+        writeValues(printWriter, PlaybackManager.MAINFILE_KEY, outputFileName);
         
         printWriter.println();
     }
@@ -85,15 +110,46 @@ public class ExtractEventList implements IReport {
         printWriter.println("# Created by: "+new VersionInfo().getSystemVersionInfo());
         
         printWriter.println();
+        
+        Vector<String> failedRunExtract = new Vector<String>();
+
+        extractDirectory = removeUpTo(reportFilename, reportDirectory+File.separator) + ".files";
 
         for (Run run : runs) {
-            writeRow(printWriter, run);
+            try {
+                writeRow(printWriter, run);
+            } catch (Exception e) {
+                failedRunExtract.add("Failed run: " + run);
+                printWriter.println();
+                printWriter.println("# error extracting run " + run + " " + e.getMessage());
+            }
+        }
+        
+        String [] list = (String[]) failedRunExtract.toArray(new String[failedRunExtract.size()]);
+        
+        printWriter.println();
+        printWriter.println("# "+list.length+" errors extracting runs");
+        
+        for (String s : list){
+            printWriter.println("# "+s);
         }
         
         printWriter.println();
         printWriter.println("# EOF "+getReportTitle()); 
         printWriter.println("------------------------------------------------------------");
 
+    }
+
+    protected String removeUpTo(String source, String targetString) {
+
+        int index = source.indexOf(targetString);
+
+        if (index > 0) {
+            String debug2 = source.substring(index);
+            return debug2;
+        } else {
+            return source;
+        }
     }
 
     private void printHeader(PrintWriter printWriter) {
@@ -110,8 +166,10 @@ public class ExtractEventList implements IReport {
     }
 
     public void createReportFile(String filename, Filter inFilter) throws IOException {
+        
+        reportFilename = filename;
 
-        PrintWriter printWriter = new PrintWriter(new FileOutputStream(filename, false), true);
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream(reportFilename, false), true);
 
         try {
             printHeader(printWriter);
