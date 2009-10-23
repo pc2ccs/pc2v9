b3f6b5d004933e5fe950762bbe7ba6b5c4d37bda
Samir E. Ashoo <ashoos@ecs.csus.edu>
2009-10-23 21:37:36 +0000

    bug 451: updated scripts to handle EXITCODE.TXT
    svn path=/trunk/; revision=1915

--- samps/validiff.bat
+++ samps/validiff.bat
@@ -1,19 +1,47 @@
-@echo off
-rem 
-rem  File:    validdiff
-rem  Purpose: to show gvim diff and also execute internal PC^2 Validator
-rem 
-rem  $HeadURL$
-rem  $Id$
-rem 
-rem  Directions
-rem  Add/Edit Problem | Validator Tab 
-rem  Select Use External Validator
-rem  Validator Command Line must be: {:infile} {:outfile} {:ansfile} {:resfile} 
-rem  
+@ECHO OFF
+REM
+REM
+REM File: validiff.bat
+REM Purpose: to show gvim diff and also execute internal PC^2 Validator
+REM
+REM
+REM $HeadURL$
+REM $Id$
+REM Directions
+REM Add/Edit Problem | Validator Tab 
+REM Select Use External Validator
+REM Validator Command Line must be: {:infile} {:outfile} {:ansfile} {:resfile} 
+REM 
 
-java -cp /usr/pc2v9/lib/pc2.jar edu.csus.ecs.pc2.validator.Validator $* -pc2 1 true
 
-gvim -d $2 $3 > NUL
+IF %4. == . GOTO :NO_PARAMS
 
-rem  eof validiff
+IF EXIST EXITCODE.TXT GOTO :RUNTIME
+GOTO :VALIDATE
+
+
+:RUNTIME
+
+ECHO ^<?xml version="1.0"?^> > %4
+ECHO ^<result outcome =  "No - Run-time Error" security = "%4"^>Team execution exited non-zero, this is a possible run-time error >> %4
+ECHO ^</result^> >> %4
+
+ECHO. >> %2
+ECHO PC2: Team program exit code below >> %2
+TYPE EXITCODE.TXT >> %2
+
+GOTO :END
+
+:VALIDATE
+
+java -cp C:\PC2\LIB\pc2.jar edu.csus.ecs.pc2.validator.Validator %1 %2 %3 %4 -pc2 1 true
+
+IF NOT EXIST C:\WORK\COMPUTER gvim -d %2 %3
+
+GOTO :END
+
+:NO_PARAMS
+ECHO MISSING NECESSARY PARAMTERS
+GOTO :END
+
+:END
