707e37e1ea226664609ca917a2d487e258045f73
Douglas Lane <laned@ecs.csus.edu>
2009-10-23 21:42:42 +0000

    bug 70: add ProfileReport class
    svn path=/trunk/; revision=1916

---
+++ src/edu/csus/ecs/pc2/core/report/ProfileReport.java
@@ -0,0 +1,119 @@
+package edu.csus.ecs.pc2.core.report;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import edu.csus.ecs.pc2.VersionInfo;
+import edu.csus.ecs.pc2.core.IInternalController;
+import edu.csus.ecs.pc2.core.Utilities;
+import edu.csus.ecs.pc2.core.log.Log;
+import edu.csus.ecs.pc2.core.model.Filter;
+import edu.csus.ecs.pc2.core.model.IInternalContest;
+import edu.csus.ecs.pc2.core.model.Profile;
+
+/**
+ * Print list of clarifications.
+ * 
+ * @author pc2@ecs.csus.edu
+ * @version $Id$
+ */
+
+// $HeadURL$
+public class ProfileReport implements IReport {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 808321237990590312L;
+
+    private IInternalContest contest;
+
+    private IInternalController controller;
+
+    private Log log;
+
+    private Filter filter = new Filter();
+
+    public void writeReport(PrintWriter printWriter) {
+
+        Profile profile = contest.getProfile();
+
+        printWriter.println();
+        printWriter.println("Profile name: " + profile.getName());
+        printWriter.println("  contest id = " + profile.getContestId());
+        printWriter.println("  descripton = " + profile.getDescription());
+
+    }
+
+    private void printHeader(PrintWriter printWriter) {
+        printWriter.println(new VersionInfo().getSystemName());
+        printWriter.println("Date: " + Utilities.getL10nDateTime());
+        printWriter.println(new VersionInfo().getSystemVersionInfo());
+        printWriter.println();
+        printWriter.println(getReportTitle() + " Report");
+        
+        printWriter.println();
+        printWriter.println("Contest date/time: "+ contest.getContestTime().getResumeTime().getTime());
+    }
+
+    private void printFooter(PrintWriter printWriter) {
+        printWriter.println();
+        printWriter.println("end report");
+    }
+
+    public void createReportFile(String filename, Filter inFilter) throws IOException {
+
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream(filename, false), true);
+
+        try {
+            printHeader(printWriter);
+
+            try {
+                writeReport(printWriter);
+            } catch (Exception e) {
+                printWriter.println("Exception in report: " + e.getMessage());
+                e.printStackTrace(printWriter);
+            }
+
+            printFooter(printWriter);
+
+            printWriter.close();
+            printWriter = null;
+
+        } catch (Exception e) {
+            log.log(Log.INFO, "Exception writing report", e);
+        }
+    }
+
+    public String[] createReport(Filter inFilter) {
+        throw new SecurityException("Not implemented");
+    }
+
+    public String createReportXML(Filter inFilter) {
+        throw new SecurityException("Not implemented");
+    }
+
+    public String getReportTitle() {
+        return "Profile";
+    }
+
+    public void setContestAndController(IInternalContest inContest, IInternalController inController) {
+        this.contest = inContest;
+        this.controller = inController;
+        log = controller.getLog();
+    }
+
+    public String getPluginTitle() {
+        return "Profile Report";
+    }
+
+    public Filter getFilter() {
+        return filter;
+    }
+
+    public void setFilter(Filter filter) {
+        this.filter = filter;
+    }
+
+}
