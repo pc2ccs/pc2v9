97a31785eadbbef6688530291ec0bc94a6ff732a
Douglas Lane <laned@ecs.csus.edu>
2012-01-09 00:35:51 +0000

    bug 626: add JUnit test files for new CCS pc2 submit command line logic
    svn path=/trunk/; revision=2455

---
+++ testdata/SubmitterTest/Hello.java
@@ -0,0 +1,26 @@
+//
+
+// $Id$
+
+//
+
+
+
+public class hello {
+
+    public static void main(String[] args) 
+
+    {
+
+         System.out.println("Hello World.");
+
+    }
+
+}
+
+
+
+// eof
+
+
+
---
+++ testdata/SubmitterTest/stTestLP/hello.c
@@ -0,0 +1,20 @@
+/*
+ * File:    hello.c 
+ * Purpose: prints: Hello World.
+ * Author:  pc2@ecs.csus.edu or http://www.ecs.csus.edu/pc2
+ *
+ * $Id$
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+        printf("Hello World.\n");
+        
+        exit(0);
+}
+
+/* eof */
