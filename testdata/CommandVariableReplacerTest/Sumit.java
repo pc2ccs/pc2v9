31721227fa612b93b2477c3e6eb4a597ac640935
Douglas Lane <laned@ecs.csus.edu>
2012-04-21 16:26:44 +0000

    bug 695: added test data for replacer
    svn path=/trunk/; revision=2541

---
+++ testdata/CommandVariableReplacerTest/Sumit.java
@@ -0,0 +1,28 @@
+public class Sumit {
+
+    public void doSum(String filename) {
+        try {
+            RandomAccessFile file = new RandomAccessFile(filename, "r");
+            String line;
+            int sum = 0;
+            int rv = 0;
+            while ((line = file.readLine()) != null) {
+                rv = new Integer(line.trim()).intValue();
+                if (rv > 0) {
+                    sum = sum + rv;
+                    // System.out.println(line);
+                }
+            }
+            System.out.print("The sum of the integers is " + sum);
+        } catch (Exception e) {
+            System.out.println("Possible trouble reading Sumit.dat");
+        }
+    }
+
+    public static void main(String[] args) {
+
+       new Sumit().doSum("+inputfilename+"); 
+
+    }
+}
+
