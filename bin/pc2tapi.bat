c46084d00a625a970b00eb055a742a32a2f3420b
Douglas Lane <laned@ecs.csus.edu>
2012-10-09 04:10:51 +0000

    bug 713: add scripts to run API test frame
    svn path=/trunk/; revision=2578

---
+++ bin/pc2tapi.bat
@@ -0,0 +1,33 @@
+@echo off
+
+rem Purpose: start pc2 test API Frame
+rem Author : pc2@ecs.csus.edu
+rem $HeadURL$
+
+rem Windows 2000 and beyond syntax
+set PC2BIN=%~dp0
+if exist %PC2BIN%\pc2env.bat goto :continue
+
+rem fallback to path (or current directory)
+set PC2BIN=%0\..
+if exist %PC2BIN%\pc2env.bat goto :continue
+
+rem else rely on PC2INSTALL variable
+set PC2BIN=%PC2INSTALL%\bin
+if exist %PC2BIN%\pc2env.bat goto :continue
+
+echo.
+echo ERROR: Could not locate scripts.
+echo.
+echo Please set the variable PC2INSTALL to the location of
+echo   the VERSION file (ex: c:\pc2-9.0.0)
+echo.
+pause
+goto :end
+
+:continue
+call %PC2BIN%\pc2env.bat
+java -Xms64M -Xmx768M -cp %pc2_classpath% edu.csus.ecs.pc2.api.ContestTestFrame %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:end
+rem eof pc2tapi.bat $Id$
