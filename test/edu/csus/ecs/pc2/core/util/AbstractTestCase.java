651084694e50b58333cec8a44e0923819e355f45
Troy Boudreau <boudreat@ecs.csus.edu>
2013-02-06 03:47:20 +0000

    bug 636: fix HELLO_SOURCE_FILENAME "Hello.java" to "hello.java" (eg how it is in the samps/src directory).
    svn path=/trunk/; revision=2607

--- test/edu/csus/ecs/pc2/core/util/AbstractTestCase.java
+++ test/edu/csus/ecs/pc2/core/util/AbstractTestCase.java
@@ -57,7 +57,8 @@ public class AbstractTestCase extends TestCase {
      */
     public static final String DEFAULT_PC2_OUTPUT_FOR_TESTING_DIRECTORY = "testout";
     
-    public static final String HELLO_SOURCE_FILENAME = "Hello.java";
+    // XXX samps/src/hello.java should probably be renamed to Hello.java
+    public static final String HELLO_SOURCE_FILENAME = "hello.java";
     
     public static final String SUMIT_SOURCE_FILENAME = "Sumit.java";
     
