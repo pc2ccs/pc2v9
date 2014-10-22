616a47eaed041730b5f99f9eff47272f0e5ff284
Troy Boudreau <boudreat@ecs.csus.edu>
2014-10-22 04:08:38 +0000

    bug 2: add the version numbers on startup.
    svn path=/trunk/; revision=103

--- EWUTeam/src/ServerInterface.java
+++ EWUTeam/src/ServerInterface.java
@@ -1,10 +1,16 @@
 import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
+import edu.csus.ecs.pc2.VersionInfo;
 import edu.csus.ecs.pc2.api.IClarification;
 import edu.csus.ecs.pc2.api.IClarificationEventListener;
 import edu.csus.ecs.pc2.api.IClient;
@@ -48,6 +54,7 @@ public class ServerInterface {
 	private int cur_clar_num = 0;
 
 	private IStanding[] standingsArray = null;
+    private VersionInfo versionInfo = new VersionInfo();
 	
     public class RunListenerEventImplementation implements IRunEventListener {
 
@@ -130,6 +137,13 @@ public class ServerInterface {
 					+ e.getMessage(), e);
 		}
 		standingsArray = populateStandings();
+	
+		try {
+			System.out.println("Starting JavaBridge version "+getVersionNumber()+"-"+getBuildNumber()+" with PC^2 version "+getVersionNumberforPC2()+"-"+getBuildNumberForPC2());
+		} catch (IOException e) {
+			System.out.println("Trouble reading the version and build numbers "+e.getMessage());
+		}
+		System.out.println("JavaBridge is ready");
 	}
 
 	private IStanding[] populateStandings() {
@@ -587,5 +601,66 @@ public class ServerInterface {
 		return null;
 	}
 
+    /**
+     * @return build number for pc2.
+     */
+    public String getBuildNumberForPC2()
+    {
+        return versionInfo.getBuildNumber();
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String getVersionNumberforPC2()
+    {
+        /*
+         * $ cat MANIFEST.MF 
+         * Manifest-Version: 1.0
+         * Ant-Version: Apache Ant 1.7.0
+         * Created-By: 1.5.0_17-b04 (Sun Microsystems Inc.)
+         * Specification-Version: 9.3beta
+         * Implementation-Title: CSUS Programming Contest Control System
+         * Implementation-Version: 2697
+         * Built-On: Wednesday, September 25 2013 03:33 UTC
+         * Built-On-Date: 20130925
+         * Main-Class: edu.csus.ecs.pc2.Starter
+         */
+
+        return versionInfo.getVersionNumber();
+    }
+
+    public String getBuildNumber() throws IOException
+    {
+        return getManifestValue("Implementation-Version");
+    }
+
+    public String getVersionNumber() throws IOException
+    {
+        /*
+         * $ cat MANIFEST.MF 
+         * Manifest-Version: 1.0
+         * Ant-Version: Apache Ant 1.7.0
+         * Created-By: 24.0-b56 (Oracle Corporation)
+         * Main-Class: PC2JavaMiniserver
+         * Specification-Version: 2.0
+         * Implementation-Title: EWU Web Team Client
+         * Implementation-Version: 60
+         * Built-On: Saturday, October 26 2013 06:21 UTC
+         * Class-Path: pc2.jar JavaBridge.jar
+         */
+        return getManifestValue("Specification-Version");
+    }
+
+    public String getManifestValue(String name) throws IOException
+    {
+        URLClassLoader classLoader = (URLClassLoader) getClass().getClassLoader();
+        URL url = classLoader.findResource("META-INF/MANIFEST.MF");
+        Manifest manifest = new Manifest(url.openStream());
+        Attributes attributes = manifest.getMainAttributes();
+        return attributes.getValue(name);
+    }
+    
 }// end class:ServerInterface
 
