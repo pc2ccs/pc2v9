11b26afd3f67637d9ae4cb6f2aa469c0a4cc9c38
Douglas Lane <laned@ecs.csus.edu>
2015-04-04 04:27:01 +0000

    bug 156:  Updated and tested unix scripts
    svn path=/trunk/; revision=114

--- EWUTeam/scripts/ewuzip.bat
+++ EWUTeam/scripts/ewuzip.bat
@@ -28,7 +28,8 @@ goto :end
 :continue
 call %EWUBIN%\ewuenv.bat
 
-java -Xms64M -Xmx768M -cp %libdir%\PC2JavaServer.jar edu.csus.pc2.ewuteam.Zip
+java -Xms64M -Xmx768M -cp %libdir%\PC2JavaServer.jar edu.csus.pc2.ewuteam.Zip %1 %2 %3 %4 %5 %6 %7 %8 %9
+
 
 :end
 rem eof ewuzip.bat $Id$
