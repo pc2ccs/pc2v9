fb65cf1817106478e25b23088156bfc01697dd24
John Clevenger <clevenger@csus.edu>
2015-07-15 01:14:08 +0000

    bug 918: modified constructor to accept RunTestCases instead of raw Objects, making it easier to use the storage provided by the parent DefaultTableModel class.
    svn path=/trunk/; revision=3073

--- src/edu/csus/ecs/pc2/ui/TestCaseResultsTableModel.java
+++ src/edu/csus/ecs/pc2/ui/TestCaseResultsTableModel.java
@@ -2,7 +2,10 @@ package edu.csus.ecs.pc2.ui;
 
 import java.util.Vector;
 
+import javax.swing.JLabel;
 import javax.swing.table.DefaultTableModel;
+
+import edu.csus.ecs.pc2.core.model.RunTestCase;
 
 /**
  * This class defines a table model for tables holding Test Case Results.
@@ -16,13 +19,55 @@ public class TestCaseResultsTableModel extends DefaultTableModel {
     
     /**
      * Constructs a Table Model for Test Case Results. The data and columnName values
-     * are stored in Vectors in the parent class, initialized by a call to
-     * super(data,columnnames) in this constructor.
-     * @param data - the data to go in the model
+     * are stored in Vectors in the DefaultTableModel parent class.
+     * @param testCases - the data to go in the model
      * @param columnNames - Strings defining the table column headers
      */
-    public TestCaseResultsTableModel(Object[][] data, Object[] columnNames) {
-        super(data,columnNames);
+    public TestCaseResultsTableModel(RunTestCase[] testCases, Object[] columnNames) {
+        
+        //create a default table model with zero rows and columns
+        super ();
+        
+        //add the column names to the model
+        for (int col=0; col<columnNames.length; col++) {
+            super.setColumnIdentifiers(columnNames);
+        }
+        
+        //add the row data for each test case to the model
+        for (int row=0; row< testCases.length; row++) {
+            
+            //selection checkbox state
+            Boolean selected = new Boolean (!testCases[row].isPassed());
+            
+            //test case number (row+1)
+            String testCaseNum = new String(Integer.toString(row+1));
+            
+            //test case result (passed/failed)
+            boolean result = testCases[row].isPassed();
+            JLabel resultLabel = new JLabel(result?"Pass":"Fail");
+            
+            //elapsed time of test case
+            String time = new String(Long.toString(testCases[row].getElapsedMS()));
+            
+            //link for viewing team output
+            JLabel teamOutputViewLabel = new JLabel("View");
+            
+            //link for comparing team output with corresponding judge's output
+            JLabel teamOutputCompareLabel = new JLabel("Compare");
+            
+            //link for viewing judge's output
+            JLabel judgesOutputViewLabel = new JLabel("View");
+            
+            JLabel judgesDataViewLabel = new JLabel("View");
+            
+            //build the row object and add it to the model
+            Object [] rowData = new Object [] {selected, testCaseNum, resultLabel, time, 
+                    teamOutputViewLabel, teamOutputCompareLabel, judgesOutputViewLabel, 
+                    judgesDataViewLabel };
+
+            super.addRow(rowData);
+        }
+
     }
     
     /**
