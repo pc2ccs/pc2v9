aa90ac4bd7b029b1c118e5b693527af0d96ea8b2
Troy Boudreau <boudreat@ecs.csus.edu>
2015-09-22 20:39:11 +0000

    bug 636: C# sample (tested with mono 2.0)
    svn path=/trunk/; revision=3159

---
+++ samps/src/hello.cs
@@ -0,0 +1,17 @@
+/*
+ *
+ * File:    hello.php
+ * Purpose: prints Hello World.
+ * pc2@ecs.csus.edu at http://www.ecs.csus.edu/pc2
+ *
+ * $Id$
+*/
+using System;
+ 
+public class HelloWorld
+{
+    static public void Main ()
+    {
+        Console.WriteLine ("Hello World.");
+    }
+}
