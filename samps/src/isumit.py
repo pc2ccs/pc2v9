7317bf82248d1c338cdd17ee4faae9863519a22f
Troy Boudreau <boudreat@ecs.csus.edu>
2015-10-17 00:09:01 +0000

    bug 636: add isumit samples for cs & python2/3.  Expand keywords Id on sumit.py
    svn path=/trunk/; revision=3209

--- samps/src/isumit.py
+++ samps/src/isumit.py
@@ -1,11 +1,9 @@
 #
-# File:    sumit.py
-# Purpose: to sum the integers in the file sumit.dat
+# File:    isumit.py
+# Purpose: to sum the integers from stdin
 # Author:  pc2@ecs.csus.edu or http://www.ecs.csus.edu/pc2
 #
-# Sat Oct 30 13:20:59 PDT 1999
-#
-# $Id$
+# $Id: sumit.pl 326 2006-10-25 02:53:57Z laned $
 #
 
 import sys
@@ -13,9 +11,7 @@ import sys
 # softspace required to avoid extra space after print
 # sys.stdout.softspace=0
 
-datafile = "sumit.dat"
-
-hand = open (datafile, "r")
+hand = sys.stdin
 
 sum = 0
 
@@ -24,7 +20,7 @@ for line in hand .readlines():
   if num > 0:
     sum += num
 
-print "The sum of the integers is", sum
+print("The sum of the integers is %s"% sum)
 
-# eof sumit.py $Id$
+# eof sumit.py $Id: hello.py 2323 2011-09-11 02:02:34Z laned $
 
