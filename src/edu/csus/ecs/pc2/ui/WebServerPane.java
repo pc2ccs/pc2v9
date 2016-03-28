c74244e56bc2071b96c96a0835580a0b413a394b
Troy Boudreau <boudreat@ecs.csus.edu>
2016-03-27 20:27:42 -0700

    bug 599: source code formatting only
    attempt to cleanup stuff checkstyle is complaining about.

--- src/edu/csus/ecs/pc2/ui/WebServerPane.java
+++ src/edu/csus/ecs/pc2/ui/WebServerPane.java
@@ -43,12 +43,9 @@ import edu.csus.ecs.pc2.services.web.TeamService;
 
 import javax.swing.JCheckBox;
 
-
 /**
- * This class provides a GUI for configuring the embedded Jetty webserver.
- * It allows specifying the port on which Jetty will listen and the REST service
- * endpoints to which Jetty will respond.  (Note that REST endpoints are handled
- * using Jersey, the JAX-RS implementation.)
+ * This class provides a GUI for configuring the embedded Jetty webserver. It allows specifying the port on which Jetty will listen and the REST service endpoints to which Jetty will respond. (Note
+ * that REST endpoints are handled using Jersey, the JAX-RS implementation.)
  * 
  * By default the Jetty webserver is configured to listen on port 50080.
  * 
@@ -58,7 +55,6 @@ import javax.swing.JCheckBox;
 
 // $HeadURL$
 public class WebServerPane extends JPanePlugin {
-
 
     private static final long serialVersionUID = 1L;
 
@@ -75,15 +71,21 @@ public class WebServerPane extends JPanePlugin {
     private JLabel portLabel = null;
 
     private JTextField portTextField = null;
- 
-    private Server jettyServer = null ;
+
+    private Server jettyServer = null;
 
     private JLabel webServerStatusLabel = null;
+
     private JCheckBox chckbxScoreboard;
+
     private JLabel lblEnabledWebServices;
+
     private JCheckBox chckbxProblems;
+
     private JCheckBox chckbxLanguages;
+
     private JCheckBox chckbxStarttime;
+
     private JCheckBox chckbxTeams;
 
     /**
@@ -107,7 +109,7 @@ public class WebServerPane extends JPanePlugin {
 
         updateGUI();
     }
-    
+
     @Override
     public String getPluginTitle() {
         return "Web Server Pane";
@@ -152,13 +154,12 @@ public class WebServerPane extends JPanePlugin {
     }
 
     /**
-     * Starts a Jetty webserver running on the port specified in the GUI textfield, and registers
-     * a set of default REST (Jersey/JAX-RS) services with Jetty.
-     * TODO:  need to provide support for dynamically reconfiguring the registered services.
+     * Starts a Jetty webserver running on the port specified in the GUI textfield, and registers a set of default REST (Jersey/JAX-RS) services with Jetty. TODO: need to provide support for
+     * dynamically reconfiguring the registered services.
      * 
      */
     private void startWebServer() {
-        
+
         if (portTextField.getText() == null) {
             showMessage("You must enter a port number");
             return;
@@ -178,12 +179,8 @@ public class WebServerPane extends JPanePlugin {
                 char[] password = "i don't care".toCharArray();
                 ks.load(null, password);
                 /**
-                 * TODO: 
-                 *  create a self signed cert and add it to the keystore
-                 *  with alias of jetty, replacing command line of
-                 * keytool -keystore cacert.pc2 -alias jetty -genkey -keyalg RSA -sigalg SHA256withRSA
-                 * something like this will do:
-                 * CN=pc2 jetty, OU=PC^2, O=PC^2, L=Unknown, ST=Unknown, C=Unknown
+                 * TODO: create a self signed cert and add it to the keystore with alias of jetty, replacing command line of keytool -keystore cacert.pc2 -alias jetty -genkey -keyalg RSA -sigalg
+                 * SHA256withRSA something like this will do: CN=pc2 jetty, OU=PC^2, O=PC^2, L=Unknown, ST=Unknown, C=Unknown
                  */
 
                 // Store away the keystore
@@ -191,7 +188,7 @@ public class WebServerPane extends JPanePlugin {
                 ks.store(fos, password);
                 fos.close();
             }
-            
+
             ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
             context.setContextPath("/");
 
@@ -199,7 +196,7 @@ public class WebServerPane extends JPanePlugin {
             context.addServlet(new ServletHolder(new ServletContainer(getResourceConfig())), "/*");
 
             jettyServer = new Server();
-            
+
             HttpConfiguration http_config = new HttpConfiguration();
             http_config.setSecureScheme("https");
             http_config.setSecurePort(port);
@@ -225,75 +222,69 @@ public class WebServerPane extends JPanePlugin {
 
             // only enable https
             jettyServer.setConnectors(new Connector[] { https });
-                
+
             jettyServer.setHandler(context);
 
-//            ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
-//            jerseyServlet.setInitOrder(0);
-//
-//            // Tells the Jersey Servlet which REST service/classes to load.
-//            //  Note that class names must be semi-colon separated in the second String parameter.
-//            jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", getServiceClassesList());
+            // ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
+            // jerseyServlet.setInitOrder(0);
+            //
+            // // Tells the Jersey Servlet which REST service/classes to load.
+            // // Note that class names must be semi-colon separated in the second String parameter.
+            // jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", getServiceClassesList());
 
             jettyServer.start();
-            
-        } 
-        catch (NumberFormatException e) {
-            showMessage("Unable to start web services: invalid port number: "+ e.getMessage());
-            e.printStackTrace(); 
+
+        } catch (NumberFormatException e) {
+            showMessage("Unable to start web services: invalid port number: " + e.getMessage());
+            e.printStackTrace();
             getLog().log(Log.INFO, e.getMessage(), e);
-        }
-        catch (IOException e1) {
-            showMessage("Unable to start web services: "+ e1.getMessage());
-            e1.printStackTrace(); 
+        } catch (IOException e1) {
+            showMessage("Unable to start web services: " + e1.getMessage());
+            e1.printStackTrace();
             getLog().log(Log.INFO, e1.getMessage(), e1);
-        }
-        catch (Exception e2) {
-            showMessage("Unable to start web services: "+ e2.getMessage());
-            e2.printStackTrace(); 
+        } catch (Exception e2) {
+            showMessage("Unable to start web services: " + e2.getMessage());
+            e2.printStackTrace();
             getLog().log(Log.INFO, e2.getMessage(), e2);
-  
+
         }
 
         updateGUI();
     }
-    
-    
+
     /**
-     * This method constructs a Jersey {@link ResourceConfig} containing a Resource (Service class)
-     * for each REST service marked as "enabled" by the user on the WebServerPane GUI.
-     * Each Resource is constructed with the current contest and controller so that it has access to
-     * the contest data.
+     * This method constructs a Jersey {@link ResourceConfig} containing a Resource (Service class) for each REST service marked as "enabled" by the user on the WebServerPane GUI. Each Resource is
+     * constructed with the current contest and controller so that it has access to the contest data.
      * 
      * @return a ResourceConfig containing the enabled REST service resources
      */
     private ResourceConfig getResourceConfig() {
-        
-        //create and (empty) ResourceConfig
+
+        // create and (empty) ResourceConfig
         ResourceConfig resConfig = new ResourceConfig();
-        
-        //add each of the enabled services to the config:
-        
+
+        // add each of the enabled services to the config:
+
         if (getChckbxScoreboard().isSelected()) {
             resConfig.register(new ScoreboardService(getContest(), getController()));
         }
-        
+
         if (getChckbxProblems().isSelected()) {
             resConfig.register(new ProblemService(getContest(), getController()));
         }
-        
+
         if (getChckbxLanguages().isSelected()) {
             resConfig.register(new LanguageService(getContest(), getController()));
         }
-        
+
         if (getChckbxStarttime().isSelected()) {
             resConfig.register(new StarttimeService(getContest(), getController()));
         }
-        
+
         if (getChckbxTeams().isSelected()) {
             resConfig.register(new TeamService(getContest(), getController()));
         }
-        
+
         return resConfig;
     }
 
@@ -322,11 +313,8 @@ public class WebServerPane extends JPanePlugin {
     }
 
     /**
-     * Stops the Jetty web server if it is running.
-     * Also destroys the web server.
-     * TODO:  shouldn't really destroy the webserver; just stop it and cache the reference so
-     * that it can be quickly restarted.  (However, need to consider what happens if the user
-     * selects a different set of services to be enabled...)
+     * Stops the Jetty web server if it is running. Also destroys the web server. TODO: shouldn't really destroy the webserver; just stop it and cache the reference so that it can be quickly
+     * restarted. (However, need to consider what happens if the user selects a different set of services to be enabled...)
      */
     protected void stopWebServer() {
 
@@ -352,10 +340,10 @@ public class WebServerPane extends JPanePlugin {
         if (centerPanel == null) {
             centerPanel = new JPanel();
             GridBagLayout gblCenterPanel = new GridBagLayout();
-            gblCenterPanel.columnWidths = new int[]{198, 57, 167, 0};
-            gblCenterPanel.rowHeights = new int[]{36, 23, 32, 23, 0, 0, 0};
-            gblCenterPanel.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-            gblCenterPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+            gblCenterPanel.columnWidths = new int[] { 198, 57, 167, 0 };
+            gblCenterPanel.rowHeights = new int[] { 36, 23, 32, 23, 0, 0, 0 };
+            gblCenterPanel.columnWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
+            gblCenterPanel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
             centerPanel.setLayout(gblCenterPanel);
             webServerStatusLabel = new JLabel();
             webServerStatusLabel.setFont(new Font("Dialog", Font.BOLD, 14));
@@ -440,22 +428,21 @@ public class WebServerPane extends JPanePlugin {
     }
 
     /**
-     * Updates the state of the web server status label and Start/Stop buttons to 
-     * correspond to the state of the Jetty Server.
+     * Updates the state of the web server status label and Start/Stop buttons to correspond to the state of the Jetty Server.
      */
-    private void updateGUI () {
-        
+    private void updateGUI() {
+
         boolean serverRunning;
-        if (jettyServer==null) {
+        if (jettyServer == null) {
             serverRunning = false;
         } else {
             serverRunning = jettyServer.isRunning();
         }
-        getStartButton().setEnabled(! serverRunning);
+        getStartButton().setEnabled(!serverRunning);
         getStopButton().setEnabled(serverRunning);
         updateWebServerSettings(serverRunning);
 
-        if (serverRunning){
+        if (serverRunning) {
             webServerStatusLabel.setText("Web Server is running...");
         } else {
             webServerStatusLabel.setText("Web Server STOPPED");
@@ -474,50 +461,55 @@ public class WebServerPane extends JPanePlugin {
 
     private JCheckBox getChckbxScoreboard() {
         if (chckbxScoreboard == null) {
-        	chckbxScoreboard = new JCheckBox("/Scoreboard");
-        	chckbxScoreboard.setSelected(true);
-        	chckbxScoreboard.setHorizontalAlignment(SwingConstants.LEFT);
-        	chckbxScoreboard.setToolTipText("Enable getting contest scoreboard");
+            chckbxScoreboard = new JCheckBox("/Scoreboard");
+            chckbxScoreboard.setSelected(true);
+            chckbxScoreboard.setHorizontalAlignment(SwingConstants.LEFT);
+            chckbxScoreboard.setToolTipText("Enable getting contest scoreboard");
         }
         return chckbxScoreboard;
     }
+
     private JLabel getLblEnabledWebServices() {
         if (lblEnabledWebServices == null) {
-        	lblEnabledWebServices = new JLabel("Enable Web Services:");
+            lblEnabledWebServices = new JLabel("Enable Web Services:");
         }
         return lblEnabledWebServices;
     }
+
     private JCheckBox getChckbxProblems() {
         if (chckbxProblems == null) {
-        	chckbxProblems = new JCheckBox("/Problems");
-        	chckbxProblems.setSelected(true);
-        	chckbxProblems.setToolTipText("Enable getting contest problems");
-        	chckbxProblems.setHorizontalAlignment(SwingConstants.LEFT);
+            chckbxProblems = new JCheckBox("/Problems");
+            chckbxProblems.setSelected(true);
+            chckbxProblems.setToolTipText("Enable getting contest problems");
+            chckbxProblems.setHorizontalAlignment(SwingConstants.LEFT);
         }
         return chckbxProblems;
     }
+
     private JCheckBox getChckbxLanguages() {
         if (chckbxLanguages == null) {
-        	chckbxLanguages = new JCheckBox("/Languages");
-        	chckbxLanguages.setSelected(true);
-        	chckbxLanguages.setHorizontalAlignment(SwingConstants.LEFT);
-        	chckbxLanguages.setToolTipText("Enable getting contest languages");
+            chckbxLanguages = new JCheckBox("/Languages");
+            chckbxLanguages.setSelected(true);
+            chckbxLanguages.setHorizontalAlignment(SwingConstants.LEFT);
+            chckbxLanguages.setToolTipText("Enable getting contest languages");
         }
         return chckbxLanguages;
     }
+
     private JCheckBox getChckbxStarttime() {
         if (chckbxStarttime == null) {
-        	chckbxStarttime = new JCheckBox("/Starttime");
-        	chckbxStarttime.setSelected(true);
+            chckbxStarttime = new JCheckBox("/Starttime");
+            chckbxStarttime.setSelected(true);
         }
         return chckbxStarttime;
     }
+
     private JCheckBox getChckbxTeams() {
         if (chckbxTeams == null) {
-        	chckbxTeams = new JCheckBox("/Teams");
-        	chckbxTeams.setToolTipText("Enable getting contest teams");
-        	chckbxTeams.setSelected(true);
-        	chckbxTeams.setHorizontalAlignment(SwingConstants.LEFT);
+            chckbxTeams = new JCheckBox("/Teams");
+            chckbxTeams.setToolTipText("Enable getting contest teams");
+            chckbxTeams.setSelected(true);
+            chckbxTeams.setHorizontalAlignment(SwingConstants.LEFT);
         }
         return chckbxTeams;
     }
