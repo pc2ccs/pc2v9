7409c771a2060e99539543180801fc829efd3abb
john <clevenger@csus.edu>
2016-05-30 10:49:03 -0700

    bug 800: suppressed "setVisible()" in SubmitClarificationPane and SubmitRunPane setContestAndController().
    According to https://bugs.openjdk.java.net/browse/JDK-6191978,
    components within JTabbedPanes should not have their setVisible()
    methods called directly (rather, the Tab should be activated via
    setSelectedTab()).  This is what was causing the "bleed-through" bug in
    Team and Judge GUIs.

--- src/edu/csus/ecs/pc2/ui/SubmitRunPane.java
+++ src/edu/csus/ecs/pc2/ui/SubmitRunPane.java
@@ -707,7 +707,7 @@ public class SubmitRunPane extends JPanePlugin {
                 populateGUI();
                 updateGUIperPermissions();
                 
-                setVisible(true);
+//                setVisible(true);
             }
         });
 
