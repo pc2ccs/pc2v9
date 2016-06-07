929f9141aa394bc5e656f4a3eaa5a30f2928d9ec
john <clevenger@csus.edu>
2016-06-07 09:26:17 -0700

    bug 1052: added support for /language service to match the CLI spec at https://clics.ecs.baylor.edu/index.php/Draft_CCS_REST_interface#GET_baseurl.2Flanguages. Tested with zero, 1, 2 and 3 languages; verified output matches spec.
    Note: the spec says the ID value for each language is to be "an
    integer".  The implementation uses the Long value which is part of the
    toString() of the ElementID for the language as this integer.  Since the
    value of a Long is an integer, this technically meets the spec...
    although I'm betting there are some who will argue that an integer must
    fit into an "int" (which I would argue is incorrect).

--- src/edu/csus/ecs/pc2/services/web/LanguageService.java
+++ src/edu/csus/ecs/pc2/services/web/LanguageService.java
@@ -29,7 +29,9 @@ public class LanguageService {
     }
 
     /**
-     * This method returns a String representation of the current contest languages in JSON format. The returned string is a JSON array with one language description per array element.
+     * This method returns a String representation of the current contest languages in JSON format. 
+     * The returned string is a JSON array with one language description per array element, matching the
+     * description at {@link https://clics.ecs.baylor.edu/index.php/Draft_CCS_REST_interface#GET_baseurl.2Flanguages}.
      * 
      * @return a String containing the contest languages in JSON form
      */
@@ -40,23 +42,61 @@ public class LanguageService {
         // get the problems from the contest
         Language[] languages = contest.getLanguages();
 
-        // map the language descriptions into JSON form
+        // get an object to map the language descriptions into JSON form
         ObjectMapper mapper = new ObjectMapper();
-        String jsonLanguages = "{" + "\"languages\"" + ":" + "\"empty\"" + "}";
+        
+        //the spec defines that the return value is an array ( [] in JSON)
+        String jsonOutput = "[";  
+        
         try {
-            jsonLanguages = mapper.writeValueAsString(languages);
+            boolean addedOne = false;
+            
+            //for each defined language
+            for (Language lang : languages) {
+
+                //if we've already added one or more langs, add a comma separator
+                if (addedOne) {
+                    jsonOutput += "," ;
+                }
+                
+                //add the language-object opening bracket
+                jsonOutput += "{";
+                
+                //add the ID of the language to the output JSON.  The spec requires the ID to be an integer; it is
+                //based on the fact that the toString() for an ElementID is assumed to be of the form "someString-someUniqueNumber"
+                String idString = lang.getElementId().toString();
+                Long id = new Long(idString.substring(idString.lastIndexOf('-')+1));
+                jsonOutput += "\"id\":" + mapper.writeValueAsString(id) + ",";
+                
+                //add the language name to the output JSON
+                String name = lang.getDisplayName();
+                jsonOutput += "\"name\":\"" + name + "\"";
+                
+                //add the language-object closing bracket
+                jsonOutput += "}";
+                
+                addedOne = true;
+            }
+
         } catch (JsonProcessingException e) {
             // TODO: log exception
+            System.err.println ("LanguageService: error: " + e.getMessage());
+            e.printStackTrace();
+            // TODO: return HTTP error
+        } catch (Exception e) {
+            // TODO: log exception
+            System.err.println ("LanguageService: error: " + e.getMessage());
             e.printStackTrace();
             // TODO: return HTTP error
         }
+        jsonOutput += "]";
 
         // TODO: figure out how to set the Response to "OK" (or whether this is necessary)
         // return Response.status(Response.Status.OK).build();
 
         // output the response to the requester (note that this actually returns it to Jersey,
         // which forwards it to the caller as the HTTP response).
-        return jsonLanguages;
+        return jsonOutput;
 
     }
 }
