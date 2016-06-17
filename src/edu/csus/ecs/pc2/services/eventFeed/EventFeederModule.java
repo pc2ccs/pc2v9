60e9220bfe6b34989a11821b28132f2b7ac59952
Douglas A. Lane <laned@ecs.csus.edu>
2016-06-16 19:58:12 -0700

    bug 1053: add support to start web server with --nogui starts if pc2ws.properties file is present.

--- src/edu/csus/ecs/pc2/services/eventFeed/EventFeederModule.java
+++ src/edu/csus/ecs/pc2/services/eventFeed/EventFeederModule.java
@@ -1,9 +1,12 @@
 package edu.csus.ecs.pc2.services.eventFeed;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Properties;
 
 import edu.csus.ecs.pc2.VersionInfo;
 import edu.csus.ecs.pc2.core.IInternalController;
@@ -25,6 +28,8 @@ public class EventFeederModule implements UIPlugin {
     public static final int DEFAULT_EVENT_FEED_PORT_NUMBER = 4713;
 
     private static final long serialVersionUID = 4459685961401012498L;
+
+    public static final String WEB_SERVICES_PROPERTIES_FILENAME = "pc2ws.properties";
 
     private int port = DEFAULT_EVENT_FEED_PORT_NUMBER;
 
@@ -66,16 +71,40 @@ public class EventFeederModule implements UIPlugin {
             e.printStackTrace(System.err);
             getLog().log(Log.INFO, e.getMessage(), e);
         }
+        
+        if (Utilities.fileExists(WEB_SERVICES_PROPERTIES_FILENAME)) {
+            try {
+                
+                showMessage("Found web server properties file "+WEB_SERVICES_PROPERTIES_FILENAME+" loading...");
+                Properties wsProps = loadPropertiesFile(WEB_SERVICES_PROPERTIES_FILENAME);
+                showMessage("Loaded "+wsProps);
+
+                showMessage("Starting Web Server");
+
+                WebServer server = new WebServer();
+                server.startWebServer(inContest, inController, wsProps);
+            } catch (Exception e) {
+                showMessage("Unable to start web server "+e.getMessage(), e);
+            }
+        } else {
+            showMessage("Note: no web services will be started ");
+        }
     }
 
     protected String getL10nDateTime() {
         DateFormat dateFormatter = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, Locale.getDefault());
         return (dateFormatter.format(new Date()));
     }
+    
+    private void showMessage(final String message, Exception ex) {
+        getLog().log(Log.INFO, message, ex);
+        System.out.println(new Date() + " " +message);
+        ex.printStackTrace();
+    }
 
-    private void showMessage(final String string) {
-        getLog().info(string);
-        System.out.println(string);
+    private void showMessage(String message) {
+        System.out.println(new Date() + " " +message);
+        getLog().info(message);
     }
 
     public Log getLog() {
@@ -92,6 +121,29 @@ public class EventFeederModule implements UIPlugin {
 
     public void setPort(int port) {
         this.port = port;
+    }
+    
+
+    
+    /**
+     * Loads properties from the specified file and returns them in a Properties object.
+     * @param filename - the file to load from
+     * @return a Properties object containing the Properties given in the file
+     */
+
+    public static Properties loadPropertiesFile(String filename) {
+
+        Properties properties = new Properties();
+        try {
+            FileReader reader = new FileReader(new File(filename));
+            properties.load(reader);
+                //logging:
+                System.out.println("Log:  Loaded " + properties.size() + " properties from '" + filename + "'");
+        } catch (Exception e) {
+            ; // exception ignored. No need to report, if exception then fall through.
+        }
+
+        return properties;
     }
 
 }
