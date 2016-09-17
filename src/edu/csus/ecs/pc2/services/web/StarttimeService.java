31fa0f60c17eb9d8be3a2d1761da206825c4c720
John <clevenger@csus.edu>
2016-09-16 21:07:31 -0700

    bug 1055: updated getStarttime() service to return JAX/RS Response objects; started reformatting of putStarttime() service to support setting a new start time.

--- src/edu/csus/ecs/pc2/services/web/StarttimeService.java
+++ src/edu/csus/ecs/pc2/services/web/StarttimeService.java
@@ -8,12 +8,18 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import edu.csus.ecs.pc2.core.IInternalController;
 import edu.csus.ecs.pc2.core.model.IInternalContest;
 
 /**
  * WebService to handle "starttime" REST endpoint as described by the CLICS wiki.
+ * Invoking REST endpoint "getStarttime()" returns the currently configured start time (or "undefined"
+ * if the contest has already been started or if there is no start time currently set).
+ * Invoking REST endpoint "setStarttime()" when the contest has not already been started
+ * sets the contest scheduled start time to either a specified future time or to "undefined".
+ * If setStarttime() is invoked after the contest has started the call is ignored (but an error is returned). 
  * 
  * @author pc2@ecs.csus.edu
  *
@@ -23,10 +29,12 @@ import edu.csus.ecs.pc2.core.model.IInternalContest;
 public class StarttimeService {
 
     private IInternalContest contest;
+    private IInternalController controller;
 
-    public StarttimeService(IInternalContest contest, IInternalController controller) {
+    public StarttimeService(IInternalContest inContest, IInternalController inController) {
         super();
-        this.contest = contest;
+        this.contest = inContest;
+        this.controller = inController;
     }
 
     /**
@@ -34,12 +42,14 @@ public class StarttimeService {
      * received (input) string, which it expects to be in JSON format as described
      * in the CLICS Wiki "StartTime" interface specification.
      * 
-     * @return a String containing the contest starttime
+     * @return a {@link Response} object indicating the status of the setStarttime request
      */
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public String setStartime() {
+    public Response setStartime() {
 
+        //check to insure contest has not already been started; if so, ignore, log, and return some kind of error status
+        
         // from the CLI Wiki Contest_Start_Interface spec:
         // PUT HTTP body is application/json:
         // { "starttime":1265335138.26 }
@@ -58,7 +68,7 @@ public class StarttimeService {
         // which converts it to JSON and forwards that to the caller as the HTTP.response).
         // return Response.status(Response.Status.OK).build();
 
-        return "Start Time <here>";
+        return Response.ok("Start Time <here>").build();
 
     }
 
@@ -66,12 +76,12 @@ public class StarttimeService {
      * This method returns a representation of the current contest scheduled start time in JSON format
      * as described on the CLICS wiki.
      * 
-     * @return a JSON String containing the scheduled contest start time, or "undefined" if no start
-     * time is currently scheduled.
+     * @return a {@link Response} object containing a JSON String giving the scheduled contest start time, 
+     * or the string "undefined" if no start time is currently scheduled (which includes if the contest has already started).
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getStartTime() {
+    public Response getStartTime() {
 
         // from the CLI Wiki Contest_Start_Interface spec:
         // GET HTTP body returns application/json:
@@ -83,10 +93,14 @@ public class StarttimeService {
         Date startDate = contest.getContestInformation().getScheduledStartDate();
         long startTime;
         if (startDate == null) {
+            //there is no start time currently scheduled
             startTime = 0;
         } else {
+            //there IS a scheduled start time set; get it
             startTime = startDate.getTime();
         }
+        
+        //build a string describing the scheduled start time
         String stringTime = "";
         if(startTime == 0) {
             stringTime = "\"undefined\"";
@@ -94,14 +108,12 @@ public class StarttimeService {
             stringTime = new Long(startTime).toString();
         }
 
+        //format the start time as JSON
         String jsonStartTime = "{" + "\"starttime\"" + ":" + stringTime + "}";
 
-        // TODO: figure out how to set the Response to "OK" (or whether this is necessary)
-        // return Response.status(Response.Status.OK).build();
-
-        // output the response to the requester (note that this actually returns it to Jersey,
+        // output the starttime response to the requester (note that this actually returns it to Jersey,
         // which forwards it to the caller as the HTTP response).
-        return jsonStartTime;
+        return Response.ok(jsonStartTime,MediaType.APPLICATION_JSON).build();
 
     }
 
