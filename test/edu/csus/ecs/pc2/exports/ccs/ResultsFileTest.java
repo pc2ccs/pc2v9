b8aeb1c529c183d7a0f200801676eb771d70fe91
Douglas A. Lane <laned@ecs.csus.edu>
2016-11-21 21:34:36 -0800

    bug 1156: add test for rankings
    Add testRankings method to ensure that every
    team is ranked.

--- test/edu/csus/ecs/pc2/exports/ccs/ResultsFileTest.java
+++ test/edu/csus/ecs/pc2/exports/ccs/ResultsFileTest.java
@@ -127,6 +127,128 @@ public class ResultsFileTest extends TestCase {
         compareResults(results, expectedResults);
 
     }
+    
+    /**
+     * Bug 1156 - tests rankings.
+     * 
+     * Each team has its own rank, no ties.
+     */
+    public void testRankings() throws Exception {
+        
+
+        String[] runsData = { //
+        "1,1,A,1,No", // 20 (a No before first yes)
+                "2,1,A,3,Yes", // 3 (first yes counts Minute points but never
+                               // Run Penalty points)
+                "3,1,A,5,No", // zero -- after Yes
+                "4,1,A,7,Yes", // zero -- after Yes
+                "5,1,A,9,No", // zero -- after Yes
+                "6,1,B,11,No", // zero -- not solved
+                "7,1,B,13,No", // zero -- not solved
+                "8,2,A,30,Yes", // 30 (minute points; no Run points on first
+                                // Yes)
+                "9,2,B,35,No", // zero -- not solved
+                "10,2,B,40,No", // zero -- not solved
+                "11,2,B,45,No", // zero -- not solved
+                "12,2,B,50,No", // zero -- not solved
+                "13,2,B,55,No", // zero -- not solved
+                "14,12,B,55,Yes", // solved
+                "15,12,A,55,Yes", // solved
+                "16,5,B,53,Yes", // solved
+                "17,5,A,35,Yes", // solved
+                "18,7,B,55,Yes", // solved
+                "19,7,A,55,Yes", // solved
+                "20,10,B,15,Yes", // solved
+                "21,11,A,25,Yes", // solved
+                "22,12,B,35,Yes", // solved
+                "23,13,A,45,Yes", // solved
+                "24,14,B,55,Yes", // solved
+                "25,15,A,65,Yes", // solved
+                "26,19,B,75,Yes", // solved
+                "27,19,A,85,Yes", // solved
+                
+                "28,9,A,87,Yes", // solved
+                "29,10,A,92,Yes", // solved
+                "30,11,B,101,Yes", // solved
+                "31,12,A,105,Yes", // solved
+                "32,13,A,117,Yes", // solved
+                "33,14,B,123,Yes", // solved
+                "34,15,A,132,Yes", // solved
+                "35,16,A,140,Yes", // solved
+                "36,17,B,141,Yes", // solved
+                "37,18,A,150,Yes", // solved
+                "38,19,A,158,Yes", // solved
+                "39,20,B,164,Yes", // solved
+                "40,1,A,174,Yes", // solved
+                "41,2,A,182,Yes", // solved
+                "42,3,B,189,Yes", // solved
+                "43,4,A,198,Yes", // solved
+                "44,5,A,205,Yes", // solved
+                "45,6,B,208,Yes", // solved
+                "46,7,A,210,Yes", // solved
+                "47,8,A,219,Yes", // solved
+                "48,9,B,227,Yes", // solved
+                "49,10,A,234,Yes", // solved
+                "50,11,A,243,Yes", // solved
+                "51,12,B,250,Yes", // solved
+                "52,13,A,254,Yes", // solved
+
+        };
+
+        // Reservation Id; rank ; medal; solved
+
+        // for medal ranks 4, 8, 12
+        String[] expectedResults = { //
+                "2024;1;Gold Medal;2", // results 
+                "2031;2;Gold Medal;2", // results 
+                "2029;3;Gold Medal;2", // results 
+                "2026;4;Gold Medal;2", // results 
+                "2030;5;Silver Medal;2", // results 
+                "2038;6;Silver Medal;2", // results 
+                "2028;7;Silver Medal;2", // results 
+                "2020;8;Silver Medal;1", // results 
+                "2021;9;Bronze Medal;1", // results 
+                "2032;10;Bronze Medal;1", // results 
+                "2033;11;Bronze Medal;1", // results 
+                "2034;12;Bronze Medal;1", // results 
+                "2035;13;Honorable;1", // results 
+                "2036;14;Honorable;1", // results 
+                "2037;15;Honorable;1", // results 
+                "2039;16;Honorable;1", // results 
+                "2022;17;Honorable;1", // results 
+                "2023;18;Honorable;1", // results 
+                "2025;19;Honorable;1", // results 
+                "2027;20;Honorable;1", // results 
+        };
+
+        ResultsFile resultsFile = new ResultsFile();
+
+        FinalizeData finalizeData = createSampFinalData(1);
+        resultsFile.setFinalizeData(finalizeData);
+
+        int numTeams = 20;
+        SampleContest sample = new SampleContest();
+        IInternalContest contest = sample.createContest(1, 1, numTeams, 12, true);
+
+        SampleContest.assignReservationIds(contest, 2020);
+
+        addRuns(contest, runsData);
+        
+        contest.setFinalizeData(finalizeData);
+
+        String[] results = resultsFile.createTSVFileLines(contest); // using getStandingsRecords
+        // String[] results = resultsFile.createTSVFileLinesTwo(contest); // using XML
+
+        if (debugMode) {
+            dumpStringArray(results);
+
+            // printExpectedTestData(results);
+        }
+        
+        compareResults(results, expectedResults);
+    
+        
+    }
 
     /**
      * Test with 27 or more runs.
@@ -244,7 +366,7 @@ public class ResultsFileTest extends TestCase {
 
         compareResults(results, expectedResults);
     }
-
+    
     @SuppressWarnings("unused")
     private void printExpectedTestData(String[] expected) {
 
