34b4f60ad73e26717b2141a7a7a43dbc93d88afe
john <clevenger@csus.edu>
2016-11-25 12:53:03 -0800

    bug 1159: DefaultValidatorSettings: added a constructor accepting an option string; added toString() method; added constants defining option tokens.

--- src/edu/csus/ecs/pc2/core/model/DefaultValidatorSettings.java
+++ src/edu/csus/ecs/pc2/core/model/DefaultValidatorSettings.java
@@ -8,14 +8,31 @@ package edu.csus.ecs.pc2.core.model;
 
 import java.io.Serializable;
 
+import edu.csus.ecs.pc2.core.log.Log;
+import edu.csus.ecs.pc2.core.log.StaticLog;
+
 /**
  * @author pc2@ecs.csus.edu
  *
  */
-public class DefaultValidatorSettings implements Serializable {
+public class DefaultValidatorSettings implements Serializable, Cloneable {
+
+    static Log log = null;
 
     private static final long serialVersionUID = 1L;
-
+    
+    public static final String VTOKEN_CASE_SENSITIVE = "case_sensitive";
+    public static final String VTOKEN_SPACE_CHANGE_SENSITIVE = "space_change_sensitive";
+    public static final String VTOKEN_FLOAT_RELATIVE_TOLERANCE = "float_relative_tolerance";
+    public static final String VTOKEN_FLOAT_ABSOLUTE_TOLERANCE = "float_absolute_tolerance";
+    
+    public static final boolean DEFAULT_CASE_SENSITIVITY = false;
+    public static final boolean DEFAULT_SPACE_SENSITIVITY = false;
+    public static final boolean DEFAULT_IS_FLOAT_RELATIVE_TOLERANCE_SPECIFIED = false;
+    public static final boolean DEFAULT_IS_FLOAT_ABSOLUTE_TOLERANCE_SPECIFIED = false;
+    public static final double DEFAULT_FLOAT_RELATIVE_TOLERANCE = -1.0;
+    public static final double DEFAULT_FLOAT_ABSOLUTE_TOLERANCE = -1.0;
+    
     private boolean isCaseSensitive;
     
     private boolean isSpaceSensitive;
@@ -24,20 +41,117 @@ public class DefaultValidatorSettings implements Serializable {
     
     private boolean isFloatAbsoluteToleranceSpecified;
     
-    private double floatRelativeTolerance = -1.0;
+    private double floatRelativeTolerance;
     
-    private double floatAbsoluteTolerance = -1.0;
+    private double floatAbsoluteTolerance;
     
     /**
-     * Constructs a DefaultValidatorSettings object with default values specifying
-     * NOT case-sensitive, NOT space-sensitive, and with neither absolute and relative
-     * float tolerances specified.
+     * Constructs a DefaultValidatorSettings object with default values matching the 
+     * defined public default constants.
      */
     public DefaultValidatorSettings() {
-        this.isCaseSensitive = false;
-        this.isSpaceSensitive = false;
-        this.isFloatRelativeToleranceSpecified = false;
-        this.isFloatAbsoluteToleranceSpecified = false;
+        if (log==null) {
+            log = new Log("DefaultValidatorSettings.log");
+            StaticLog.setLog(log);
+            log = StaticLog.getLog();            
+        }
+
+        this.isCaseSensitive = DEFAULT_CASE_SENSITIVITY;
+        this.isSpaceSensitive = DEFAULT_SPACE_SENSITIVITY;
+        this.isFloatRelativeToleranceSpecified = DEFAULT_IS_FLOAT_RELATIVE_TOLERANCE_SPECIFIED;
+        this.isFloatAbsoluteToleranceSpecified = DEFAULT_IS_FLOAT_RELATIVE_TOLERANCE_SPECIFIED;
+        this.floatRelativeTolerance = DEFAULT_FLOAT_RELATIVE_TOLERANCE;
+        this.floatAbsoluteTolerance = DEFAULT_FLOAT_ABSOLUTE_TOLERANCE;
+    }
+    
+    /**
+     * Constructs a DefaultValidatorSettings object containing the values specified in the received String.
+     * The received String is expected to contain zero or more space-separated "validator setting" options
+     * as defined in the CLICS <A href="https://clics.ecs.baylor.edu/index.php/Problem_format#Default_Validator_Capabilities">
+     * <I>default validator options</i></a> specification.
+     * Any options not specified are set to the defaults values defined by the public default constants,
+     * as if the object was constructed by calling {@link DefaultValidator#DefaultValidator()}. 
+     * Any values in the received string not matching the CLICS validator options are logged but
+     * otherwise silently ignored.  The same is true if valid options are specified (e.g. "float_tolerance")
+     * but the required following tolerance value ("epsilon") is missing.
+     */
+    public DefaultValidatorSettings(String options) {
+        
+        //start with the default settings
+        this();
+        
+        //see if the input string has anything to offer
+        if (options!=null && options.length()>0) {
+            
+            //split the string into space-delimited strings
+            String [] opts = options.split(" ");
+            
+            //process each separate string option
+            for (int i=0; i<opts.length; i++) {
+                
+                switch (opts[i]) {
+                    case VTOKEN_CASE_SENSITIVE:
+                    case "case-sensitive":
+                        this.setCaseSensitive(true);
+                        break;
+                        
+                    case VTOKEN_SPACE_CHANGE_SENSITIVE:
+                    case "space-change-sensitive":
+                        this.setSpaceSensitive(true);
+                        break;
+                        
+                    case VTOKEN_FLOAT_RELATIVE_TOLERANCE:
+                    case "float-relative-tolerance":
+                        if (i<opts.length-1) {
+                            try {
+                                double epsilon = Double.parseDouble(opts[i+1]) ;
+                                this.setFloatRelativeTolerance(epsilon);
+                            } catch (NumberFormatException | NullPointerException e) {
+                                log.warning("DefaultValidatorSettings(String) constructor: invalid float tolerance epsilon value; option ignored");
+                            }
+                            i++;
+                        } else {
+                            log.warning("DefaultValidatorSettings(String) constructor missing float tolerance epsilon value; option ignored"); 
+                        }
+                        break;
+                        
+                    case VTOKEN_FLOAT_ABSOLUTE_TOLERANCE:
+                    case "float-absolute-tolerance":
+                        if (i<opts.length-1) {
+                            try {
+                                double epsilon = Double.parseDouble(opts[i+1]) ;
+                                this.setFloatAbsoluteTolerance(epsilon);
+                            } catch (NumberFormatException | NullPointerException e) {
+                                log.warning("DefaultValidatorSettings(String) constructor: invalid float tolerance epsilon value; option ignored");
+                            }
+                            i++;
+                        } else {
+                            log.warning("DefaultValidatorSettings(String) constructor missing float tolerance epsilon value; option ignored"); 
+                        }
+                        break;
+                        
+                    case "float_tolerance":
+                    case "float-tolerance":
+                        if (i<opts.length-1) {
+                            try {
+                                double epsilon = Double.parseDouble(opts[i+1]) ;
+                                this.setFloatRelativeTolerance(epsilon);
+                                this.setFloatAbsoluteTolerance(epsilon);
+                            } catch (NumberFormatException | NullPointerException e) {
+                                log.warning("DefaultValidatorSettings(String) constructor: invalid float tolerance epsilon value; option ignored");
+                            }
+                            i++;
+                        } else {
+                            log.warning("DefaultValidatorSettings(String) constructor missing float tolerance epsilon value; option ignored"); 
+                        }
+                        break;
+                        
+                    default:
+                        log.warning("DefaultValidatorSettings(String) constructor received unknown option '" + opts[i] +"'; ignored");
+                        break;
+                }//end switch
+            }//end for each option field
+        }
     }
 
     /**
@@ -173,7 +287,7 @@ public class DefaultValidatorSettings implements Serializable {
     }
     
     /**
-     * Returns a DefaultValidatorSettings object which is a copy of this object.
+     * Returns a DefaultValidatorSettings object which is a clone of this object.
      */
     public DefaultValidatorSettings clone() {
         DefaultValidatorSettings clone = new DefaultValidatorSettings();
@@ -183,5 +297,40 @@ public class DefaultValidatorSettings implements Serializable {
         clone.setFloatRelativeTolerance(this.getFloatRelativeTolerance());
         return clone;
     }
-
+    
+    /**
+     * Returns a String representation of this DefaultValidatorSettings object.
+     * The format of the string is as if the validator options were specified on a command line
+     * as per the CLICS Default Validator invocation specification; that is, a series of space-delimited
+     * options.  Note that this means that if an option is currently not specified, the option will not
+     * be present in the returned string.  This in turn means, for example, that the string returned for
+     * a DefaultValidatorSettings object for which no options have been specified will be the empty string.
+     */
+    public String toString() {
+        String retStr = "";
+        if (this.isCaseSensitive()) {
+            retStr += "case_sensitive";
+        }
+        if (this.isSpaceSensitive()) {
+            if (retStr.length()>0) {
+                retStr += " ";
+            }
+            retStr += "space_change_sensitive";
+        }
+        if (this.isFloatAbsoluteToleranceSpecified()) {
+            if (retStr.length()>0) {
+                retStr += " ";
+            }
+            retStr += "float_absolute_tolerance ";
+            retStr += Double.toString(getFloatAbsoluteTolerance());
+        }
+        if (this.isFloatRelativeToleranceSpecified()) {
+            if (retStr.length()>0) {
+                retStr += " ";
+            }
+            retStr += "float_relative_tolerance ";
+            retStr += Double.toString(getFloatRelativeTolerance());
+        }
+        return retStr;
+    }
 }
