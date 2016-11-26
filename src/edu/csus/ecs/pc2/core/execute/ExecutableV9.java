b4ede0ca7709f72c3752e845215464450b403b73
john <clevenger@csus.edu>
2016-11-25 17:15:15 -0800

    bug 1159: reverted ExecutableV9 to what it should have been before I incorrectly deleted checks for PC2Validator.

--- src/edu/csus/ecs/pc2/core/execute/ExecutableV9.java
+++ src/edu/csus/ecs/pc2/core/execute/ExecutableV9.java
@@ -618,9 +618,8 @@ public class ExecutableV9 extends Plugin implements IExecutable {
         String commandPattern = problem.getValidatorCommandLine();
         boolean pc2JarUseDirectory = false;
 
-        if (problem.isUsingCLICSDefaultValidator()) {
+        if (problem.isUsingPC2Validator()) {
 
-        	//TODO: this needs to change to reflect the DefaultValidator rather than the old "pc2 validator"
             /**
              * The internal command is set to: <validator> <input_filename> <output_filename> <answer_filename> <results_file> -pc2|-appes [other files] Where validator is
              * Problem.INTERNAL_VALIDATOR_NAME aka "pc2.jar edu.csus.ecs.pc2.validator.Validator"
@@ -640,7 +639,7 @@ public class ExecutableV9 extends Plugin implements IExecutable {
         String cmdLine = executeUtilities.substituteAllStrings(commandPattern);
 
         if (File.separator.equals("\\")) {
-            if (problem.isUsingCLICSDefaultValidator()) {
+            if (problem.isUsingPC2Validator()) {
                 cmdLine = cmdLine.replaceFirst("-cp ", "-cp \"");
                 cmdLine = cmdLine.replaceFirst("jar ", "jar\" ");
                 log.log(Log.DEBUG, "after replaceFirst: " + cmdLine);
