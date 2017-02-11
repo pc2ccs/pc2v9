5c6e2fb388307e23b5fa8a0a7499726d92a0a941
john <clevenger@csus.edu>
2017-02-11 11:45:03 -0800

    bug 1159: added accessors to new class PC2ValidatorSettings.

--- src/edu/csus/ecs/pc2/validator/PC2ValidatorSettings.java
+++ src/edu/csus/ecs/pc2/validator/PC2ValidatorSettings.java
@@ -16,7 +16,40 @@ public class PC2ValidatorSettings {
     
     public PC2ValidatorSettings() {
         
-        this.validatorProgramName = "";
+        this.validatorProgramName = Constants.PC2_VALIDATOR_NAME;
         this.validatorCommandLine = Constants.DEFAULT_PC2_VALIDATOR_COMMAND;
     }
+
+    /**
+     * @return the validatorProgramName
+     */
+    public String getValidatorProgramName() {
+        return validatorProgramName;
+    }
+
+    /**
+     * Sets the Validator Program Name in this PC2ValidatorSettings object to the specified String.
+     * 
+     * @param validatorProgramName the name of the Validator Program
+     */
+    public void setValidatorProgramName(String validatorProgramName) {
+        this.validatorProgramName = validatorProgramName;
+    }
+
+    /**
+     * @return the validatorCommandLine
+     */
+    public String getValidatorCommandLine() {
+        return validatorCommandLine;
+    }
+
+    /**
+     * Sets the Validator Command Line in this PC2ValidatorSettings object to the specified String.
+     * 
+     * @param validatorCommandLine the value to which the Validator Command Line should be set
+     */
+    public void setValidatorCommandLine(String validatorCommandLine) {
+        this.validatorCommandLine = validatorCommandLine;
+    }
+    
 }
