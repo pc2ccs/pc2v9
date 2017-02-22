c960313b33a74cd558f5d65efc80ede4996439f2
John <clevenger@csus.edu>
2017-02-21 17:07:36 -0800

    bug 1159: added more test case samples.

--- samps/src/ISumitWrongOutputSpacing.java
+++ samps/src/ISumitWrongOutputSpacing.java
@@ -3,16 +3,18 @@ import java.io.*;
 //
 // File:    ISumit.java
 // Purpose: to sum the integers from stdin, but produce output which differs from 
-//    the traditional Sumit output in that it uses incorrect CASE (to test case-sensitivity in Validators).
+//    the traditional Sumit output in that it uses incorrect SPACING 
+//    (to test space-sensitivity in Validators).
 
 // Author:  pc2@ecs.csus.edu or http://www.ecs.csus.edu/pc2
 // 
-// Tue Feb 21 16:56:00 PDT 2017
+// Tue Feb 21 04:44:00 PDT 2017
 // 
+//
 // $Id: isumit.java 1962 2009-11-25 03:42:12Z boudreat $
 //
 
-public class ISumitWrongOutputCase {
+public class ISumitWrongOutputSpacing {
     public static void main(String[] args) 
     {
 	try
@@ -30,7 +32,7 @@ public class ISumitWrongOutputCase {
 				sum = sum + rv;
 			// System.out.println(line);
 		}
-		System.out.print("THE SUM OF THE INTEGERS IS ");
+		System.out.print("The    sum   of   the   integers is   ");
 		System.out.println(sum);
 	}
 	catch(Exception e)
