6fc2f3e2206e6ca925fc7ac580df798eef7470b1
Douglas A. Lane <laned@ecs.csus.edu>
2017-02-28 15:52:36 -0800

    bug 1211: add ewinstall and ewinstall.bat

---
+++ projects/EWTeam/scripts/ewinstall.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem Purpose: Show how to install
+rem Author : pc2@ecs.csus.edu
+
+echo.
+echo EWTeam Installation Instructions
+echo.
+echo See the Appendix on "Team Clients"  in the PC2 Contest Administrator's
+echo Guide for information about how to install and run the EWTeam. 
+echo.
+
+rem eof ewinstall.bat 
