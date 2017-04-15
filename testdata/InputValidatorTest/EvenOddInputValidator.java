e3a561f74f29f73a205c884b259c294df87f08d9
john <clevenger@csus.edu>
2017-04-15 10:59:07 -0700

    bug 1160: fixed error in EvenOddInputValidator; recompiled.

--- testdata/InputValidatorTest/EvenOddInputValidator.java
+++ testdata/InputValidatorTest/EvenOddInputValidator.java
@@ -12,6 +12,11 @@ import java.util.Scanner;
  * and should return Exit Code 42 if the input data satisfies the validation rules of the contest problem. Any other Exit Code
  * represents failure of the input data to comply with the validation rules. 
  * 
+ * Note that this code does NOT check whether the input data follows the "alternating even-odd" requirement; only that the input consists
+ * solely of positive integers.  Checking for alternating even/odd numbers is a function of a problem solution, not an input validator
+ * (that is, a valid input file could fail to have alternating integers -- it would be a data file for which the problem solution should
+ * return "No, the input doesn't meet the requirement"; but it's still valid input data file.
+ * 
  * This implementation returns Exit Code -39 if there is an internal error in the Input Validator.
  * 
  * @author pc2@ecs.csus.edu
@@ -33,8 +38,6 @@ public class EvenOddInputValidator {
         Scanner scanner = new Scanner(System.in);
 
         boolean done = false;
-        boolean first = true;
-        boolean lookingForOdd = true; // default initialization; required but not used
         int nextInt;
         int exitCode = INPUT_VALIDATOR_INTERNAL_ERROR_EXIT_CODE;
 
@@ -43,8 +46,10 @@ public class EvenOddInputValidator {
 
             // if there's no more input we're done (and we haven't had an error-exit)
             if (!scanner.hasNext()) {
+                
                 done = true;
                 exitCode = INPUT_VALIDATOR_SUCCESS_EXIT_CODE;
+                
             } else {
 
                 // we know the input has SOMETHING; verify it's an INTEGER (the problem spec requires the input consist solely of integers)
@@ -55,59 +60,12 @@ public class EvenOddInputValidator {
                     
                 } else {
                     
-                    //we know there's an integer next; check whether it matches the even-odd sequence
-                    try {
-                        // get the next integer
-                        nextInt = scanner.nextInt();
-
-                        // make sure the integer is positive (required by the problem spec)
-                        if (nextInt <= 0) {
-                            done = true;
-                            exitCode = INPUT_VALIDATOR_FAILED_EXIT_CODE;
-                        } else {
-
-                            // if this is the first time, indicate whether we're starting with odd or even
-                            if (first) {
-                                if (nextInt % 2 != 0) {
-                                    lookingForOdd = true;
-                                } else {
-                                    lookingForOdd = false;
-                                }
-                                first = false;
-                            }
-
-                            // see if the current token meets the even/odd requirement
-                            if (lookingForOdd) {
-                                if (nextInt % 2 == 0) {
-                                    // we needed an odd but got an even
-                                    exitCode = INPUT_VALIDATOR_FAILED_EXIT_CODE;
-                                    done = true;
-                                }
-                            } else {
-                                if (nextInt % 2 != 0) {
-                                    // we needed an even but got an odd
-                                    exitCode = INPUT_VALIDATOR_FAILED_EXIT_CODE;
-                                    done = true;
-                                }
-                            }
-
-                            // if we get here then all input so far is valid and matches the even/odd alternation requirement
-
-                            // toggle requirement for next loop
-                            lookingForOdd = !lookingForOdd;
-                        }
-
-                    } catch (InputMismatchException e) {
-
+                    // we know there's an integer next; make sure it is positive (required by the problem spec)
+                    nextInt = scanner.nextInt();
+                    if (nextInt <= 0) {
+                        done = true;
                         exitCode = INPUT_VALIDATOR_FAILED_EXIT_CODE;
-                        done = true;
-
-                    } catch (IllegalStateException e) {
-
-                        exitCode = INPUT_VALIDATOR_INTERNAL_ERROR_EXIT_CODE;
-                        done = true;
                     }
-                    
                 } //end scanner.hasNextInt()
             } //end scanner.hasNext()
         } //end while
