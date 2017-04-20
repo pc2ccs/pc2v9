4eb08c8d7b21040abb1e15f3885e98ce341b6c4a
John <clevenger@csus.edu>
2017-04-20 16:04:38 -0700

    bug 1227: changed fetchRun() logic, added additional logging.
    fetchRun() is still generating internal server errors, apparently
    because it is failing to convert the runFiles object received from the
    server to JSON properly.

--- src/edu/csus/ecs/pc2/services/web/FetchRunService.java
+++ src/edu/csus/ecs/pc2/services/web/FetchRunService.java
@@ -10,6 +10,7 @@ import javax.ws.rs.core.Response.Status;
 
 import edu.csus.ecs.pc2.core.IInternalController;
 import edu.csus.ecs.pc2.core.log.Log;
+import edu.csus.ecs.pc2.core.model.ClientId;
 import edu.csus.ecs.pc2.core.model.IInternalContest;
 import edu.csus.ecs.pc2.core.model.IRunListener;
 import edu.csus.ecs.pc2.core.model.Run;
@@ -68,28 +69,37 @@ public class FetchRunService {
             }
             if (run == null) {
                 //not found
+                controller.getLog().log(Log.INFO, "Unable to fetch run " + runID + " from server: not found");
                 return Response.noContent().build();
             } else {
+                controller.getLog().log(Log.INFO, "Requesting run " + runID + " from server");
                 controller.checkOutRun(run, true, false);  //checkoutRun(run, isReadOnlyRequest, isComputerJudgedRequest)
                 
                 int waitedMS = 0;
                 serverReplied = false;
                 
                 //wait for callback to run listener -- but only for up to 10 sec
-                while (!serverReplied && waitedMS++ < 10000) {
+                while (!serverReplied && waitedMS < 10000) {
                     Thread.sleep(100);
+                    waitedMS += 100;
                 }
                 
                 if (serverReplied) {
                     
+                    controller.getLog().log(Log.INFO, "Got a reply from the server...");
+
                     if (run != null && runFiles != null) {
+                        controller.getLog().log(Log.INFO, "Returning runFiles: " + runFiles.toString());
                         return Response.ok(runFiles, MediaType.APPLICATION_JSON).build();
                     } else {
-                        return Response.status(Status.NO_CONTENT).build();
+                        controller.getLog().log(Log.INFO, "Returned run or runFiles was null; returning 'NOT_FOUND'");
+                        return Response.status(Status.NOT_FOUND).build();
                     }
                     
                 } else {
+                    controller.getLog().log(Log.INFO, "No response from server after " + waitedMS + "ms");
                     controller.getLog().log(Log.INFO, "Unable to fetch run " + runID + " from server: not found");
+                    controller.getLog().log(Log.INFO, "Returning 'NOT_FOUND'");
                     return Response.status(Status.NOT_FOUND).build();
                 }
             }
@@ -123,25 +133,51 @@ public class FetchRunService {
 
         public void runChanged(RunEvent event) {
             
-            if (run != null) {
-                if (event.getRun().getElementId().equals(run.getElementId())) {
+            Action action = event.getAction();
+            Action details = event.getDetailedAction();
+            Run aRun = event.getRun();
+            RunFiles aRunFiles = event.getRunFiles();
+            String msg = event.getMessage();
+            
+            controller.getLog().log(Log.INFO, "RunListener: Action=" + action + "; DetailedAction=" + details + "; msg=" + msg
+                                    + "; run=" + aRun + "; runFiles=" + aRunFiles);
+
+            
+            if (event.getRun() != null) {
                     
                     // RUN_NOT_AVAILABLE is undirected (sentToClient is null)
                     if (event.getAction().equals(Action.RUN_NOT_AVAILABLE)) {
                         
                         runStatus = Action.RUN_NOT_AVAILABLE;
-                        controller.getLog().log(Log.INFO, "Requested run not available");
+                        controller.getLog().log(Log.INFO, "Reply from server: requested run not available");
                         
                     } else {
                         
+                        //make sure this RunEvent was meant for me
                         if (event.getSentToClientId() != null && event.getSentToClientId().equals(contest.getClientId())) {
                             
+                            controller.getLog().log(Log.INFO, "Reply from server: " + "Run Status=" + event.getAction()
+                                                    + "; run=" + event.getRun() + ";  runFiles=" + event.getRunFiles());
+                            
+                            aRun = event.getRun();
+                            aRunFiles = event.getRunFiles();                          
+                        } else {
+                            ClientId toClient = event.getSentToClientId() ;
+                            ClientId myID = contest.getClientId();
+
+                            controller.getLog().log(Log.INFO, "Event not for me: sent to " + toClient + " but my ID is " + myID);
+
+                            //this needs to be reconsidered; why are we continuing when the event wasn't sent to us?  (Why wasn't it sent to us?)
                             run = event.getRun();
-                            runFiles = event.getRunFiles();                          
+                            runFiles = event.getRunFiles();
                         }
                     }
-                }
+                    
+            } else {
+                //run from server was null
+                controller.getLog().log(Log.INFO, "Run received from server was null");
             }
+            
             
             serverReplied = true;
         }
