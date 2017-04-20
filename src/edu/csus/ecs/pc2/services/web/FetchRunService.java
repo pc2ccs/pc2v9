0e08a7f230f5acc4eb268a10cb35276d7cf22abd
john <clevenger@csus.edu>
2017-04-20 00:47:40 -0700

    bug 1227: First draft of FetchSubmissionService which actually requests a submission (run) from the server.

--- src/edu/csus/ecs/pc2/services/web/FetchRunService.java
+++ src/edu/csus/ecs/pc2/services/web/FetchRunService.java
@@ -6,9 +6,16 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import edu.csus.ecs.pc2.core.IInternalController;
+import edu.csus.ecs.pc2.core.log.Log;
 import edu.csus.ecs.pc2.core.model.IInternalContest;
+import edu.csus.ecs.pc2.core.model.IRunListener;
+import edu.csus.ecs.pc2.core.model.Run;
+import edu.csus.ecs.pc2.core.model.RunEvent;
+import edu.csus.ecs.pc2.core.model.RunEvent.Action;
+import edu.csus.ecs.pc2.core.model.RunFiles;
 
 
 /**
@@ -21,13 +28,21 @@ import edu.csus.ecs.pc2.core.model.IInternalContest;
 @Produces(MediaType.APPLICATION_JSON)
 public class FetchRunService {
 
-//    private IInternalContest contest;
-//    private IInternalController controller;
+    private IInternalContest contest;
+    private IInternalController controller;
+    
+    private Run run;
+    private RunFiles runFiles;
+    boolean serverReplied;
+    Action runStatus ;
 
     public FetchRunService(IInternalContest inContest, IInternalController inController) {
         super();
-//        this.contest = inContest;
-//        this.controller = inController;
+        this.contest = inContest;
+        this.controller = inController;
+        
+        contest.addRunListener(new RunListenerImplementation());
+
     }
 
     /**
@@ -39,24 +54,105 @@ public class FetchRunService {
     @Produces(MediaType.APPLICATION_JSON)
     public Response fetchRun(@PathParam("id") String runID) {
         
+        serverReplied = false;
         
         //create a dummy hardcoded run for testing
-        String run = "[{\"runID\":" + runID + ",\"filename\":\"a.java\",\"content\":\"<base64_string>\"},{\"filename\":\"helper.java\",\"content\":\"<base64_string>\"}]" ;
+//        String fakeRun = "[{\"runID\":" + runID + ",\"filename\":\"a.java\",\"content\":\"<base64_string>\"},{\"filename\":\"helper.java\",\"content\":\"<base64_string>\"}]" ;
+        
+        run = null;
+        try {
+            Run [] runs = contest.getRuns();
+            for (int i=0; i<runs.length; i++) {
+                if (runs[i].getNumber() == Integer.parseInt(runID)) {
+                    run = runs[i];
+                    break;
+                }
+            }
+            if (run == null) {
+                //not found
+                return Response.noContent().build();
+            } else {
+                controller.checkOutRun(run, true, false);  //checkoutRun(run, isReadOnlyRequest, isComputerJudgedRequest)
+                
+                int waitedMS = 0;
+                serverReplied = false;
+                
+                //wait for callback to run listener -- but only for up to 10 sec
+                while (!serverReplied && waitedMS++ < 10000) {
+                    Thread.sleep(100);
+                }
+                
+                if (serverReplied) {
+                    
+                    if (run != null && runFiles != null) {
+                        return Response.ok(runFiles, MediaType.APPLICATION_JSON).build();
+                    } else {
+                        return Response.status(Status.NO_CONTENT).build();
+                    }
+                    
+                } else {
+                    controller.getLog().log(Log.INFO, "Unable to fetch run " + runID + " from server: not found");
+                    return Response.status(Status.NOT_FOUND).build();
+                }
+            }
 
-//        try {
-//            //code here to construct a "request-run" packet, send it to the server, and wait (?) for the response
-//        } catch (IllegalContestState e) {
-//            //log exception
-//            controller.getLog().log(Log.WARNING, "FetchRunService: problem fetching run from server ", e);
-//            e.printStackTrace();
-//
-//            // return HTTP error response code
-//            return Response.serverError().entity(e.getMessage()).build();
-//        }
+        } catch (Exception e) {
+            //log exception
+            controller.getLog().log(Log.WARNING, "FetchRunService: problem fetching run from server ", e);
+            e.printStackTrace();
 
-        // output the response to the requester (note that this actually returns it to Jersey,
-        // which forwards it to the caller as the HTTP response).
-        return Response.ok(run,MediaType.APPLICATION_JSON).build();
+            // return HTTP error response code
+            return Response.serverError().entity(e.getMessage()).build();
+        }
+
     }
+    
+    /**
+     * Run Listener for FetchRun Service.
+     * 
+     * @author pc2@ecs.csus.edu
+     */
+
+    public class RunListenerImplementation implements IRunListener {
+
+        public void runAdded(RunEvent event) {
+            // ignore
+        }
+        
+        public void refreshRuns(RunEvent event) {
+            // ignore
+        }
+
+        public void runChanged(RunEvent event) {
+            
+            if (run != null) {
+                if (event.getRun().getElementId().equals(run.getElementId())) {
+                    
+                    // RUN_NOT_AVAILABLE is undirected (sentToClient is null)
+                    if (event.getAction().equals(Action.RUN_NOT_AVAILABLE)) {
+                        
+                        runStatus = Action.RUN_NOT_AVAILABLE;
+                        controller.getLog().log(Log.INFO, "Requested run not available");
+                        
+                    } else {
+                        
+                        if (event.getSentToClientId() != null && event.getSentToClientId().equals(contest.getClientId())) {
+                            
+                            run = event.getRun();
+                            runFiles = event.getRunFiles();                          
+                        }
+                    }
+                }
+            }
+            
+            serverReplied = true;
+        }
+
+        public void runRemoved(RunEvent event) {
+            // ignore
+        }
+    }
+
+
 
 }
