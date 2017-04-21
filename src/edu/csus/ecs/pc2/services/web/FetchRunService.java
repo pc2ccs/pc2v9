19609b86695c752e3e14fd23fd632817fb59e24d
john <clevenger@csus.edu>
2017-04-20 22:12:49 -0700

    bug 1227: changed REST endpoint to "submission_files" and changed to use a QueryParam instead of a PathParam, to match the current (2014) CLICS specification.

--- src/edu/csus/ecs/pc2/services/web/FetchRunService.java
+++ src/edu/csus/ecs/pc2/services/web/FetchRunService.java
@@ -2,13 +2,13 @@ package edu.csus.ecs.pc2.services.web;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.SecurityContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -29,7 +29,7 @@ import edu.csus.ecs.pc2.core.model.RunFiles;
  * @author john
  *
  */
-@Path("/submission/{id}")
+@Path("/submission_files")
 @Produces(MediaType.APPLICATION_JSON)
 public class FetchRunService {
 
@@ -57,7 +57,7 @@ public class FetchRunService {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response fetchRun(@PathParam("id") String runID, @Context SecurityContext sc) {
+    public Response fetchRun(@QueryParam("id") String runID, @Context SecurityContext sc) {
         
         if (!sc.isUserInRole("admin")) {
             // do not return runs if the requestor is not an admin  (this might need to be relaxed later, e.g. by including wider credential...)
