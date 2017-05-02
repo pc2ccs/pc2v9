ce5f42a94b709c706b13eedb830a4b64c994acf9
john <clevenger@csus.edu>
2017-05-02 13:14:38 -0700

    bug 1160: updated JavaDoc for PassFailCellRenderer.

--- src/edu/csus/ecs/pc2/ui/cellRenderer/PassFailCellRenderer.java
+++ src/edu/csus/ecs/pc2/ui/cellRenderer/PassFailCellRenderer.java
@@ -19,13 +19,13 @@ import javax.swing.table.TableCellRenderer;
  * <P>
  * If the Object is a Boolean, the cell is rendered Pass/Fail (green/red) based on the value of the Boolean (true==pass).
  * <p>
- * If the received Object is a String then the cell is rendered based on the text of the string: "pass" renders as
- * a green/pass while "fail" renders as a red/fail.  The check for the value of the String is not case-sensitive.
+ * If the received Object is a String containing either "pass" or "fail" then the cell is rendered based on the text of the string:
+ * "pass" renders as a green/pass while "fail" renders as a red/fail.  The check for the value of the String is not case-sensitive.
  * <P>
  * If the received Object is a JLabel, the String text in the JLabel is fetched and then the cell is rendered in the same way
  * as for Strings, above. 
  *
- * If the value passed to the {@link #setValue(Object)} method is any of the above-listed types then 
+ * If the value passed to the {@link #setValue(Object)} method is anything not covered of the above-listed steps then 
  * the cell is rendered with a yellow background containing question marks.
  * 
  * @author pc2@ecs.csus.edu
