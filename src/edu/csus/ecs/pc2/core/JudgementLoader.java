95300ae5d2cab0c695b604b3038d534931ed3b1e
Douglas A. Lane <laned@ecs.csus.edu>
2017-10-03 14:28:41 -0700

    bug 1222: added load judgement class
    has methods to load reject.ini with acronyms.

---
+++ src/edu/csus/ecs/pc2/core/JudgementLoader.java
@@ -0,0 +1,94 @@
+package edu.csus.ecs.pc2.core;
+
+import java.io.File;
+
+import edu.csus.ecs.pc2.core.model.IInternalContest;
+import edu.csus.ecs.pc2.core.model.Judgement;
+
+/**
+ * Provides a way to load reject.ini file.
+ * 
+ * @author Douglas A. Lane, PC^2 Team, pc2@ecs.csus.edu
+ */
+public class JudgementLoader {
+
+    /**
+     * Loads reject.ini file contents into Judgements.
+     * 
+     * If finds reject.ini file, reads file. Adds Yes judgement, then prepends "No - " onto each entry from the reject.ini file and returns true.
+     * 
+     * Returns false if cannot read reject.ini file or reject.ini file is empty (perhaps only containing comments).
+     * 
+     * @return true if loaded, false if could not read file.
+     */
+    public boolean loadedJudgementsFromIni(IInternalContest contest, String filename) {
+
+        if (new File(filename).exists()) {
+
+            String[] lines = Utilities.loadINIFile(filename);
+            return loadedJudgements(contest, lines);
+        }
+
+        return false;
+    }
+
+    public boolean loadedJudgements(IInternalContest contest, String[] lines) {
+
+        if (lines == null || lines.length == 0) {
+            return false;
+        }
+
+        Judgement yesJudgement = new Judgement("Yes", Judgement.ACRONYM_ACCEPTED);
+
+        for (String line : lines) {
+            if (Judgement.ACRONYM_ACCEPTED.equals(getAcronym(line))) {
+                yesJudgement = new Judgement(getJudgementText(line), Judgement.ACRONYM_ACCEPTED);
+            }
+        }
+
+        contest.addJudgement(yesJudgement);
+        int offset = contest.getJudgements().length;
+        
+        for (String line : lines) {
+            
+            if (line.trim().startsWith("#") || line.trim().length() == 0){
+                continue;
+            }
+
+            String judgementText = getJudgementText(line);
+            String acronym = getAcronym(line);
+
+            if (acronym == null) {
+                String waNumber = String.format("%03d", offset++);
+                acronym = Judgement.ACRONYM_WRONG_ANSWER + waNumber;
+            }
+
+            // Already loaded Yes 
+            if (!Judgement.ACRONYM_ACCEPTED.equals(getAcronym(line))) {
+                Judgement judgement = new Judgement("No - " + judgementText, acronym);
+                contest.addJudgement(judgement);
+            }
+        }
+
+        return true;
+    }
+
+    protected String getJudgementText(String line) {
+        String[] fields = line.split("[|]");
+        if (fields.length > 1) {
+            return fields[0].trim();
+        } else {
+            return line.trim();
+        }
+    }
+
+    protected String getAcronym(String line) {
+        String[] fields = line.split("[|]");
+        if (fields.length > 1) {
+            return fields[1].trim();
+        } else {
+            return null;
+        }
+    }
+
+}
---
+++ test/edu/csus/ecs/pc2/core/JudgementLoaderTest.java
@@ -0,0 +1,172 @@
+package edu.csus.ecs.pc2.core;
+
+import edu.csus.ecs.pc2.core.model.IInternalContest;
+import edu.csus.ecs.pc2.core.model.InternalContest;
+import edu.csus.ecs.pc2.core.model.Judgement;
+import junit.framework.TestCase;
+
+public class JudgementLoaderTest extends TestCase {
+    
+    
+    /**
+     * Test Load from 
+     * @throws Exception
+     */
+    public void testLoadFromLLines() throws Exception {
+        
+        String [] lines = {
+                //
+                "#", //
+                "# reject.ini - Override no judgement text.", //
+                "#", //
+                "# This sample contains the default judgements coded into the system.", //
+                "#", //
+                "# To use, this file should be renamed to reject.ini and placed in", //
+                "# the directory from which the 1st server is started.  To verify these", //
+                "# judgements are active view the Judgements report from the Server UI.", //
+                "# If no reject.ini is provided, the system will default to these", //
+                "# judgements.", //
+                "#", //
+                "# Blank lines and lines beginning with a # are ignored.", //
+                "#", //
+                "# $HeadURL$", //
+                "#", //
+                "Compilation Error", //
+                "Run-time Error", //
+                "Time Limit Exceeded", //
+                "Wrong Answer", //
+                "Excessive Output", //
+                "Output Format Error", //
+                "Other - Contact Staff", //
+                "# eof $Id$", //
+        };
+        
+        IInternalContest contest = new InternalContest();
+
+        boolean loaded = new JudgementLoader().loadedJudgements(contest, lines);
+
+        assertTrue("Expected judgements to be loaded ", loaded);
+
+        Judgement[] judgements = contest.getJudgements();
+        assertEquals("Expecting number of judgements ", 8, judgements.length);
+
+        Judgement judgement = judgements[1];
+        assertEquals("Expecting acronym for " + judgement, "WA001", judgement.getAcronym());
+
+    }
+    
+    /**
+     * Test load acronynms.
+     * @throws Exception
+     */
+    public void testLoadWithAcronyms() throws Exception {
+        
+
+        String [] lines = {
+                //
+                "Compilation Error|CE", //
+                "Run-time Error|RTE", //
+                "Time Limit Exceeded|TLE", //
+                "Wrong Answer|WA", //
+                "Excessive Output|EE", //
+                "Output Format Error|EFE", //
+                "Various Differences|VD", //
+                "Other - Contact Staff|CS", //
+                "# eof $Id$", //
+        };
+
+        IInternalContest contest = new InternalContest();
+
+        boolean loaded = new JudgementLoader().loadedJudgements(contest, lines);
+        assertTrue("Expected judgements to be loaded ", loaded);
+
+        Judgement[] judgements = contest.getJudgements();
+        assertEquals("Expecting number of judgements ", 9, judgements.length);
+
+        Judgement judgement = judgements[1];
+        assertEquals("Expecting acronym for " + judgement, "CE", judgement.getAcronym());
+        
+        judgement = judgements[0];
+        assertEquals("Expecting acronym for " + judgement, "AC", judgement.getAcronym());
+
+        
+    }
+    
+    /**
+     * TEst for AC in file.
+     * @throws Exception
+     */
+    public void testLoadWithAltAC() throws Exception {
+
+        String [] lines = {
+                //
+                "Accepted|AC", //
+                "Compilation Error|CE", //
+                "Run-time Error|RTE", //
+                "Time Limit Exceeded|TLE", //
+                "Wrong Answer|WA", //
+                "Excessive Output|EE", //
+                "Output Format Error|EFE", //
+                "Various Differences|VD", //
+                "Other - Contact Staff|CS", //
+                "# eof $Id$", //
+        };
+        
+        for (String string : lines) {
+            System.out.println(string);
+        }
+
+        IInternalContest contest = new InternalContest();
+
+        boolean loaded = new JudgementLoader().loadedJudgements(contest, lines);
+        assertTrue("Expected judgements to be loaded ", loaded);
+
+        Judgement[] judgements = contest.getJudgements();
+        assertEquals("Expecting number of judgements ", 9, judgements.length);
+
+        Judgement judgement = judgements[1];
+        assertEquals("Expecting acronym for " + judgement, "CE", judgement.getAcronym());
+        
+        judgement = judgements[0];
+        assertEquals("Expecting acronym for " + judgement, "AC", judgement.getAcronym());
+        assertEquals("Expecting judgement for " + judgement, "Accepted", judgement.toString());
+    }
+    
+    /**
+     * Test load AC line at alternate location in file.
+     * 
+     * @throws Exception
+     */
+    public void testLoadWithAltACAtEndOfList() throws Exception {
+
+        String [] lines = {
+                //
+                "Compilation Error|CE", //
+                "Run-time Error|RTE", //
+                "Time Limit Exceeded|TLE", //
+                "Wrong Answer|WA", //
+                "Excessive Output|EE", //
+                "Output Format Error|EFE", //
+                "Various Differences|VD", //
+                "Other - Contact Staff|CS", //
+                "Accepted|AC", //
+                "# eof $Id$", //
+        };
+
+        IInternalContest contest = new InternalContest();
+
+        boolean loaded = new JudgementLoader().loadedJudgements(contest, lines);
+        assertTrue("Expected judgements to be loaded ", loaded);
+
+        Judgement[] judgements = contest.getJudgements();
+        assertEquals("Expecting number of judgements ", 9, judgements.length);
+
+        Judgement judgement = judgements[1];
+        assertEquals("Expecting acronym for " + judgement, "CE", judgement.getAcronym());
+        
+        judgement = judgements[0];
+        assertEquals("Expecting acronym for " + judgement, "AC", judgement.getAcronym());
+        assertEquals("Expecting judgement for " + judgement, "Accepted", judgement.toString());
+    }
+    
+}
