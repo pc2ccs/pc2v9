0812c5afdd9e9b3a3e1b72d9822f5d9f9234dae9
Troy Boudreau <boudreat@ecs.csus.edu>
2017-10-10 13:02:29 -0700

    bug 1313: do not show problems to public role unless contest is started

--- src/edu/csus/ecs/pc2/services/web/ProblemService.java
+++ src/edu/csus/ecs/pc2/services/web/ProblemService.java
@@ -5,10 +5,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -53,7 +55,7 @@ public class ProblemService implements Feature {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getProblems() {
+    public Response getProblems(@Context SecurityContext sc) {
 
         // get the problems from the contest
         Problem[] problems = model.getProblems();
@@ -61,11 +63,13 @@ public class ProblemService implements Feature {
         // get an object to map the problems descriptions into JSON form
         ObjectMapper mapper = new ObjectMapper();
         ArrayNode childNode = mapper.createArrayNode();
-        for (int i = 0; i < problems.length; i++) {
-            Problem problem = problems[i];
-            childNode.add(jsonTool.convertToJSON(problem, i));
+        // public only gets the problems when the contest starts
+        if (!sc.isUserInRole("public") || model.getContestTime().isContestStarted()) {
+            for (int i = 0; i < problems.length; i++) {
+                Problem problem = problems[i];
+                childNode.add(jsonTool.convertToJSON(problem, i));
+            }
         }
-
         // output the response to the requester (note that this actually returns it to Jersey,
         // which forwards it to the caller as the HTTP response).
         return Response.ok(childNode.toString(), MediaType.APPLICATION_JSON).build();
@@ -74,7 +78,7 @@ public class ProblemService implements Feature {
     @GET
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     @Path("{problemId}/")
-    public Response getProblem(@PathParam("problemId") String problemId) {
+    public Response getProblem(@Context SecurityContext sc, @PathParam("problemId") String problemId) {
         // get the problems from the contest
         Problem[] problems = model.getProblems();
 
