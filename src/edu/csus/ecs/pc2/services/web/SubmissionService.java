9e4158cc01f6bfb39911c8c46454b74dbdf21f40
Troy Boudreau <boudreat@ecs.csus.edu>
2017-10-10 13:10:42 -0700

    bug 1058: restrict files to admin and analysts
    return UNAUTHORIZED for users of other roles.

--- src/edu/csus/ecs/pc2/services/web/SubmissionService.java
+++ src/edu/csus/ecs/pc2/services/web/SubmissionService.java
@@ -19,12 +19,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -126,7 +128,11 @@ public class SubmissionService implements Feature {
     @GET
     @Produces("application/zip")
     @Path("{submissionId}/files/")
-    public Response getSubmissionFiles(@PathParam("submissionId") String submissionId) {
+    public Response getSubmissionFiles(@Context SecurityContext sc, @PathParam("submissionId") String submissionId) {
+        // only admin and analyst are authorized to access this endpoint
+        if (!(sc.isUserInRole("admin") || sc.isUserInRole("analyst"))) {
+            return Response.status(Status.UNAUTHORIZED).build();
+        }
         // get the submissions from the contest
         Run[] runs = model.getRuns();
 
