1f9dd5e139d4d757fb10b69f1043eabd3fc5ed09
Troy Boudreau <boudreat@ecs.csus.edu>
2017-10-10 12:50:52 -0700

    bug 1318: Use freezeTime to block public access
    for runs submitted after the freeze time, do not show their
    judgements to public users.

--- src/edu/csus/ecs/pc2/services/web/JudgementService.java
+++ src/edu/csus/ecs/pc2/services/web/JudgementService.java
@@ -5,16 +5,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import edu.csus.ecs.pc2.core.IInternalController;
+import edu.csus.ecs.pc2.core.Utilities;
 import edu.csus.ecs.pc2.core.model.IInternalContest;
 import edu.csus.ecs.pc2.core.model.Run;
 import edu.csus.ecs.pc2.core.util.JSONTool;
@@ -53,15 +56,22 @@ public class JudgementService implements Feature {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getJudgements() {
+    public Response getJudgements(@Context SecurityContext sc) {
         // get the groups from the contest
         Run[] runs = model.getRuns();
+        long freezeTime = getFreezeTime();
 
         // get an object to map the groups descriptions into JSON form
         ObjectMapper mapper = new ObjectMapper();
         ArrayNode childNode = mapper.createArrayNode();
         for (int i = 0; i < runs.length; i++) {
             Run run = runs[i];
+            if (sc.isUserInRole("public")) {
+                // if run is after scoreboard freeze, and public access do not show judgement
+                if (run.getElapsedMS() / 1000 > freezeTime) {
+                    continue;
+                }
+            }
             childNode.add(jsonTool.convertJudgementToJSON(run));
         }
 
@@ -73,15 +83,22 @@ public class JudgementService implements Feature {
     @GET
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     @Path("{judgementId}/")
-    public Response getJudgement(@PathParam("judgementId") String judgementId) {
+    public Response getJudgement(@Context SecurityContext sc, @PathParam("judgementId") String judgementId) {
         // get the runs from the contest
         Run[] runs = model.getRuns();
+        long freezeTime = getFreezeTime();
 
         // get an object to map the judgements descriptions into JSON form
         ObjectMapper mapper = new ObjectMapper();
         ArrayNode childNode = mapper.createArrayNode();
         for (int i = 0; i < runs.length; i++) {
             Run run = runs[i];
+            if (sc.isUserInRole("public")) {
+                // if run is after scoreboard freeze, and public access do not show judgement
+                if (run.getElapsedMS() / 1000 > freezeTime) {
+                    continue;
+                }
+            }
             // judgementId's match runId's
             if (run.getElementId().toString().equals(judgementId)) {
                 childNode.add(jsonTool.convertJudgementToJSON(run));
@@ -89,6 +106,15 @@ public class JudgementService implements Feature {
         }
         return Response.ok(childNode.toString(), MediaType.APPLICATION_JSON).build();
 
+    }
+
+    private long getFreezeTime() {
+        long freezeTime = Utilities.convertStringToSeconds(model.getContestInformation().getFreezeTime());
+        if (freezeTime == -1) {
+            // invalid so set to end of contest
+            freezeTime = model.getContestTime().getContestLengthSecs();
+        }
+        return freezeTime;
     }
 
     @Override
