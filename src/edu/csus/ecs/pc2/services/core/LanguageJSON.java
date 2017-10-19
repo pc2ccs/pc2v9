d8d7cabd779df82724ff8899e0643351fcdb22be
Douglas A. Lane <laned@ecs.csus.edu>
2017-10-18 17:25:05 -0700

    bug 1309: removed dup language JSON
    Fixed bug where duplicate JSON was output for a language

--- src/edu/csus/ecs/pc2/services/core/LanguageJSON.java
+++ src/edu/csus/ecs/pc2/services/core/LanguageJSON.java
@@ -15,11 +15,12 @@ import edu.csus.ecs.pc2.core.model.Language;
 public class LanguageJSON extends JSONUtilities {
     
     private ObjectMapper mapper = new ObjectMapper();
-    private ArrayNode childNode = mapper.createArrayNode();
+    
 
     public String createJSON(IInternalContest contest, Language language, int languageNumber) {
         
         ObjectNode element = mapper.createObjectNode();
+        ArrayNode childNode = mapper.createArrayNode();
         element.put("id",  Integer.toString(languageNumber));
         element.put("name", language.getDisplayName());
         childNode.add(element);
