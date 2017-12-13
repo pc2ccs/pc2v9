5e0882a447f1acd9ca704f39e7edaea8792f0978
Troy Boudreau <boudreat@ecs.csus.edu>
2017-12-12 16:12:28 -0800

    bug 1379: add newline characters to tsv files.
    Also synced up generateOutput between View and Module.

--- src/edu/csus/ecs/pc2/ui/board/ScoreboardModule.java
+++ src/edu/csus/ecs/pc2/ui/board/ScoreboardModule.java
@@ -144,143 +144,155 @@ public class ScoreboardModule implements UIPlugin {
     }
 
     private void generateOutput(String xmlString) {
-
-        // log.info( " generateOutput(xmlString)");
-
-        File inputDir = new File(xslDir);
-        if (!inputDir.isDirectory()) {
-            log.warning("xslDir is not a directory");
-            return;
-        }
-        File outputDirFile = new File(outputDir);
-        if (!outputDirFile.exists() && !outputDirFile.mkdirs()) {
-            log.warning("Could not create " + outputDirFile.getAbsolutePath() + ", defaulting to current directory");
-            outputDir = ".";
-            outputDirFile = new File(outputDir);
-        }
-        if (!outputDirFile.isDirectory()) {
-            log.warning(outputDir + " is not a directory.");
-            return;
-        } else {
-            log.fine("Sending output to " + outputDirFile.getAbsolutePath());
-        }
-        try {
-            File output = File.createTempFile("__t", ".tmp", new File("."));
-            FileOutputStream outputXML = new FileOutputStream(output);
-            outputXML.write(xmlString.getBytes());
-            outputXML.close();
-            if (output.length() > 0) {
-                File outputFile = new File("results.xml");
-                // behaviour of renameTo is platform specific, try the possibly atomic 1st
-                if (!output.renameTo(outputFile)) {
-                    // otherwise fallback to the delete then rename
-                    outputFile.delete();
-                    if (!output.renameTo(outputFile)) {
-                        log.warning("Could not create " + outputFile.getCanonicalPath());
-                    }
-                }
-            } else {
-                // 0 length file
-                log.warning("New results.xml is empty, not updating");
-                output.delete();
-            }
-            output = null;
-        } catch (FileNotFoundException e1) {
-            log.log(Log.WARNING, "Could not write to " + "results.xml", e1);
-        } catch (IOException e) {
-            log.log(Log.WARNING, "Problem writing to " + "results.xml", e);
-        }
-        String[] inputFiles = inputDir.list();
-        XSLTransformer transformer = new XSLTransformer();
-        for (int i = 0; i < inputFiles.length; i++) {
-            String xslFilename = inputFiles[i];
-            if (xslFilename.endsWith(".xsl")) {
-                String outputFilename = xslFilename.substring(0, xslFilename.length() - 4) + ".html";
-                try {
-                    File output = File.createTempFile("__t", ".htm", outputDirFile);
-                    FileOutputStream outputStream = new FileOutputStream(output);
-                    transformer.transform(xslDir + File.separator + xslFilename, new ByteArrayInputStream(xmlString.getBytes()), outputStream);
-                    outputStream.close();
-                    if (output.length() > 0) {
-                        File outputFile = new File(outputDir + File.separator + outputFilename);
-                        if (xslFilename.equals("pc2export.xsl")) {
-                            // change that, we want the pc2export written as a .dat in the cwd
-                            outputFile = new File("pc2export.dat");
-                        }
-                        // dump json and tsv and csv files in the html directory
-                        if (xslFilename.endsWith(".json.xsl") || xslFilename.endsWith(".tsv.xsl") || xslFilename.endsWith(".csv.xsl") || xslFilename.endsWith(".php.xsl")) {
-                            outputFile = new File(outputDir + File.separator + xslFilename.substring(0, xslFilename.length() - 4));
-                        }
-                        // behaviour of renameTo is platform specific, try the possibly atomic 1st
-                        if (!output.renameTo(outputFile)) {
-                            // otherwise fallback to the delete then rename
-                            outputFile.delete();
-                            if (!output.renameTo(outputFile)) {
-                                log.warning("Could not create " + outputFile.getCanonicalPath());
-                            }
-                        }
-                    } else {
-                        // 0 length file
-                        log.warning("output from tranformation " + xslFilename + " was empty");
-                        output.delete();
-                    }
-                    output = null;
-                } catch (IOException e) {
-                    log.log(Log.WARNING, "Trouble transforming " + xslFilename, e);
-                } catch (TransformerConfigurationException e) {
-                    // unfortunately this prints the details to stdout (or maybe stderr)
-                    log.log(Log.WARNING, "Trouble transforming " + xslFilename, e);
-                } catch (Exception e) {
-                    log.log(Log.WARNING, "Trouble transforming " + xslFilename, e);
-                }
-            }
-        }
-        FinalizeData finalizeData = contest.getFinalizeData();
-        if (finalizeData != null && finalizeData.isCertified()) {
-            File outputResultsDirFile = new File("results");
-            if (!outputResultsDirFile.exists() && !outputResultsDirFile.mkdirs()) {
-                log.warning("Could not create " + outputResultsDirFile.getAbsolutePath() + ", defaulting to current directory");
-                outputDir = ".";
-                outputResultsDirFile = new File(outputDir);
-            }
-            if (!outputResultsDirFile.isDirectory()) {
-                log.warning(outputDir + " is not a directory.");
-                return;
-            } else {
-                log.fine("Sending results output to " + outputResultsDirFile.getAbsolutePath());
-            }
-            try {
-                ResultsFile resultsFile = new ResultsFile();
-                String[] createTSVFileLines = resultsFile.createTSVFileLines(contest);
-                FileWriter outputFile = new FileWriter(outputResultsDirFile + File.separator + "results.tsv");
-                for (int i = 0; i < createTSVFileLines.length; i++) {
-                    outputFile.write(createTSVFileLines[i]);
-                }
-                outputFile.close();
-            } catch (IllegalContestState | IOException e) {
-                log.log(Log.WARNING, "Trouble creating results.tsv", e);
-            }
-            try {
-                ScoreboardFile scoreboardFile = new ScoreboardFile();
-                String[] createTSVFileLines = scoreboardFile.createTSVFileLines(contest);
-                FileWriter outputFile = new FileWriter(outputResultsDirFile + File.separator + "scoreboard.tsv");
-                for (int i = 0; i < createTSVFileLines.length; i++) {
-                    outputFile.write(createTSVFileLines[i]);
-                }
-                outputFile.close();
-            } catch (IllegalContestState | IOException e) {
-                log.log(Log.WARNING, "Trouble creating scoreboard.tsv", e);
-            }
-            StandingsJSON2016 standingsJson = new StandingsJSON2016();
-            try {
-                String createJSON = standingsJson.createJSON(contest, controller);
-                FileWriter outputFile = new FileWriter(outputResultsDirFile + File.separator + "scoreboard.json");
-                outputFile.write(createJSON);
-                outputFile.close();
-            } catch (IllegalContestState | IOException e) {
-                log.log(Log.WARNING, "Trouble creating scoreboard.json", e);
-            }
-        }
+    	// FUTUREWORK move to to a common location (currently in both Module and View)
+		File inputDir = new File(xslDir);
+		if (!inputDir.isDirectory()) {
+			log.warning("xslDir is not a directory");
+			return;
+		}
+		File outputDirFile = new File(outputDir);
+		if (!outputDirFile.exists() && !outputDirFile.mkdirs()) {
+			log.warning("Could not create " + outputDirFile.getAbsolutePath() + ", defaulting to current directory");
+			outputDir = ".";
+			outputDirFile = new File(outputDir);
+		}
+		if (!outputDirFile.isDirectory()) {
+			log.warning(outputDir + " is not a directory.");
+			return;
+		} else {
+			log.fine("Sending output to " + outputDirFile.getAbsolutePath());
+		}
+		try {
+			File output = File.createTempFile("__t", ".tmp", new File("."));
+			FileOutputStream outputXML = new FileOutputStream(output);
+			outputXML.write(xmlString.getBytes());
+			outputXML.close();
+			if (output.length() > 0) {
+				File outputFile = new File("results.xml");
+				// behaviour of renameTo is platform specific, try the possibly
+				// atomic 1st
+				if (!output.renameTo(outputFile)) {
+					// otherwise fallback to the delete then rename
+					outputFile.delete();
+					if (!output.renameTo(outputFile)) {
+						log.warning("Could not create " + outputFile.getCanonicalPath());
+					}
+				}
+			} else {
+				// 0 length file
+				log.warning("New results.xml is empty, not updating");
+				output.delete();
+			}
+			output = null;
+		} catch (FileNotFoundException e1) {
+			log.log(Log.WARNING, "Could not write to " + "results.xml", e1);
+		} catch (IOException e) {
+			log.log(Log.WARNING, "Problem writing to " + "results.xml", e);
+		}
+		// TODO consider changing this to use a filenameFilter
+		String[] inputFiles = inputDir.list();
+		XSLTransformer transformer = new XSLTransformer();
+		for (int i = 0; i < inputFiles.length; i++) {
+			String xslFilename = inputFiles[i];
+			if (xslFilename.endsWith(".xsl")) {
+				String outputFilename = xslFilename.substring(0, xslFilename.length() - 4) + ".html";
+				try {
+					File output = File.createTempFile("__t", ".htm", outputDirFile);
+					FileOutputStream outputStream = new FileOutputStream(output);
+					transformer.transform(xslDir + File.separator + xslFilename,
+							new ByteArrayInputStream(xmlString.getBytes()), outputStream);
+					outputStream.close();
+					if (output.length() > 0) {
+						File outputFile = new File(outputDir + File.separator + outputFilename);
+						if (xslFilename.equals("pc2export.xsl")) {
+							// change that, we want the pc2export written as a
+							// .dat in the cwd
+							outputFile = new File("pc2export.dat");
+						}
+						// dump json and tsv and csv files in the html directory
+						if (xslFilename.endsWith(".json.xsl") || xslFilename.endsWith(".tsv.xsl")
+								|| xslFilename.endsWith(".csv.xsl") || xslFilename.endsWith(".php.xsl")) {
+							outputFile = new File(
+									outputDir + File.separator + xslFilename.substring(0, xslFilename.length() - 4));
+						}
+						// behaviour of renameTo is platform specific, try the
+						// possibly atomic 1st
+						if (!output.renameTo(outputFile)) {
+							// otherwise fallback to the delete then rename
+							outputFile.delete();
+							if (!output.renameTo(outputFile)) {
+								log.warning("Could not create " + outputFile.getCanonicalPath());
+							} else {
+								log.finest("rename2 to " + outputFile.getCanonicalPath() + " succeeded.");
+							}
+						} else {
+							log.finest("rename to " + outputFile.getCanonicalPath() + " succeeded.");
+						}
+					} else {
+						// 0 length file
+						log.warning("output from tranformation " + xslFilename + " was empty");
+						output.delete();
+					}
+					output = null;
+				} catch (IOException e) {
+					// TODO re-visit this log message
+					log.log(Log.WARNING, "Trouble transforming " + xslFilename, e);
+				} catch (TransformerConfigurationException e) {
+					// unfortunately this prints the details to stdout (or maybe
+					// stderr)
+					log.log(Log.WARNING, "Trouble transforming " + xslFilename, e);
+				} catch (Exception e) {
+					log.log(Log.WARNING, "Trouble transforming " + xslFilename, e);
+				}
+			}
+		}
+		FinalizeData finalizeData = contest.getFinalizeData();
+		if (finalizeData != null && finalizeData.isCertified()) {
+			File outputResultsDirFile = new File("results");
+			if (!outputResultsDirFile.exists() && !outputResultsDirFile.mkdirs()) {
+				log.warning("Could not create " + outputResultsDirFile.getAbsolutePath()
+						+ ", defaulting to current directory");
+				outputDir = ".";
+				outputResultsDirFile = new File(outputDir);
+			}
+			if (!outputResultsDirFile.isDirectory()) {
+				log.warning(outputDir + " is not a directory.");
+				return;
+			} else {
+				log.fine("Sending results output to " + outputResultsDirFile.getAbsolutePath());
+			}
+			try {
+				ResultsFile resultsFile = new ResultsFile();
+				String[] createTSVFileLines = resultsFile.createTSVFileLines(contest);
+				FileWriter outputFile = new FileWriter(outputResultsDirFile + File.separator + "results.tsv");
+				for (int i = 0; i < createTSVFileLines.length; i++) {
+					outputFile.write(createTSVFileLines[i] + System.getProperty("line.separator"));
+				}
+				outputFile.close();
+			} catch (IllegalContestState | IOException e) {
+				log.log(Log.WARNING, "Trouble creating results.tsv", e);
+			}
+			try {
+				ScoreboardFile scoreboardFile = new ScoreboardFile();
+				String[] createTSVFileLines = scoreboardFile.createTSVFileLines(contest);
+				FileWriter outputFile = new FileWriter(outputResultsDirFile + File.separator + "scoreboard.tsv");
+				for (int i = 0; i < createTSVFileLines.length; i++) {
+					outputFile.write(createTSVFileLines[i] + System.getProperty("line.separator"));
+				}
+				outputFile.close();
+			} catch (IllegalContestState | IOException e) {
+				log.log(Log.WARNING, "Trouble creating scoreboard.tsv", e);
+			}
+			StandingsJSON2016 standingsJson = new StandingsJSON2016();
+			try {
+				String createJSON = standingsJson.createJSON(contest, controller);
+				FileWriter outputFile = new FileWriter(outputResultsDirFile + File.separator + "scoreboard.json");
+				outputFile.write(createJSON);
+				outputFile.close();
+			} catch (IllegalContestState | IOException e) {
+				log.log(Log.WARNING, "Trouble creating scoreboard.json", e);
+			}
+		}
     }
 
     protected Properties getScoringProperties() {
