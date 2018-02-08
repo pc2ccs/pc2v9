1fa7552a3581a8d86c591a3f24e5aa354f88b7c0
Douglas A. Lane <laned@ecs.csus.edu>
2018-02-08 07:30:47 -0800

    bug 1285: add class to for Bug 1351 testing

---
+++ testdata/ClicsValidatorTest/RuntimeError.java
@@ -0,0 +1,22 @@
+/**
+ * Program that returns a "run time error".
+ * 
+ * Source from: https://pc2.ecs.csus.edu/bugzilla/show_bug.cgi?id=1351#c2
+ * 
+ * @author Douglas A. Lane, PC^2 Team, pc2@ecs.csus.edu
+ */
+public class RuntimeError {
+
+	/**
+	 * Create condition that emulates a run time error.
+	 * 
+	 * In pc2 if the return code is -1 that is considered a run-time error.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Exiting with exit code '-1'");
+		System.err.println("Exiting with exit code '-1'");
+		System.exit(-1);
+	}
+}
