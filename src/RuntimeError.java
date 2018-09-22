ca073fbbd8ec1df000b4ed2d5e2972c9bc84a23d
Douglas A. Lane <laned@ecs.csus.edu>
2018-09-22 13:06:32 -0700

    bug 1285: adding sample class for RTE testing
    Added class RuntimeError into samples.  Code was created for
    b1351 - CLICS validator returns Wrong Answer for Runtime Error

---
+++ src/RuntimeError.java
@@ -0,0 +1,20 @@
+/**
+ * Program that causes a RTE.
+ * 
+ * Source: https://pc2.ecs.csus.edu/bugzilla/show_bug.cgi?id=1351#c2
+ * 
+ * @author PC^2 Team, pc2@ecs.csus.edu
+ */
+public class RuntimeError {
+
+    /**
+     * Write lines and exit with -1 exit code.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Exiting with exit code '-1'");
+        System.err.println("Exiting with exit code '-1'");
+        System.exit(-1);
+    }
+}
