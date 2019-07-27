3b042095af215d1c99251225e9f29443f857fedb
john <clevenger@csus.edu>
2019-07-26 18:58:49 -0700

    bug 1466: added graphical Logo to About Pane.

--- src/edu/csus/ecs/pc2/ui/AboutPane.java
+++ src/edu/csus/ecs/pc2/ui/AboutPane.java
@@ -3,17 +3,22 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
 
 import edu.csus.ecs.pc2.VersionInfo;
 import edu.csus.ecs.pc2.core.ClipboardUtilities;
-import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
 
 /**
  * Show information about the pc2 version and run time environment.
@@ -34,22 +39,52 @@ public class AboutPane extends JPanePlugin {
     public AboutPane() {
         setLayout(new BorderLayout(0, 0));
         
+        
+        JPanel titleBarPanel = new JPanel();
+        add(titleBarPanel, BorderLayout.NORTH);
+        
+        JLabel lblAboutPc = new JLabel("About PC^2");
+        lblAboutPc.setFont(new Font("Tahoma", Font.BOLD, 16));
+        titleBarPanel.add(lblAboutPc);
+        
         JPanel centerPanel = new JPanel();
         add(centerPanel);
         centerPanel.setLayout(new BorderLayout(0, 0));
+        
+        //create a panel to hold the PC2 graphical logo
+        JPanel logoPanel = new JPanel();
+        
+        //create the logo image from the images folder
+        BufferedImage pc2LogoImage = null;
+        try {
+            //TODO: read the image from the pc2.jar images folder
+            pc2LogoImage = ImageIO.read(new File("./images/PC2Logo135x135.png"));
+        } catch (IOException e1) {
+            //TODO: remove println
+            System.out.println ("Problem reading PC2 logo file.");
+            e1.printStackTrace();
+            try {
+                //TODO: read the image from the pc2.jar images folder
+                pc2LogoImage = ImageIO.read(new File("./images/csus_logo.png"));
+                System.out.println ("Found CSUS logo file");
+            } catch (IOException e2) {
+                //TODO: remove println
+                System.out.println ("Problem reading CSUS logo file.");
+                e2.printStackTrace();
+            }
+        }
+        
+        //add the logo image to the logo panel
+        JLabel logoLabel = new JLabel(new ImageIcon(pc2LogoImage));
+        logoPanel.add(logoLabel);
+        
+        //add the logoPanel to the center panel
+        centerPanel.add(logoPanel, BorderLayout.WEST);
         
         JTextPane informationPane = new JTextPane();
         informationPane.setFont(new Font("Courier New", Font.PLAIN, 12));
         JScrollPane scrollPane = new JScrollPane(informationPane);
         centerPanel.add(scrollPane);
-        
-        
-        JPanel panel = new JPanel();
-        add(panel, BorderLayout.NORTH);
-        
-        JLabel lblAboutPc = new JLabel("About PC^2");
-        lblAboutPc.setFont(new Font("Tahoma", Font.BOLD, 16));
-        panel.add(lblAboutPc);
         
         JPanel buttonPanel = new JPanel();
         add(buttonPanel, BorderLayout.SOUTH);
@@ -111,8 +146,11 @@ public class AboutPane extends JPanePlugin {
         
         JFrame frame = new JFrame();
         AboutPane pane = new AboutPane();
-        frame.add(pane);
+        frame.getContentPane().add(pane);
         frame.setSize(600, 400);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         frame.setVisible(true);
     }
 }
