9163877614e1c7d24e480bcf0d7a5f6f35869763
john <clevenger@csus.edu>
2019-11-24 12:45:09 -0800

    bug 1624: multiple updates to MockContestAPIAdapter.
    Discarded saving of constructor parameter values which are not used (the
    Mock has no need to save the Remote CCS URL for example).
    
    Updated the JSON strings returned for judgementTypes, languages, etc.
    (removed hard-coded CR/LF sequences which came from copying the JSON off
    the CLICS web page examples).
    
    Added method getJSONConfigurationString() to return an array containing
    the configuration elements for the "mock remote CCS".

--- src/edu/csus/ecs/pc2/shadow/MockContestAPIAdapter.java
+++ src/edu/csus/ecs/pc2/shadow/MockContestAPIAdapter.java
@@ -1,6 +1,7 @@
 // Copyright (C) 1989-2019 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.shadow;
 
+import java.io.InputStream;
 import java.net.URL;
 
 /**
@@ -12,19 +13,15 @@ import java.net.URL;
  */
 public class MockContestAPIAdapter implements IRemoteContestAPIAdapter {
 
-    private URL remoteURL;
-    private String login;
-    private String password;
-
-    /**
-     * @param remoteURL
-     * @param login
-     * @param password
+    /** A mock implementation of the RemoteContestAPIAdapter.
+     * Does not use the received parameters; instead, returns "mock data".
+     * 
+     * @param remoteURL - unused; required for interface compliance
+     * @param login     - unused; required for interface compliance
+     * @param password  - unused; required for interface compliance
      */
     public MockContestAPIAdapter(URL remoteURL, String login, String password) {
-        this.remoteURL = remoteURL;
-        this.login = login;
-        this.password = password;
+
     }
 
     /**
@@ -32,26 +29,26 @@ public class MockContestAPIAdapter implements IRemoteContestAPIAdapter {
      */
     @Override
     public String getRemoteContestConfiguration() {
-       String judgementTypes = "[{\r\n" + 
-               "    \"id\": \"CE\",\r\n" + 
-               "    \"name\": \"Compiler Error\",\r\n" + 
-               "    \"penalty\": false,\r\n" + 
-               "    \"solved\": false\r\n" + 
-               "}, {\r\n" + 
-               "    \"id\": \"AC\",\r\n" + 
-               "    \"name\": \"Accepted\",\r\n" + 
-               "    \"penalty\": false,\r\n" + 
-               "    \"solved\": true\r\n" + 
+       String judgementTypes = "[{" + 
+               "    \"id\": \"CE\"," + 
+               "    \"name\": \"Compiler Error\"," + 
+               "    \"penalty\": false," + 
+               "    \"solved\": false" + 
+               "}, {" + 
+               "    \"id\": \"AC\"," + 
+               "    \"name\": \"Accepted\"," + 
+               "    \"penalty\": false," + 
+               "    \"solved\": true" + 
                "}]" ;
        String languages = "[{\r\n" + 
-               "    \"id\": \"java\",\r\n" + 
-               "    \"name\": \"Java\"\r\n" + 
-               "}, {\r\n" + 
-               "    \"id\": \"cpp\",\r\n" + 
-               "    \"name\": \"GNU C++\"\r\n" + 
-               "}, {\r\n" + 
-               "    \"id\": \"python2\",\r\n" + 
-               "    \"name\": \"Python 2\"\r\n" + 
+               "    \"id\": \"java\"," + 
+               "    \"name\": \"Java\"" + 
+               "}, {" + 
+               "    \"id\": \"cpp\"," + 
+               "    \"name\": \"GNU C++\"" + 
+               "}, {" + 
+               "    \"id\": \"python2\"," + 
+               "    \"name\": \"Python 2\"" + 
                "}]";
        String problems = "[{\"id\":\"asteroids\",\"label\":\"A\",\"name\":\"Asteroid Rangers\",\"ordinal\":1,\"color\":\"blue\",\"rgb\":\"#00f\",\"time_limit\":2,\"test_data_count\":10},\r\n" + 
                "  {\"id\":\"bottles\",\"label\":\"B\",\"name\":\"Curvy Little Bottles\",\"ordinal\":2,\"color\":\"gray\",\"rgb\":\"#808080\",\"time_limit\":3.5,\"test_data_count\":15}\r\n" + 
@@ -67,24 +64,84 @@ public class MockContestAPIAdapter implements IRemoteContestAPIAdapter {
        String teams = "[{\"id\":\"11\",\"icpc_id\":\"201433\",\"name\":\"Shanghai Tigers\",\"organization_id\":\"inst123\",\"group_ids\":[\"asia-74324325532\"]},\r\n" + 
                "  {\"id\":\"123\",\"name\":\"CMU1\",\"organization_id\":\"inst105\",\"group_ids\":[\"8\",\"11\"]}\r\n" + 
                " ]";
-       String contestState = "{\r\n" + 
-               "   \"started\": \"2014-06-25T10:00:00+01\",\r\n" + 
-               "   \"ended\": null,\r\n" + 
-               "   \"frozen\": \"2014-06-25T14:00:00+01\",\r\n" + 
-               "   \"thawed\": null,\r\n" + 
-               "   \"finalized\": null,\r\n" + 
-               "   \"end_of_updates\": null\r\n" + 
+       String contestState = "{" + 
+               "   \"started\": \"2014-06-25T10:00:00+01\"," + 
+               "   \"ended\": null," + 
+               "   \"frozen\": \"2014-06-25T14:00:00+01\"," + 
+               "   \"thawed\": null," + 
+               "   \"finalized\": null," + 
+               "   \"end_of_updates\": null" + 
                " }";
-       String retStr = "["
-               + judgementTypes + ","
-               + languages + ","
-               + problems + ","
-               + groups + ","
-               + organizations + ","
-               + teams + ","
-               + contestState
-               + "]";
+       
+       String retStr = getJSONConfigurationString(judgementTypes,languages,problems,groups,organizations,teams,contestState);
+       
         return retStr;
+    }
+
+
+    @Override
+    public InputStream getRemoteContestEventFeed() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    /** Returns a JSON string defining an array whose elements are made up of the specified CLICS Contest API endpoint values.
+     * 
+     * For example, the element in the array corresponding to the received value "problems" is a JSON string with the key "problems"
+     * and a value defined by the received parameter "problems".
+     * 
+     * @param judgementTypes
+     * @param languages
+     * @param problems
+     * @param groups
+     * @param organizations
+     * @param teams
+     * @param contestState
+     * @return
+     */
+    private String getJSONConfigurationString(String judgementTypes, String languages, String problems, String groups, String organizations, String teams, String contestState) {
+
+        String retStr = "[";
+        retStr += getArrayElement("judgementTypes",judgementTypes);
+        retStr += ",\r\n";
+        retStr += getArrayElement("languages",languages);
+        retStr += ",\r\n";
+        retStr += getArrayElement("problems",problems);
+        retStr += ",\r\n";
+        retStr += getArrayElement("groups",groups);
+        retStr += ",\r\n";
+        retStr += getArrayElement("organizations",organizations);
+        retStr += ",\r\n";
+        retStr += getArrayElement("teams",teams);
+        retStr += ",\r\n";
+        retStr += getArrayElement("contestState",contestState);
+        retStr += "]";
+        
+        return retStr;
+    }
+
+    /**
+     * Returns a JSON array element (surrounded by square brackets) consisting of the specified keyword
+     * and with a value defined by the received value parameter.
+     * 
+     * @param retStr the String to which a JSON array element is to be added
+     * @param keyword the JSON keyword for the element to be added
+     * @param value a JSON string defining the value of the added element
+     */
+    private String getArrayElement(String keyword, String value) {
+
+        String newStr = "[";
+        newStr += "\"" + keyword + "\":" + "\"" + value + "\"";
+        newStr += "]";
+        
+        return newStr ;
+        
+    }
+
+    public static void main (String [] args) {
+        System.out.println ("MockContestAPIAdapter:");
+        System.out.println ("   Mock contest configuration:");
+        System.out.println (new MockContestAPIAdapter(null, null, null).getRemoteContestConfiguration());
     }
 
 }
