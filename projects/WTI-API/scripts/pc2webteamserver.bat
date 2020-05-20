8d3491f3dab90e86d0e73d5d1f26779fa2712c9d
john <clevenger@csus.edu>
2020-05-19 19:12:09 -0700

    i80: This is a squash of 5 commits.
    The first commit deleted duplicate batch files, and also
    cleaned up cruft in both DOS and Unix files.
    
    Second commit: renamed batch files to "pc2wti".
    
    Third commit: corrected file name in comments.
    
    Fourth commit: restored CRLF eol chars to DOS pc2wti.bat
    
    Fifth commit: moved the pc2wti scripts to the WTI-API project.
    
    i80: moved pc2wti scripts into WTI-API project.
    
    Also restored CRLF eol in packageWTI.xml.

--- projects/WTI-API/scripts/pc2webteamserver.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Copyright (C) 1989-2020 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
-
-rem Purpose: start the WebTeamInterface (WTI) project web team server module
-rem Author : pc2@ecs.csus.edu
-
-java -Djdk.crypto.KeyAgreement.legacyKDF=true -Xms64M -Xmx2048M -jar WebTeamInterface-1.1.jar
-
-rem eof pc2webteamserver.bat 
