0d5c6036fcd97d3e49b20bed06af373ca317f210
Douglas A. Lane <laned@ecs.csus.edu>
2021-07-31 17:12:16 -0700

    i276: writes groups.tsv file, added --if option, -f option
    Groups from teams.json written to groups.tsv
    add unit test
    update usage

--- src/edu/csus/ecs/pc2/ui/team/CreateTeamsTSV.java
+++ src/edu/csus/ecs/pc2/ui/team/CreateTeamsTSV.java
@@ -6,7 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -26,13 +29,19 @@ import edu.csus.ecs.pc2.core.imports.clics.TeamAccount;
 
 public class CreateTeamsTSV {
 
+    public static final String GROUPS_TSV_FILENAME = "groups.tsv";
+
     private static final String TAB = "\t";
 
-    private static final String TEAMS_TSV_FILENAME = "teams.tsv";
+    public static final String TEAMS_TSV_FILENAME = "teams.tsv";
 
-    private static final String TEAMS_JSON_FILENAME = "teams.json";
+    public static final String TEAMS_JSON_FILENAME = "teams.json";
 
     private static final String OUTPUT_FILE_KEY = "--of";
+    
+    private static final String INPUT_FILE_KEY = "--if";
+    
+    private static final String OVERWRITE_FILE_KEY = "-f";
 
     /**
      * Successful run exit code.
@@ -46,14 +55,23 @@ public class CreateTeamsTSV {
     private static final int FAILURE_EXIT_CODE = 4;
 
     private boolean debugMode = false;
+    
+    private boolean allowOverWrite = false;
 
     /**
      * Default output file name
      */
     private String outputFileName = TEAMS_TSV_FILENAME;
 
-    public CreateTeamsTSV(String[] args) throws CommandLineErrorException {
+    /**
+     * Count for group ids.
+     */
+    private Map<String,Integer> groupMap = new HashMap<String, Integer>();
 
+    private String inputFileName = TEAMS_JSON_FILENAME;
+
+    public CreateTeamsTSV(String[] args) throws CommandLineErrorException {
+        loadVariables(args);
     }
 
     @SuppressWarnings("unused")
@@ -65,7 +83,8 @@ public class CreateTeamsTSV {
 
     protected void loadVariables(String[] args) throws CommandLineErrorException {
 
-        ParseArguments arguments = new ParseArguments(args);
+        String[] argsWithValues = { OUTPUT_FILE_KEY, INPUT_FILE_KEY};
+        ParseArguments arguments = new ParseArguments(args, argsWithValues);
 
         if (arguments.isOptPresent("--help")) {
             usage();
@@ -73,6 +92,10 @@ public class CreateTeamsTSV {
         }
 
         debugMode = arguments.isOptPresent("--debug");
+        
+        
+        allowOverWrite = arguments.isOptPresent(OVERWRITE_FILE_KEY);
+        
 
         if (debugMode) {
             arguments.dumpArgs(System.err);
@@ -81,6 +104,11 @@ public class CreateTeamsTSV {
         if (arguments.isOptPresent(OUTPUT_FILE_KEY)) {
             outputFileName = arguments.getOptValue(OUTPUT_FILE_KEY);
         }
+        
+        if (arguments.isOptPresent(INPUT_FILE_KEY)) {
+            inputFileName = arguments.getOptValue(INPUT_FILE_KEY);
+        }
+        
 
     }
 
@@ -93,12 +121,17 @@ public class CreateTeamsTSV {
     private static void usage() {
         String[] usage = { //
                 "", //
-                "Usage CreateTeamsTSV [--help] [--of outputfilename]", //
+                "Usage CreateTeamsTSV [--help] [-f] [--of outputfilename] [--if teams_file.json]", //
                 "", //
-                "Purpose: read teams.json file create teams.tsv file.", //
+                "Purpose: read teams.json file create teams.tsv and groups.tsv files.", //
                 "", //
-                "where:", //
-                "--of file  - alternate output filename, default output filename is " + TEAMS_TSV_FILENAME, "", //
+                "This will not overwrite output file, use -f to overwrite output file (and "+GROUPS_TSV_FILENAME+")", //
+                "", //
+                "options:", //
+                "--of file  - alternate output filename, default output filename is " + TEAMS_TSV_FILENAME, //
+                "--if file  - alternate input JSON filename, default  filename is " + TEAMS_JSON_FILENAME, //
+                     "-f   overwrite output file, by default will not overwrite output file.", //
+                "", //
                 "On success exit code will be " + SUCCESS_EXIT_CODE, //
                 "Any other exit code is an error.", //
                 "", //
@@ -109,13 +142,13 @@ public class CreateTeamsTSV {
         }
     }
 
-    private void writeTSVFile() throws Exception {
+    protected void writeTSVFile() throws Exception {
 
-        if (new File(outputFileName).exists()) {
+        if (!allowOverWrite  && new File(outputFileName).exists()) {
             fatalError("Output file exists (" + outputFileName + "), will not overwrite.  Use " + OUTPUT_FILE_KEY + " to write to a different file");
         }
 
-        List<TeamAccount> accounts = loadAccounts(TEAMS_JSON_FILENAME);
+        List<TeamAccount> accounts = loadAccounts(inputFileName);
 
         String [] header = { "Teams", "1" };
 
@@ -133,6 +166,13 @@ public class CreateTeamsTSV {
             if (teamAccount.getGroup_ids().size() > 0) {
                 groupId = teamAccount.getGroup_ids().get(0);
             }
+            
+            Integer value = groupMap.get(groupId);
+            if (value == null) {
+                value = 0;
+            }
+            groupMap.put(groupId, value.intValue()+1);
+            
             /**
                      1  Team Number     22  integer
 2   External ID     24314   integer
@@ -173,17 +213,48 @@ public class CreateTeamsTSV {
         return list;
     }
     
+    protected void updateGroupsTSV() throws IOException {
+
+        String outDir = new File(outputFileName).getParent();
+        if (outDir == null) {
+            outDir = ".";
+        }
+        String outGroupName = outDir + File.separator + GROUPS_TSV_FILENAME;
+        FileWriter writer = (new FileWriter(new File(outGroupName)));
+        
+        String[] groupHeaerFields = { "groups", "1" };
+
+        writer.write(String.join(TAB, groupHeaerFields) + NL);
+
+        Set<String> groupIds = groupMap.keySet();
+        String[] ids = (String[]) groupIds.toArray(new String[groupIds.size()]);
+        Arrays.sort(ids);
+        
+        for (String id : ids) {
+            writer.write(id + TAB + "Group " + id + NL);
+        }
+        
+        writer.close();
+        writer = null;
+
+    }
+    
     public String getOutputFileName() {
         return outputFileName;
+    }
+    
+    public String getInputFileName() {
+        return inputFileName;
     }
 
     public static void main(String[] args) {
 
         try {
             CreateTeamsTSV creator = new CreateTeamsTSV(args);
-            System.out.println("Input: "+TEAMS_JSON_FILENAME);
+            System.out.println("Input: "+creator.getInputFileName());
             creator.writeTSVFile();
-            System.out.println("Output: "+creator.getOutputFileName());
+            System.out.println("Output: "+creator.getOutputFileName() + ", " + GROUPS_TSV_FILENAME);
+            creator.updateGroupsTSV();
             System.exit(SUCCESS_EXIT_CODE);
         } catch (CommandLineErrorException e) {
             System.err.println("Error on command line: " + e.getMessage());
@@ -192,4 +263,6 @@ public class CreateTeamsTSV {
             e.printStackTrace(System.err);
         }
     }
+
+ 
 }
--- test/edu/csus/ecs/pc2/ui/team/CreateTeamsTSVTest.java
+++ test/edu/csus/ecs/pc2/ui/team/CreateTeamsTSVTest.java
@@ -21,8 +21,9 @@ public class CreateTeamsTSVTest extends AbstractTestCase {
     public void testloadAccounts() throws Exception {
 
         String dataDir = getDataDirectory(this.getName());
-        String name = dataDir + File.separator + "teams.json";
+        String name = dataDir + File.separator + CreateTeamsTSV.TEAMS_JSON_FILENAME;
 
+        assertFileExists(name);
 //        ensureDirectory(dataDir);
 //        editFile(name);
 
@@ -33,6 +34,33 @@ public class CreateTeamsTSVTest extends AbstractTestCase {
 
         assertNotNull(list);
         assertEquals("Expected teams", 171, list.size());
+    }
+    
+    public void testWriteTSVFiles() throws Exception {
+        String dataDir = getDataDirectory("testloadAccounts");
+        String name = dataDir + File.separator + CreateTeamsTSV.TEAMS_JSON_FILENAME;
+        
+        assertFileExists(name);
+//        startExplorer(dataDir);
+        
+        String outdir = getOutputDataDirectory(this.getName());
+        String outName = outdir + File.separator + CreateTeamsTSV.TEAMS_TSV_FILENAME;
+
+        removeDirectory(outdir);
+        ensureDirectory(outdir);
+
+        String[] args = { "--of", outName, "--if", name};
+        new CreateTeamsTSV(args);
+        CreateTeamsTSV creator = new CreateTeamsTSV(args);
+        creator.writeTSVFile();
+        creator.updateGroupsTSV();
+
+        assertFileExists(outName);
+        String grOutName = outdir + File.separator + CreateTeamsTSV.GROUPS_TSV_FILENAME;
+        assertFileExists(grOutName);
+        
+        startExplorer(outdir);
+//      editFile(name);
 
     }
 
