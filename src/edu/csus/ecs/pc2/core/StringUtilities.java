904b1ad14044a88237fbb04d2db30cb106f9648c
Douglas A. Lane <laned@ecs.csus.edu>
2022-12-04 08:23:30 -0800

    i593: add support for number ranges in yaml
    Now loads number list with using StringUtilities.getNumberList
    which now supports ranges of values.

--- src/edu/csus/ecs/pc2/core/StringUtilities.java
+++ src/edu/csus/ecs/pc2/core/StringUtilities.java
@@ -1,7 +1,8 @@
-// Copyright (C) 1989-2019 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+// Copyright (C) 1989-2022 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.core;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -274,6 +275,85 @@ public final class StringUtilities implements Serializable {
         }
 
         return value;
+    }
+    
+    /**
+     * Return a list of integers for input string.
+     * 
+     * Input string is a list of comma delimited numbers or ranges.
+     * 
+     * Invalid number conditions
+     * <li> range end must be greater than or equal to start number in range
+     * <li> all numbers must be greater than 0
+     * <li> missing start range number, ex -12
+     * <li> missing end range number, ex. 22-
+     * <li> missing number, ex 1,2,,5
+     * <li> too many dashes in a range, ex 1-5-6-7
+     * 
+     * @throws RuntimeException if invalid range
+     * @param numberString
+     * @return raw (not sorted, not unique) list of numbers 
+     */
+    public static int[] getNumberList(String numberString) {
+
+        // TODO REFACTOR replace ContestSnakeYAMLLoader.getNumberList with this method
+        
+        String[] list = numberString.split(",");
+        List<Integer> outList = new ArrayList<Integer>();
+        
+        for (String numberItem : list) {
+
+            String trimmed = numberItem.replaceAll(" ", ""); // remove all spaces
+            trimmed = trimmed.replaceAll("^0", ""); // remove left padding zero
+            
+            if (0 == trimmed.length()) {
+                throw new RuntimeException("Invalid/missing number in range, '" + trimmed + "' input: '" + numberString + "'");
+            }
+            
+            if (trimmed.indexOf('-') > -1) {
+                if (trimmed.startsWith("-")) {
+                    throw new RuntimeException("Invalid range, missing range start number '" + trimmed + "' input: '" + numberString + "'");
+                }
+                if (trimmed.endsWith("-")) {
+                    throw new RuntimeException("Invalid range, missing range end number '" + trimmed + "' input: '" + numberString + "'");
+                }
+
+                String[] ranges = trimmed.split("-");
+                if (ranges.length != 2) {
+                    throw new RuntimeException("Invalid range format (too many dashes?) '" + trimmed + "' input: '" + numberString + "'");
+                }
+
+                int startRange = getIntegerValue(ranges[0], 0);
+                int endRange = getIntegerValue(ranges[1], 0);
+
+                if (endRange < startRange) {
+                    throw new RuntimeException("Invalid range range end number must be >= start range'" + trimmed + "' input: '" + numberString + "'");
+                }
+
+                for (int i = startRange; i <= endRange; i++) {
+                    outList.add(i);
+                }
+
+            } else {
+                outList.add(getIntegerValue(trimmed, 0));
+            }
+        }
+        int [] outArray = outList.stream() //
+                .mapToInt(Integer::intValue) //
+                .toArray();
+        return outArray;
+    }
+    
+    public static int getIntegerValue(String string, int defaultNumber) {
+        // TODO REFACTOR replace ContestSnakeYAMLLoader.getIntegerValue with this method
+
+        int number = defaultNumber;
+
+        if (string != null && string.length() > 0) {
+            number = Integer.parseInt(string.trim());
+        }
+
+        return number;
     }
     
 }
--- test/edu/csus/ecs/pc2/core/StringUtilitiesTest.java
+++ test/edu/csus/ecs/pc2/core/StringUtilitiesTest.java
@@ -1,5 +1,7 @@
-// Copyright (C) 1989-2019 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+// Copyright (C) 1989-2022 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.core;
+
+import java.util.Arrays;
 
 import junit.framework.TestCase;
 
@@ -61,4 +63,69 @@ public class StringUtilitiesTest extends TestCase {
         
         
     }
+    
+    /**
+     * Test getNumberList
+     * @throws Exception
+     */
+    public void testgetNumberList() throws Exception {
+
+        String [] data = { //
+                "1;[1]", // 
+                "1,2,3,6-12;[1, 2, 3, 6, 7, 8, 9, 10, 11, 12]", //
+                "5-5;[5]", //
+                "  5  -  5   ;[5]", //
+                "1,3,20-26;[1, 3, 20, 21, 22, 23, 24, 25, 26]", //
+                "1, 2 ,3;[1, 2, 3]", //
+                "4-18;[4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]", // 
+                "4-12,1,5,6;[4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 5, 6]", //
+                "100-102,123, 321 ;[100, 101, 102, 123, 321]", //
+                "1,4,12-15,22;[1, 4, 12, 13, 14, 15, 22]", //
+                
+        };
+        
+        for (String string : data) {
+            String [] values =string.split(";");
+            String numberString =values[0];
+            String expected =values[1].trim();
+
+            int[] out = StringUtilities.getNumberList(numberString);
+//            System.out.println("new data \""+numberString+";"+Arrays.toString(out)+"\", // ");
+            assertEquals("Expected range for "+numberString, expected, Arrays.toString(out));
+        }
+    }
+    
+    /**
+     * Test getNumberList for invalid input number strings.
+     * 
+     * @throws Exception
+     */
+    public void testgetNumberListNegative() throws Exception {
+
+        String[] data = { //
+                "-", //
+                "2,-4", // missing start number
+                "1,2,3,55-,6", // missing end number
+                "20-12", // start number greater than end range number
+                "1,2,4-5-6,20", // too many dashes
+                "", // no list at all
+                "            ", // lots of space
+                "1,2,,4,5", // missing number
+                "1,2,3,     ,7,8", // missing number
+                "0-5;[5]", //
+        };
+
+        for (String inString : data) {
+            try {
+                String numberString = inString;
+                StringUtilities.getNumberList(numberString);
+
+                fail("Expecting failure for string: " + inString);
+            } catch (Exception e) {
+                ; // Passes unit test
+            }
+        }
+
+    }
+    
 }
