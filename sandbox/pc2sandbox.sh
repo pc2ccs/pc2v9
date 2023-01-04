b12a1c6cebf30c5dffab1c2959cb5ef344218949
John Buck <johnbrvc@gmail.com>
2023-01-04 18:25:05 -0500

    Updates for Cgroups v2
    Get rid of extra layer of pc2sandbox.  Use sandbox_$USER instead.
    Change owner/mode of /sys/fs/cgroup/cgroup.procs to allow switching from /sys/fs/cgroup/user-slice groups.
    Automatically enable  +cpu in cgroup.subtree_control

--- sandbox/pc2sandbox.sh
+++ sandbox/pc2sandbox.sh
@@ -21,10 +21,9 @@ FAIL_MEMORY_CONTROLLER_NOT_ENABLED=50
 
 CGROUP_PATH=/sys/fs/cgroup
 PC2_CGROUP_PATH=$CGROUP_PATH/pc2
-PC2_SANDBOX_CGROUP_PATH=$PC2_CGROUP_PATH/pc2sandbox
 # TODO: should have unique runbox in case multiple instances are executing in separate judges
 # For now, just use userid
-PC2_SANDBOX_RUN_CGROUP_PATH=$PC2_CGROUP_PATH/pc2sandbox/runbox_$USER
+PC2_SANDBOX_CGROUP_PATH=$PC2_CGROUP_PATH/sandbox_$USER
 
 # control whether the script outputs debug/tracing info
 _DEBUG="on"   # change this to anything other than "on" to disable debug/trace output
@@ -69,9 +68,9 @@ shift
 shift
 shift
 
-### Debugging - just set expected first 3 args to: 16MB 5seconds
-###MEMLIMIT=16
-###TIMELIMIT=5
+#### Debugging - just set expected first 3 args to: 16MB 5seconds
+###MEMLIMIT=8
+###TIMELIMIT=2
 ###COMMAND=$1
 ###shift
 
@@ -80,8 +79,8 @@ shift
 # make sure we have CGroups V2 properly installed on this system, including a PC2 structure
 
 DEBUG echo checking PC2 CGroup V2 installation...
-if [ ! -d "$PC2_SANDBOX_CGROUP_PATH" ]; then
-   echo $0: expected pc2sandbox CGroups v2 installation in $PC2_SANDBOX_CGROUP_PATH 
+if [ ! -d "$PC2_CGROUP_PATH" ]; then
+   echo $0: expected pc2sandbox CGroups v2 installation in $PC2_CGROUP_PATH 
    exit $FAIL_INVALID_CGROUP_INSTALLATION
 fi
 
@@ -110,6 +109,15 @@ fi
 # we seem to have a valid CGroup installation
 DEBUG echo ...done.
 
+if test -d $PC2_SANDBOX_CGROUP_PATH
+then
+	DEBUG echo Removing existing sandbox to start clean
+	rmdir $PC2_SANDBOX_CGROUP_PATH
+fi
+
+DEBUG echo Creating sandbox $PC2_SANDBOX_CGROUP_PATH
+mkdir $PC2_SANDBOX_CGROUP_PATH
+
 # set the specified memory limit - input is in MB, cgroup v2 requires bytes, so multiply by 1M
 # but only if > 0.
 # "max" means unlimited, which is the cgroup v2 default
@@ -131,26 +139,16 @@ ulimit -t $TIMELIMIT
 TIMELIMIT_US=$((TIMELIMIT * 1000000))
 #echo $TIMELIMIT_US  > $PC2_SANDBOX_RUN_CGROUP_PATH/cpu.max
 
-
-# Avoid "no internal processes" rule
-DEBUG echo creating run sub-sandbox $PC2_SANDBOX_RUN_CGROUP_PATH
-if test -d "$PC2_SANDBOX_RUN_CGROUP_PATH"
-then
-	rmdir $PC2_SANDBOX_RUN_CGROUP_PATH
-fi
-mkdir $PC2_SANDBOX_RUN_CGROUP_PATH
-
-# TODO: need to remove the requirement for sudo in the following command, which is needed here because
-# this shell is in the root cgroup by default and does not have write access to the root cgroup.procs file --
-# which it never should.
-# Need to have a way to put this shell into the pc2/pc2sandbox/cgroup.procs file as root, once.
-# See https://man7.org/conf/ndctechtown2021/cgroups-v2-part-2-diving-deeper-NDC-TechTown-2021-Kerrisk.pdf,
-# slides 25-28.
-#
+DEBUG echo setting maximum user processes to 32
+ulimit -u 32
 
 #put the current process (and implicitly its children) into the pc2sandbox cgroup.
-DEBUG echo putting $$ into $PC2_SANDBOX_RUN_CGROUP_PATH cgroup
-sudo echo $$ > $PC2_SANDBOX_RUN_CGROUP_PATH/cgroup.procs
+DEBUG echo putting $$ into $PC2_SANDBOX_CGROUP_PATH cgroup
+if ! echo $$ > $PC2_SANDBOX_CGROUP_PATH/cgroup.procs
+then
+	echo Failed. quitting.
+	exit 1
+fi
 
 # run the command
 # the following are the cgroup-tools V1 commands; need to find cgroup-tools v2 commands
@@ -167,13 +165,13 @@ COMMAND_EXIT_CODE=$?
 
 if test "$COMMAND_EXIT_CODE" -ge 128
 then
-	kills=`grep oom_kill $PC2_SANDBOX_RUN_CGROUP_PATH/memory.events | cut -d ' ' -f 2`
+	kills=`grep oom_kill $PC2_SANDBOX_CGROUP_PATH/memory.events | cut -d ' ' -f 2`
 	if test "$kills" != "0"
 	then
 		DEBUG echo The command was killed due to out of memory
 	else
 		# Get cpu time
-		cputime=`grep usage_usec $PC2_SANDBOX_RUN_CGROUP_PATH/cpu.stat | cut -d ' ' -f 2`
+		cputime=`grep usage_usec $PC2_SANDBOX_CGROUP_PATH/cpu.stat | cut -d ' ' -f 2`
 		if test "$cputime" -gt "$TIMELIMIT_US"
 		then
 			DEBUG echo The command was killed due to out of CPU Time "($cputime > $TIMELIMIT_US)"
