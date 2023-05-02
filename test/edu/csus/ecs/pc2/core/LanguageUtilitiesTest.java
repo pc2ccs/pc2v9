c6334b5cff1ca14109a19c95fcd3402ce300a208
Douglas A. Lane <laned@csus.edu>
2023-05-02 00:58:11 -0700

    i742: added unit test for matchFirstLanguage
    Added test to match model Language for C++ language (display name)
    Added methods to find file names, recursively, getAllFileEntries
    Added methods to find dir names names, recursively,
    getAllDirectoryEntries

--- test/edu/csus/ecs/pc2/core/LanguageUtilitiesTest.java
+++ test/edu/csus/ecs/pc2/core/LanguageUtilitiesTest.java
@@ -1,6 +1,8 @@
 // Copyright (C) 1989-2023 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.core;
 
+import java.util.List;
+
 import edu.csus.ecs.pc2.core.model.IInternalContest;
 import edu.csus.ecs.pc2.core.model.Language;
 import edu.csus.ecs.pc2.core.util.AbstractTestCase;
@@ -12,7 +14,12 @@ import edu.csus.ecs.pc2.core.util.AbstractTestCase;
  */
 public class LanguageUtilitiesTest extends AbstractTestCase {
 
-    public void testname() throws Exception {
+    /**
+     * Test whether in mini contest C++ extensions find C++ languages.
+     * 
+     * @throws Exception
+     */
+    public void testmatchFirstLanguage() throws Exception {
 
         IInternalContest contest = loadContestFromSampleContsts(null, "mini");
         assertNotNull(contest);
@@ -34,5 +41,35 @@ public class LanguageUtilitiesTest extends AbstractTestCase {
             assertEquals("Expecting language name", expected, actual);
         }
     }
+    
+    
+    /**
+     * Test whether in contest C++ extension cc will find C++ languages.
+     * 
+     * @throws Exception
+     */
+    public void testmatchFirstLanguageForCC() throws Exception {
 
+        IInternalContest contest = loadContestFromSampleContsts(null, "qanat");
+        assertNotNull(contest);
+
+        String cdpDir = getTestSampleContestDirectory("qanat");
+
+        List<String> allCCSamples = FileUtilities.getAllFileEntries(cdpDir, ".cc");
+        assertEquals("Expected .cc files under " + cdpDir, 8, allCCSamples.size());
+
+        /**
+         * Expected language display name.
+         */
+        String expected = "GNU C++";
+
+        for (String input : allCCSamples) {
+
+            String extension = LanguageUtilities.getExtension(input);
+            Language lang = LanguageUtilities.matchFirstLanguage(contest, extension);
+            assertNotNull("Expecting to match contest model Language", lang);
+            String actual = lang.getDisplayName();
+            assertEquals("Expecting language name", expected, actual);
+        }
+    }
 }
