227df4e5e8294b36de42e0bdf13632c44e817ae0
Douglas A. Lane <laned@csus.edu>
2023-09-10 14:54:38 -0700

    i760: Add file and field comparison classes

---
+++ src/edu/csus/ecs/pc2/core/imports/clics/FieldCompareRecord.java
@@ -0,0 +1,130 @@
+// Copyright (C) 1989-2023 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+package edu.csus.ecs.pc2.core.imports.clics;
+
+import java.util.logging.Level;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import edu.csus.ecs.pc2.core.log.StaticLog;
+import edu.csus.ecs.pc2.core.model.JSONObjectMapper;
+import edu.csus.ecs.pc2.core.report.ComparisonState;
+
+/**
+ * Information about a comparison of two fields.
+ * 
+ * @author Douglas A. Lane, PC^2 team pc2@ecs.csus.edu
+ */
+public class FieldCompareRecord {
+
+    @JsonProperty
+    private ComparisonState state = ComparisonState.UNDEFINED;
+
+    @JsonProperty
+    private String fieldName;
+
+    @JsonProperty
+    private String valueOne;
+
+    @JsonProperty
+    private String valueTwo;
+
+    @JsonProperty
+    private String details;
+
+    public FieldCompareRecord(String fieldName, String valueOne, String valueTwo, String details) {
+        super();
+        this.fieldName = fieldName;
+        this.valueOne = valueOne;
+        this.valueTwo = valueTwo;
+        this.details = details;
+
+        createComparisonState(valueOne, valueTwo);
+    }
+
+    public void createComparisonState(String value1, String value2) {
+//        TODO REFACTOR move this into ComparisonState
+
+        if (value1 == null && value2 == null) {
+            state = ComparisonState.BOTH_MISSING;
+        } else if (value1 == null) {
+            state = ComparisonState.MISSING_SOURCE;
+        } else if (value2 == null) {
+            state = ComparisonState.MISSING_TARGET;
+        } else if (value1.contentEquals(value2)) {
+            state = ComparisonState.SAME;
+        } else {
+            state = ComparisonState.NOT_SAME;
+        }
+    }
+
+    public ComparisonState getState() {
+        return state;
+    }
+
+    public void setState(ComparisonState state) {
+        this.state = state;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getValueOne() {
+        return valueOne;
+    }
+
+    public void setValueOne(String valueOne) {
+        this.valueOne = valueOne;
+    }
+
+    public String getValueTwo() {
+        return valueTwo;
+    }
+
+    public void setValueTwo(String valueTwo) {
+        this.valueTwo = valueTwo;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    /**
+     * Returns a JSON string representation of this SerializedFile object.
+     * 
+     * @return a String containing a JSON representation of this object, or null if an error occurs during creation of the JSON string
+     */
+    // TODO REFACTOR Move into a JSON Utility class
+    public String toJSON() {
+
+        // TODO REFACTOR Update getObjectMapper to use configs below.
+        ObjectMapper objectMapper = JSONObjectMapper.getObjectMapper();
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+
+        try {
+            String jsonString = objectMapper.writeValueAsString(this);
+            return jsonString;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            if (StaticLog.getLog() != null) {
+                StaticLog.getLog().log(Level.WARNING, "Error writing JSON for object " + this.getClass().getName(), e);
+            }
+            return null;
+        }
+    }
+
+}
---
+++ src/edu/csus/ecs/pc2/core/imports/clics/FileComparison.java
@@ -0,0 +1,117 @@
+// Copyright (C) 1989-2023 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+package edu.csus.ecs.pc2.core.imports.clics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import edu.csus.ecs.pc2.core.log.StaticLog;
+import edu.csus.ecs.pc2.core.model.JSONObjectMapper;
+import edu.csus.ecs.pc2.core.report.ComparisonState;
+
+/**
+ * Comparison information between two files.
+ * 
+ * @author Douglas A. Lane, PC^2 team pc2@ecs.csus.edu
+ */
+public class FileComparison {
+
+    @JsonProperty
+    private String firstFilename;
+
+    @JsonProperty
+    private String secondFilename;
+
+    @JsonProperty
+    long numberDifferences;
+
+    @JsonProperty
+    long numberComparedRecorderds;
+    
+    @JsonProperty
+    private List<FieldCompareRecord> comparedFields = new ArrayList<FieldCompareRecord>();
+
+    public FileComparison(String firstFilename, String secondFilename) {
+        super();
+        this.firstFilename = firstFilename;
+        this.secondFilename = secondFilename;
+    }
+
+    public FileComparison(String firstFilename, String secondFilename, List<FieldCompareRecord> comparedFields) {
+        super();
+        this.firstFilename = firstFilename;
+        this.secondFilename = secondFilename;
+        this.comparedFields = comparedFields;
+    }
+
+    public FieldCompareRecord addfieldCompareRecord(FieldCompareRecord fieldCompareRecord) {
+        
+        comparedFields.add(fieldCompareRecord);
+        
+        numberComparedRecorderds ++;
+        if (!ComparisonState.SAME.equals(fieldCompareRecord.getState())){
+                numberDifferences++;
+        }
+        return fieldCompareRecord;
+    }
+
+    public String getFirstFilename() {
+        return firstFilename;
+    }
+
+    public void setFirstFilename(String firstFilename) {
+        this.firstFilename = firstFilename;
+    }
+
+    public String getSecondFilename() {
+        return secondFilename;
+    }
+
+    public void setSecondFilename(String secondFilename) {
+        this.secondFilename = secondFilename;
+    }
+
+    public long getNumberDifferences() {
+        return numberDifferences;
+    }
+
+    public List<FieldCompareRecord> getComparedFields() {
+        return comparedFields;
+    }
+
+    public void setComparedFields(List<FieldCompareRecord> comparedFields) {
+        this.comparedFields = comparedFields;
+    }
+
+    /**
+     * Returns a JSON string representation of this SerializedFile object.
+     * 
+     * @return a String containing a JSON representation of this object, or null if an error occurs during creation of the JSON string
+     */
+    // TODO REFACTOR Move into a JSON Utility class
+    public String toJSON() {
+
+        // TODO REFACTOR Update getObjectMapper to use configs below.
+        ObjectMapper objectMapper = JSONObjectMapper.getObjectMapper();
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+
+        try {
+            String jsonString = objectMapper.writeValueAsString(this);
+            return jsonString;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            if (StaticLog.getLog() != null) {
+                StaticLog.getLog().log(Level.WARNING, "Error writing JSON for object " + this.getClass().getName(), e);
+            }
+            return null;
+        }
+    }
+}
