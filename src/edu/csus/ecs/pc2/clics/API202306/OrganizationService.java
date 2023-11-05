232f585cadbd5ccb6e5f39d4d961b52c43f7e300
John Buck <johnbrvc@gmail.com>
2023-11-05 17:31:54 -0500

    i842 Added CLICS Version 2023-06 API web server driver package
    Copied 2020-03 driver to the API202306 package.
    Implemented the Version endpoint since it's format is new (provider).
    Added code to fetch the available CLICS API version from the pc2v9.ini file.  If not found use defaults of 2023-06 and 2020-03.

---
+++ src/edu/csus/ecs/pc2/clics/API202306/OrganizationService.java
@@ -0,0 +1,103 @@
+// Copyright (C) 1989-2024 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+package edu.csus.ecs.pc2.clics.API202306;
+
+import java.util.Hashtable;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import edu.csus.ecs.pc2.core.IInternalController;
+import edu.csus.ecs.pc2.core.model.Account;
+import edu.csus.ecs.pc2.core.model.ClientType;
+import edu.csus.ecs.pc2.core.model.IInternalContest;
+import edu.csus.ecs.pc2.core.util.JSONTool;
+
+/**
+ * WebService for handling teams
+ * 
+ * @author ICPC
+ *
+ */
+@Path("/contest/organizations")
+@Produces(MediaType.APPLICATION_JSON)
+@Provider
+@Singleton
+public class OrganizationService implements Feature {
+
+    private IInternalContest model;
+
+    private IInternalController controller;
+
+    private JSONTool jsonTool;
+
+    public OrganizationService(IInternalContest inContest, IInternalController inController) {
+        super();
+        this.model = inContest;
+        this.controller = inController;
+        jsonTool = new JSONTool(model, controller);
+    }
+
+    /**
+     * This method returns a representation of the current model teams in JSON format. The returned value is a JSON array with one team description per array element.
+     * 
+     * @return a {@link Response} object containing the model teams in JSON form
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getOrganizations() {
+
+        // get the team accounts from the model
+        Account[] accounts = model.getAccounts();
+        // keep track of which ones we have dumped
+        Hashtable<String, Account> organizations = new Hashtable<String, Account>();
+
+        // get an object to map the groups descriptions into JSON form
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode childNode = mapper.createArrayNode();
+        for (int i = 0; i < accounts.length; i++) {
+            Account account = accounts[i];
+            if (account.getClientId().getClientType().equals(ClientType.Type.TEAM) && !account.getInstitutionCode().equals("undefined")) {
+                String id = jsonTool.getOrganizationId(account);
+                if (!organizations.containsKey(id)) {
+                    organizations.put(id, account);
+                    childNode.add(jsonTool.convertOrganizationsToJSON(account));
+                }
+            }
+        }
+        return Response.ok(childNode.toString(), MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Path("{organizationId}/")
+    public Response getOrganization(@PathParam("organizationId") String organizationId) {
+        // get the team accounts from the model
+        Account[] accounts = model.getAccounts();
+
+        for (int i = 0; i < accounts.length; i++) {
+            Account account = accounts[i];
+            if (account.getClientId().getClientType().equals(ClientType.Type.TEAM) && jsonTool.getOrganizationId(account).equals(organizationId)) {
+                return Response.ok(jsonTool.convertOrganizationsToJSON(account).toString(), MediaType.APPLICATION_JSON).build();
+            }
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @Override
+    public boolean configure(FeatureContext arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
