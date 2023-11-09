2feede597f1e369c663fd9fde267144597b7af8d
John Buck <johnbrvc@gmail.com>
2023-11-09 12:05:56 -0500

    i843 Start adding access endpoint.
    The access endpoint is new with 2022-07.  It gives the client a list of things they can access (what exactly is available to them).

---
+++ src/edu/csus/ecs/pc2/clics/API202306/CLICSEndpoint.java
@@ -0,0 +1,39 @@
+package edu.csus.ecs.pc2.clics.API202306;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.csus.ecs.pc2.VersionInfo;
+import edu.csus.ecs.pc2.services.core.JSONUtilities;
+
+/**
+ * CLICS Endpoint
+ * Contains information about an API endpoint that is supported
+ * 
+ * @author John Buck
+ *
+ */
+
+public class CLICSEndpoint {
+
+    @JsonProperty
+    private String type;
+
+    @JsonProperty
+    private String [] properties;
+
+    public CLICSEndpoint(String type, String [] properties) {
+        this.type = type;
+        this.properties = properties;
+    }
+
+    public String toJSON() {
+
+        try {
+            ObjectMapper mapper = JSONUtilities.getObjectMapper();
+            return mapper.writeValueAsString(this);
+        } catch (Exception e) {
+            return "Error creating JSON for CLICSEndpoint " + e.getMessage();
+        }
+    }
+}
