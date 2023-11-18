e8476979056f678abbd276b29efe76c42c3ec250
John Buck <johnbrvc@gmail.com>
2023-11-18 14:27:34 -0500

    i843 CI: fix spelling of getContestTimeMS
    Fix incorrect spelling of method and refactor
    Make JSONTool.getSubmissionId static.

--- src/edu/csus/ecs/pc2/core/util/JSONTool.java
+++ src/edu/csus/ecs/pc2/core/util/JSONTool.java
@@ -507,7 +507,7 @@ public class JSONTool {
         return judgement.getAcronym();
     }
 
-    public String getSubmissionId(Run submission) {
+    public static String getSubmissionId(Run submission) {
         return Integer.toString(submission.getNumber());
     }
 
@@ -542,7 +542,7 @@ public class JSONTool {
         // SOMEDAY get the time from the server instead of the judge
         element.put("time", Utilities.getIso8601formatterWithMS().format(run.getDate().getTime()));
         // note this is the contest_time as seen on the judge
-        element.put("contest_time", ContestTime.formatTimeMS(run.getConestTimeMS()));
+        element.put("contest_time", ContestTime.formatTimeMS(run.getContestTimeMS()));
         return element;
     }
 }
