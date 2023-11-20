ea973471e9061d9c5b21a0b86f1bb3f3d94b476c
John Buck <johnbrvc@gmail.com>
2023-11-19 21:15:42 -0500

    i843 fixed wrong clics api implementation for groups/location
    Mistakenly implemented 2022-07 API instead of 2023-06 for groups endpoint.
    Fixed bug in CLICSLocation bean - had wrong property names.

--- src/edu/csus/ecs/pc2/clics/API202306/CLICSGroup.java
+++ src/edu/csus/ecs/pc2/clics/API202306/CLICSGroup.java
@@ -3,11 +3,10 @@ package edu.csus.ecs.pc2.clics.API202306;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonFilter;
 import edu.csus.ecs.pc2.core.util.JSONTool;
+import edu.csus.ecs.pc2.services.core.JSONUtilities;
 import edu.csus.ecs.pc2.core.model.Group;
 
 /**
@@ -18,11 +17,7 @@ import edu.csus.ecs.pc2.core.model.Group;
  *
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonFilter("locFilter")
 public class CLICSGroup {
-
-    private static final double MAX_LATITUDE = 90.0;
-    private static final double MAX_LONGITUDE = 180.0;
     
     @JsonProperty
     private String id;
@@ -36,12 +31,23 @@ public class CLICSGroup {
     @JsonProperty
     private String type;
 
-    @JsonProperty("location.latitude")
-    private double latitude = MAX_LATITUDE + 1;
-    
-    @JsonProperty("location.longitude")
-    private double longitude = MAX_LONGITUDE + 1;
+    @JsonProperty
+    private CLICSLocation location;
 
+    /**
+     * Fill in properties for a group
+     * @param group The group 
+     */
+    public CLICSGroup(Group group) {
+ 
+        id = JSONTool.getGroupId(group);
+        name = group.getDisplayName();
+        if (group.getGroupId() != -1) {
+            icpc_id = Integer.toString(group.getGroupId());
+        }
+        type = "site" + group.getSiteNumber();
+    }
+    
     /**
      * This is mostly for testing ATM, since the groups in PC2 do not have lat/long.
      * But... a test unit could set these.
@@ -50,27 +56,17 @@ public class CLICSGroup {
      * @param longitude
      */
     public void setLocation(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
+        location = new CLICSLocation(latitude, longitude);
     }
     
-    /**
-     * Fill in properties for a group
-     * @param group The group 
-     * @param exceptProps set to be filled in with properties we do not want serialized
-     */
-    public CLICSGroup(Group group, Set<String> exceptProps) {
- 
-        id = JSONTool.getGroupId(group);
-        name = group.getDisplayName();
-        if (group.getGroupId() != -1) {
-            icpc_id = Integer.toString(group.getGroupId());
-        }
-        type = "site" + group.getSiteNumber();
-        // if location is out of range, omit it from serialization.
-        if(exceptProps != null && (latitude > MAX_LATITUDE || longitude > MAX_LONGITUDE)) {
-            exceptProps.add("location.latitude");
-            exceptProps.add("location.longitude");
+
+    public String toJSON() {
+
+        try {
+            ObjectMapper mapper = JSONUtilities.getObjectMapper();
+            return mapper.writeValueAsString(this);
+        } catch (Exception e) {
+            return "Error creating JSON for group info " + e.getMessage();
         }
     }
 }
