311e596c06acaf4932b2d7a8d261d02c25d4b086
John Buck <johnbrvc@gmail.com>
2023-11-24 17:43:14 -0500

    i843 update scoreboard endpoint for 2023-06
    Massive changes to support to scoreboard endpoint CLICS 2023-06 API.

---
+++ src/edu/csus/ecs/pc2/clics/API202306/CLICSProblemScore.java
@@ -0,0 +1,81 @@
+// Copyright (C) 1989-2024 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+package edu.csus.ecs.pc2.clics.API202306;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import edu.csus.ecs.pc2.core.StringUtilities;
+import edu.csus.ecs.pc2.core.Utilities;
+import edu.csus.ecs.pc2.core.model.ContestTime;
+import edu.csus.ecs.pc2.core.model.IInternalContest;
+import edu.csus.ecs.pc2.core.standings.ProblemSummaryInfo;
+import edu.csus.ecs.pc2.core.standings.json.ProblemScoreRow;
+
+/**
+ * CLICS Score (for a problem)
+ * Contains information about the score a team received for a single problem
+ * 
+ * @author John Buck
+ *
+ */
+
+public class CLICSProblemScore {
+
+    @JsonProperty
+    private String problem_id;
+
+    @JsonProperty
+    private int num_judged;
+
+    @JsonProperty
+    private int num_pending;
+
+    @JsonProperty
+    private boolean solved;
+
+// Not needed for pass-fail contest
+//  @JsonProperty
+//  private double score;
+
+    @JsonProperty
+    private String time;
+
+    /**
+     * Fill in API problem score information properties (for scoreboard endpoint)
+     * 
+     * @param probEleToShort hashmap for mapping problem elementid to shortname
+     * @param versionInfo
+     */
+    public CLICSProblemScore(HashMap<String, String> probEleToShort, ProblemSummaryInfo psi) {
+        num_judged = Utilities.nullSafeToInt(psi.getAttempts(), 0);
+        num_pending = Utilities.nullSafeToInt(psi.getIsPending(), 0);
+        problem_id = psi.getProblemId();
+        // look up problem short name since this is what we use in the problem endpoint.
+        if(probEleToShort.containsKey(problem_id)) {
+            problem_id = probEleToShort.get(problem_id);
+        }
+        solved = toBool(psi.getIsSolved(), false);
+        if(solved) {
+            // Problem solution time is in minutes, so multiply by 60 seconds.
+            time = ContestTime.formatTime(StringUtilities.getIntegerValue(psi.getSolutionTime(), 0)*60);
+        }
+    }
+
+    /**
+     * Convert a string that represents a boolean into a boolean scalar
+     * 
+     * @param strBool string containing a boolean representation
+     * @param defaultBool if strBool is not valid, use this
+     * @return
+     */
+    private boolean toBool(String strBool, boolean defaultBool) {
+        try {
+            return Boolean.parseBoolean(strBool.trim());
+        } catch (Exception e) {
+            return defaultBool;
+        }
+    }
+}
--- src/edu/csus/ecs/pc2/clics/API202306/ScoreboardService.java
+++ src/edu/csus/ecs/pc2/clics/API202306/ScoreboardService.java
@@ -7,7 +7,9 @@ import javax.inject.Singleton;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
@@ -23,69 +25,79 @@ import edu.csus.ecs.pc2.core.exception.IllegalContestState;
 import edu.csus.ecs.pc2.core.log.Log;
 import edu.csus.ecs.pc2.core.model.ContestTime;
 import edu.csus.ecs.pc2.core.model.IInternalContest;
+import edu.csus.ecs.pc2.services.core.JSONUtilities;
 import edu.csus.ecs.pc2.services.core.ScoreboardJson;
+import edu.csus.ecs.pc2.services.eventFeed.WebServer;
 /**
  * Webservice to handle scoreboard requests
  * 
  * @author ICPC
  */
-@Path("/contest/scoreboard")
+@Path("/contests/{contestId}/scoreboard")
 @Produces(MediaType.APPLICATION_JSON)
 @Provider
 @Singleton
 public class ScoreboardService implements Feature {
 
-    private IInternalContest contest;
+    private IInternalContest model;
     private IInternalController controller;
 
     public ScoreboardService(IInternalContest inContest, IInternalController inController) {
         super();
-        this.contest = inContest;
+        this.model = inContest;
         this.controller = inController;
     }
 
     /**
      * This method returns a representation of the current contest scoreboard in JSON format. 
-     * The return JSON is in the format defined by {@link ScoreboardJson#createJSON(IInternalContest)}.
+     * The return JSON is in the format defined by the CLICS 2023-06 spec.
      * 
-     * @return a {@link Response} object containing the JSON scoreboard
+     * @param servletRequest
+     * @param sc
+     * @param contestId
+     * @param group_id - optional group id query param
+     * @return {@link Response} object containing the JSON scoreboard
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getScoreboard(@Context HttpServletRequest servletRequest, @Context SecurityContext sc) {
+    public Response getScoreboard(@Context HttpServletRequest servletRequest, @Context SecurityContext sc, @PathParam("contestId") String contestId, @QueryParam("group_id") String group_id) {
 
-        String jsonScoreboard;
-        try {
-            ContestTime contestTime = contest.getContestTime();
-            // verify contest has started or user is an admin
-            if (contestTime.getElapsedMS() > 0 || sc.isUserInRole("admin")) {
-                //ok to return scoreboard
-
+        // check contest id
+        if(contestId.equals(model.getContestIdentifier()) == true) {
+            ContestTime contestTime = model.getContestTime();
+            // verify contest has started or user is special
+            if (contestTime.getElapsedMS() > 0 ||
+                ((sc.isUserInRole(WebServer.WEBAPI_ROLE_ADMIN) ||
+                  sc.isUserInRole(WebServer.WEBAPI_ROLE_ANALYST) ||
+                  sc.isUserInRole(WebServer.WEBAPI_ROLE_JUDGE)))) {
+                
+                // ok to return scoreboard
                 try {
-                    ScoreboardJson sbJsonObject = new ScoreboardJson();                    
-                    jsonScoreboard = sbJsonObject.createJSON(contest, controller.getLog());
+                    CLICSScoreboard scoreboard = new CLICSScoreboard(model, controller);                    
+                    return Response.ok(scoreboard.toJSON(), MediaType.APPLICATION_JSON).build();
                 } catch (IllegalContestState | JAXBException | IOException e) {
-                    controller.getLog().log(Log.WARNING,"Exception creating PC2 scoreboard JSON: " + e.getMessage(), e);
+                    controller.getLog().log(Log.WARNING, "Exception creating PC2 scoreboard JSON: " + e.getMessage(), e);
                     return Response.status(Status.INTERNAL_SERVER_ERROR).build();
                 }
                 
             } else {
                 // do not show (return) the scoreboard if the contest has not
-                // been started and the requester is not an admin)
+                // been started and the requester is not special)
                 // FIXME: might be better if this returned an "empty" scoreboard?
                 return Response.status(Status.FORBIDDEN).build();
             }
-
-        } catch (Exception e) {
-            controller.getLog().log(Log.WARNING, "ScoreboardService: problem creating scoreboard JSON:  " + e, e);
-            e.printStackTrace();
-            //return HTTP error response code
-            return Response.serverError().entity(e.getMessage()).build();
         }
-
-        // output the response to the requester (note that this actually returns it to Jersey,
-        // which forwards it to the caller as the HTTP response).
-        return Response.ok(jsonScoreboard,MediaType.APPLICATION_JSON).build();
+        return Response.status(Response.Status.NOT_FOUND).build();        
+    }
+    
+    /**
+     * Retrieve access information about this endpoint for the supplied user's security context
+     * 
+     * @param sc User's security information
+     * @return CLICSEndpoint object if the user can access this endpoint's properties, null otherwise
+     */
+    public static CLICSEndpoint getEndpointProperties(SecurityContext sc) {
+        return(new CLICSEndpoint("scoreboard", JSONUtilities.getJsonProperties(CLICSScoreboard.class)));
     }
 
     @Override
