5daee21a7cf759470203d71531a4166118e9817c
johnc <john@LenovoP360>
2023-11-28 12:13:35 -0800

    i871: added global constants file to WTI-UI project.

---
+++ projects/WTI-UI/src/constants.ts
@@ -0,0 +1,11 @@
+//App-wide constants for the PC2 WTI-UI project
+
+/**
+ * The key under which the currently-active WTI page is stored in sessionStorage.
+ */
+export const CURRENT_PAGE_KEY = 'curPageKey';
+
+/**
+ * The key for the RUNS page.
+ */
+export const RUNS_PAGE_KEY = 'runs';
