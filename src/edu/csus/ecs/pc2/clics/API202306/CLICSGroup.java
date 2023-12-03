dce1f54f5ab71da07978b6eb95996503af8e652a
John Buck <johnbrvc@gmail.com>
2023-12-03 11:52:41 -0500

    i843 added fromJSON for CLICS groups.json plus junit
    The CLICSGroup.fromJSON() method deserializes a CLICS compliant groups.json type file and creates an array of PC2 Group objects.
    Added JUnits tests to read CLICS groups.json and teams.json and validate that correct PC2 groups and accounts are generated.
    Added testdata to support the JUnit tests.

--- src/edu/csus/ecs/pc2/clics/API202306/CLICSGroup.java
+++ src/edu/csus/ecs/pc2/clics/API202306/CLICSGroup.java
@@ -1,13 +1,19 @@
 // Copyright (C) 1989-2024 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.clics.API202306;
 
+import java.io.File;
+import java.util.ArrayList;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.csus.ecs.pc2.core.util.JSONTool;
 import edu.csus.ecs.pc2.services.core.JSONUtilities;
+import edu.csus.ecs.pc2.core.StringUtilities;
+import edu.csus.ecs.pc2.core.log.Log;
+import edu.csus.ecs.pc2.core.log.StaticLog;
 import edu.csus.ecs.pc2.core.model.Group;
+import edu.csus.ecs.pc2.core.model.IInternalContest;
 
 /**
  * CLICS Group description
@@ -34,6 +40,10 @@ public class CLICSGroup {
     @JsonProperty
     private CLICSLocation location;
 
+    public CLICSGroup() {
+        // For jackson deserialize
+    }
+    
     /**
      * Fill in properties for a group
      * @param group The group 
@@ -69,4 +79,55 @@ public class CLICSGroup {
             return "Error creating JSON for group info " + e.getMessage();
         }
     }
+    
+    /**
+     * Create Group list from a groups.json like file
+     * 
+     * @param contest the contest
+     * @param filename json file to deserialize
+     * @param site the site to create the groups for
+     * @return array of accounts to add, or null on error
+     */
+    public static Group [] fromJSON(IInternalContest contest, String filename, int site) {
+        Group [] newgroups = null;
+        Log log = StaticLog.getLog();
+        boolean error = false;
+        
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            CLICSGroup [] cgroups = mapper.readValue(new File(filename), CLICSGroup[].class);
+            ArrayList<Group> groups = new ArrayList<Group>();
+            Group group;
+            
+            // convert each json group to a pc2 Group
+            for(CLICSGroup cgroup: cgroups) {
+                
+                if(StringUtilities.isEmpty(cgroup.name)) {
+                    log.log(Log.SEVERE, "no name property in group in file " + filename);
+                    error = true;
+                    break;
+                }
+                group = new Group(cgroup.name);
+                group.setSiteNumber(site);
+                int groupnum;
+                try {
+                    groupnum = Integer.parseInt(cgroup.id);
+                } catch(Exception e) {
+                    // Some sort of conversion error - log it and abort
+                    log.log(Log.SEVERE, "unable to get group number from id " + cgroup.id, e);
+                    error = true;
+                    break;
+                }
+                group.setGroupId(groupnum);
+                groups.add(group);
+            }
+            // if it all worked out, then create the array of accounts to be returned
+            if(!error) {
+                newgroups = groups.toArray(new Group[0]);
+            }
+        } catch (Exception e) {
+            log.log(Log.WARNING, "could not deserialize group file " + filename, e);
+        }        
+        return(newgroups);
+     }
 }
