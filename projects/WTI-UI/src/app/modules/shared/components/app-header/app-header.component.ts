c8a49dbacbfb9ea3ef134a126f70c223574ac48d
johnc <john@LenovoP360>
2023-12-08 16:39:25 -0800

    i863: extract team number from team login string (username, pc2 acct).
    This allows separation of the "team number" from the "team login" on the
    WTI header.
    
    Also added documentation comments.

--- projects/WTI-UI/src/app/modules/shared/components/app-header/app-header.component.ts
+++ projects/WTI-UI/src/app/modules/shared/components/app-header/app-header.component.ts
@@ -8,9 +8,19 @@ import { AuthService } from 'src/app/modules/core/auth/auth.service';
 })
 export class AppHeaderComponent {
 
+  //Return a boolean indicating whether or not to show nav-bar links in the header
   get showLinks(): boolean { return this._authService.isLoggedIn; }
+  
+  //Return a boolean indicatin whether or not to show a teamId in the header
   get showTeamId(): boolean { return this._authService.isLoggedIn; }
-  get userName(): string { return this._authService.username; }
+  
+  /* Return a string containing the "team id" -- that is, the PC2 team account number with
+     the leading "team" removed */
+  get teamId(): string { 
+    let acctId = this._authService.username; 
+    let teamId = acctId.substr(4);
+    return teamId;
+  }
 
   constructor(private _authService: AuthService) { }
 }
