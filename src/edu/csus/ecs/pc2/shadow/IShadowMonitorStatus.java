b102cfdb509b1496e972f98a5d79129524cb5e78
John Buck <johnbrvc@gmail.com>
2024-03-15 15:50:01 -0400

    i_815 Allow shadow to start before contest is started
    Remove restriction on shadowing where the contest must be started.
    Listen for CLICS "state" events and start and stop the contest appropriately.

--- src/edu/csus/ecs/pc2/shadow/IShadowMonitorStatus.java
+++ src/edu/csus/ecs/pc2/shadow/IShadowMonitorStatus.java
@@ -1,4 +1,4 @@
-// Copyright (C) 1989-2022 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+// Copyright (C) 1989-2024 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.shadow;
 
 /**
@@ -6,7 +6,7 @@ package edu.csus.ecs.pc2.shadow;
  * A user of the RemoteEventFeedMonitor has the option to get upcalls for various
  * counters that change, such as the number of events processed or the number of
  * records read.  In addition, the user can keep track of disconnects and reconnects.
- * 
+ *
  * @author John Buck
  *
  */
@@ -14,43 +14,57 @@ public interface IShadowMonitorStatus {
 
     /**
      * Updates last processed token
-     * 
+     *
      * @param token the last token received and processed on the feed
      */
     public void updateShadowLastToken(String token);
-    
+
     /**
      * Updates number of records received
-     * 
+     *
      * @param number of records received
      */
     public void updateShadowNumberofRecords(int nRec);
-    
+
+    /**
+     * Updates number of tossed records due to the contest not being staretd
+     *
+     * @param number of records tossed
+     */
+    public void updateShadowNumberofTossedRecords(int nRec);
+
     /**
      * Could not establish connection to primary
-     * 
+     *
      * @param token last successfully processed event
      */
     public void connectFailed(String token);
-    
+
     /**
      * Connection to primary succeeded
-     * 
+     *
      * @param token where to start feed from (after this)
      */
     public void connectSucceeded(String token);
-    
+
     /**
      * Normal close of remote
-     * 
+     *
      * @param msg string describing what happened
      */
     public void connectClosed(String Msg);
-    
+
     /**
      * Unexpected close of remote
-     * 
+     *
      * @param errMsg string describing what happened
      */
     public void errorDisconnect(String errMsg);
+
+    /**
+     * General information message
+     *
+     * @param infoMsg string describing what happened
+     */
+    public void statusMessage(String errMsg);
 }
