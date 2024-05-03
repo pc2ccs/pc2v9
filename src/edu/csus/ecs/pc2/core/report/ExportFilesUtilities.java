470c2c9704f8804995b3dd2bfeef4ec6f65dd744
John Buck <johnbrvc@gmail.com>
2024-05-03 15:51:27 -0400

    i960 generate event-feed.ndjson as part of Results Export Report
    End of contest procedures require the saving of the event-feed.ndjson file in addition to the already saved results.tsv, scoreboard.json and awards.json.  The changes here arrange for event-feed.ndjson to be saved as part of the Results Export Report.

--- src/edu/csus/ecs/pc2/core/report/ExportFilesUtilities.java
+++ src/edu/csus/ecs/pc2/core/report/ExportFilesUtilities.java
@@ -1,4 +1,4 @@
-// Copyright (C) 1989-2023 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+// Copyright (C) 1989-2024 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.core.report;
 
 import java.io.File;
@@ -17,18 +17,19 @@ import edu.csus.ecs.pc2.core.imports.clics.CLICSAwardUtilities;
 import edu.csus.ecs.pc2.core.log.StaticLog;
 import edu.csus.ecs.pc2.core.model.IInternalContest;
 import edu.csus.ecs.pc2.exports.ccs.ResultsFile;
+import edu.csus.ecs.pc2.services.core.EventFeedJSON;
 import edu.csus.ecs.pc2.services.core.ScoreboardJson;
 
 public class ExportFilesUtilities {
 
     /**
      * Write contest results files to directory.
-     * 
+     *
      * Creates contest result files
      * <li>{@link ResultsFile.RESULTS_FILENAME}
      * <li>{@link Constants.SCOREBOARD_JSON_FILENAME}
      * <li>{@link Constants.AWARDS_JSON_FILENAME}
-     * 
+     *
      * @param contest
      * @param outputDirectory
      *            directory to write reports to.
@@ -43,6 +44,8 @@ public class ExportFilesUtilities {
         String scoreboardJsonFilename = outputDirectory + File.separator + Constants.SCOREBOARD_JSON_FILENAME;
 
         String awardsFileName = outputDirectory + File.separator + Constants.AWARDS_JSON_FILENAME;
+
+        String eventfeedJsonFilename = outputDirectory + File.separator + Constants.EVENT_FEED_JSON_FILENAME;
 
         try {
             ResultsFile resultsFile = new ResultsFile();
@@ -85,6 +88,20 @@ public class ExportFilesUtilities {
             ExecuteUtilities.rethrow(e);
         }
 
-        return (String[]) filesCreated.toArray(new String[filesCreated.size()]);
+        try {
+            EventFeedJSON efEventFeedJSON = new EventFeedJSON(contest);
+            String json = efEventFeedJSON.createJSON(contest, null, null);
+            String[] eventfeedJsonLines = { json };
+            FileUtilities.writeFileContents(eventfeedJsonFilename, eventfeedJsonLines);
+
+            filesCreated.add(eventfeedJsonFilename);
+
+        } catch (Exception e) {
+            StaticLog.getLog().log(Level.WARNING, "Problem writing event-feed JSON file", e);
+            Utilities.printStackTrace(System.out, e);
+            ExecuteUtilities.rethrow(e);
+        }
+
+        return filesCreated.toArray(new String[filesCreated.size()]);
     }
 }
