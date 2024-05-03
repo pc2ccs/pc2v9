1bf38e2c7a099445d2dbdc8cd7da4bf3555c4db2
Kutay Karakaş <kk3528@columbia.edu>
2024-05-03 17:34:01 -0400

    i765 new ui and fixes

--- src/edu/csus/ecs/pc2/shadow/ShadowController.java
+++ src/edu/csus/ecs/pc2/shadow/ShadowController.java
@@ -95,14 +95,18 @@ public class ShadowController {
             return this.label;
         }
     }
-
+    public enum FILTERS {
+        NONE,
+        ONLY_MISSMATCH
+    }
+    
     private SHADOW_CONTROLLER_STATUS controllerStatus = null ;
     private RemoteContestConfiguration remoteContestConfig;
     private Thread monitorThread;
     private IRemoteContestAPIAdapter remoteContestAPIAdapter;
 
     private boolean convertJudgementsToBig5 = true;
-
+    private FILTERS currentFilter = FILTERS.NONE ;
     /**
      * a ContestInformation Listener
      *
@@ -1027,5 +1031,33 @@ public class ShadowController {
 
     public IInternalController getLocalController() {
         return localController;
+    }
+    
+    public FILTERS getFilter() {
+        return currentFilter;
+    }
+    
+    public void setFilter(FILTERS filter) {
+        currentFilter = filter;
+    }
+    
+    /**
+     * 
+     * @param currentJudgementMap
+     * @return currentJudgementMap but judgements that shouldnt be shown is removed.
+     */
+    public Map<String, ShadowJudgementInfo> filterJudgmenentMap(Map<String, ShadowJudgementInfo> currentJudgementMap) {
+        if (getFilter().equals(FILTERS.ONLY_MISSMATCH)) {
+            Map<String, ShadowJudgementInfo> newJudgementMap = new HashMap<String, ShadowJudgementInfo>();
+            for (String key : currentJudgementMap.keySet()) {
+
+                ShadowJudgementPair pair = currentJudgementMap.get(key).getShadowJudgementPair();
+                if (! pair.getPc2Judgement().equals(pair.getRemoteCCSJudgement())) {
+                    newJudgementMap.put(key,currentJudgementMap.get(key));
+                }
+            }
+            return newJudgementMap;
+        }
+        return currentJudgementMap;
     }
 }
