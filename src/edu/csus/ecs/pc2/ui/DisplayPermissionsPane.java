ffaa52c063127da7fa65fcffc730ac4634eb6e6e
John Buck <johnbrvc@gmail.com>
2024-07-20 12:41:16 -0400

    i_992 CI: Fix NPE in DIsplayPermissionsPane
    If the server user attempts to use the Permissions tab, an NPE resulted.  This is because there is no account for the server.  Handle this with a special case to prevent the NPE.

--- src/edu/csus/ecs/pc2/ui/DisplayPermissionsPane.java
+++ src/edu/csus/ecs/pc2/ui/DisplayPermissionsPane.java
@@ -1,4 +1,4 @@
-// Copyright (C) 1989-2019 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+// Copyright (C) 1989-2024 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.ui;
 
 import java.awt.BorderLayout;
@@ -14,13 +14,14 @@ import javax.swing.JPanel;
 import edu.csus.ecs.pc2.core.IInternalController;
 import edu.csus.ecs.pc2.core.model.Account;
 import edu.csus.ecs.pc2.core.model.ClientId;
+import edu.csus.ecs.pc2.core.model.ClientType;
 import edu.csus.ecs.pc2.core.model.IInternalContest;
 import edu.csus.ecs.pc2.core.security.Permission;
 
 /**
  * A UI that provides a button to display the PC2 {@link Permission}s associated with the currently logged-in account.
- * 
- * 
+ *
+ *
  * @author John Clevenger, PC^2 Development Team (pc2@ecs.csus.edu).
  */
 public class DisplayPermissionsPane extends JPanePlugin {
@@ -54,35 +55,42 @@ public class DisplayPermissionsPane extends JPanePlugin {
 
         //find out who I am
         ClientId clientId = getContest().getClientId();
-        
+
         //get my account
         Account myAccount = getContest().getAccount(clientId);
-        
-        //get the list of permissions granted to my account
-        Permission.Type[] myPermissions = myAccount.getPermissionList().getList();
-        
-        //construct a String listing each permission
-        String msg = "Permissions (" + myPermissions.length + "): \n";
-        
-        if (myPermissions.length==0) {
-            msg += "  <none>\n";
-        } else {
 
-            ArrayList<String> perms = new ArrayList<String>();
-            
-            for (Permission.Type permission : myPermissions) {
-                perms.add(permission.toString());
-            }
-            perms.sort(null);
-            
-            for (String perm : perms) {
-                msg += "  " + perm + "\n";
+        String msg;
+
+        // Since the server does not have an Account, we make a special check here to avoid a null
+        // reference.
+        if(myAccount == null && clientId.getClientType() == ClientType.Type.SERVER) {
+            msg = "The contest server has any permission it needs.";
+        } else {
+            //get the list of permissions granted to my account
+            Permission.Type[] myPermissions = myAccount.getPermissionList().getList();
+
+            //construct a String listing each permission
+            msg = "Permissions (" + myPermissions.length + "): \n";
+
+            if (myPermissions.length==0) {
+                msg += "  <none>\n";
+            } else {
+
+                ArrayList<String> perms = new ArrayList<String>();
+
+                for (Permission.Type permission : myPermissions) {
+                    perms.add(permission.toString());
+                }
+                perms.sort(null);
+
+                for (String perm : perms) {
+                    msg += "  " + perm + "\n";
+                }
             }
         }
-        
         //display the permision list
         JOptionPane.showMessageDialog(this, msg, "Current Permissions", JOptionPane.INFORMATION_MESSAGE);
-        
+
     }
 
     @Override
