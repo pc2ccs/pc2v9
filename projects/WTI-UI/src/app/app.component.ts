8dfd00ba7fcbf7b980c0fe99636aef6dd0a5e730
John Clevenger <clevenger@csus.edu>
2024-10-18 18:50:19 -0700

    i871: refactor loadEnvironment(); call on Refresh as well as initial.

--- projects/WTI-UI/src/app/app.component.ts
+++ projects/WTI-UI/src/app/app.component.ts
@@ -20,60 +20,66 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
   
     //check if we're loading for the first time
-    if (getCurrentPage() == null) {
+    if (!getCurrentPage()) {
         //we have no current page so we must be starting from scratch
         console.log ('Starting from scratch...');
+        this.loadEnvironment();
         
-	    // Load appconfig.json from assets directory, overwrite environment.ts
-	    // with these values
-	    this._httpClient.get('assets/appconfig.json')
-	      .subscribe((data: any) => {
-	        this.configLoaded = true;
-	        if (!data) { return; }
-	        Object.keys(data).forEach((key: string) => environment[key] = data[key]);
-	      }, (error: any) => {
-	        console.log('could not find appconfig.json in assets directory. using default values!');
-	        this.configLoaded = true;
-	      });
 	} else {
 	    //there is a current page stored; we must be reloading from (e.g.) an F5 refresh
 	    console.log ('Restarting after refresh navigation');
 	    
+        this.loadEnvironment();
+
 	    // transfer to the (former) "current page".
 	    let page = getCurrentPage();
 	    console.log ('Transferring to previous page: ' + page);
 	    
 	    //navigate to the most recently saved page
 	    // TODO:  consider whether using history.pushState()/popState() is a better solution for this...
-	    this.router.navigate(['/', page])
+	    this.router.navigate([page])
 	      .then(nav => {
-	        console.log("Call to router.navigate('/'," + page + ") returned " + nav);  //true if navigation is successful
+	        console.log("Call to router.navigate([" + page + "]) returned " + nav);  //true if navigation is successful
 	      }, err => {
-	         console.log("Call to router.navigate('/'," + page + ") returned " + err)
+	         console.log("Call to router.navigate([" + page + "]) returned " + err)
 	      });
 	    
 	}
 	
   }
-  
-}
 
-  //save the current page in sessionStorage so a subsequent F5 (refresh) can return to that page
+  // Load appconfig.json from assets directory, overwrite environment.ts with these values
+  loadEnvironment(): void {
+	console.log("Loading environment from 'assets/appconfig.json'");
+	this._httpClient.get('assets/appconfig.json')
+	    .subscribe((data: any) => {
+	        this.configLoaded = true;
+	        if (!data) { return; }
+	        Object.keys(data).forEach((key: string) => environment[key] = data[key]);
+	    }, (error: any) => {
+	        console.log('Could not find appconfig.json in assets directory. using default values!');
+	        this.configLoaded = true;
+	    });	
+  }//end function loadEnvironment()
+
+}//end class AppComponent
+
+  //save the current page in localStorage so a subsequent F5 (refresh) can return to that page
   export function saveCurrentPage(page:string) {
-      sessionStorage.setItem(Constants.CURRENT_PAGE_KEY, page) ;
+      localStorage.setItem(Constants.CURRENT_PAGE_KEY, page) ;
   }
   
   //return the most recently saved page
   export function getCurrentPage() {
-      return (sessionStorage.getItem(Constants.CURRENT_PAGE_KEY));
+      return (localStorage.getItem(Constants.CURRENT_PAGE_KEY));
   }
   
   //clear any record of a "current page"
   export function clearCurrentPage() {
-      sessionStorage.removeItem(Constants.CURRENT_PAGE_KEY);
+      localStorage.removeItem(Constants.CURRENT_PAGE_KEY);
   }
   
-  //clear all data in sessionStorage
-  export function clearSessionStorage() {
-      sessionStorage.clear();
+  //clear all data in localStorage
+  export function clearLocalStorage() {
+      localStorage.clear();
   }
