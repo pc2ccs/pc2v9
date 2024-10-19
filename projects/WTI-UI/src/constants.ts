e96eed2eecff64cfa5472728b04679df485d8be9
John Clevenger <clevenger@csus.edu>
2024-10-18 18:34:58 -0700

    i871: update WTI-UI constants.ts with keys for every UI page.

--- projects/WTI-UI/src/constants.ts
+++ projects/WTI-UI/src/constants.ts
@@ -1,7 +1,7 @@
 //App-wide constants for the PC2 WTI-UI project
 
 /**
- * The key under which the currently-active WTI page is stored in sessionStorage.
+ * The key under which the currently-active WTI page is stored in localStorage.
  */
 export const CURRENT_PAGE_KEY = 'curPageKey';
 
@@ -9,3 +9,18 @@ export const CURRENT_PAGE_KEY = 'curPageKey';
  * The key for the RUNS page.
  */
 export const RUNS_PAGE_KEY = 'runs';
+
+/**
+ * The key for the OPTIONS page.
+ */
+export const OPTIONS_PAGE_KEY = 'options';
+
+/**
+ * The key for the CLARIFICATIONS page.
+ */
+export const CLARIFICATIONS_PAGE_KEY = 'clarifications';
+
+/**
+ * The key for the SCOREBOARD page.
+ */
+export const SCOREBOARD_PAGE_KEY = 'scoreboard';
