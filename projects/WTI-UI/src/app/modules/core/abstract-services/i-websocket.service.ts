f84810596e2a6166375771dfd33809e902a91bca
johnc <john@LenovoP360>
2024-12-03 11:13:21 -0800

    i871: added param to @injectable forcing singleton; added debug output

--- projects/WTI-UI/src/app/modules/core/abstract-services/i-websocket.service.ts
+++ projects/WTI-UI/src/app/modules/core/abstract-services/i-websocket.service.ts
@@ -1,3 +1,4 @@
+import { Injectable, Injector } from '@angular/core';
 import { UiHelperService } from '../services/ui-helper.service';
 import { WebsocketMessage } from '../models/websocket-message';
 import { IContestService } from './i-contest.service';
@@ -5,11 +6,18 @@ import { ITeamsService } from './i-teams.service';
 import { AuthService } from '../auth/auth.service';
 import { DEBUG_MODE } from 'src/constants';
 
+@Injectable({
+	providedIn: 'root'   //forces the service to be a singleton across all app components ('root' == "root injector")
+})
 export abstract class IWebsocketService {
   constructor(private _uiHelperService: UiHelperService,
               private _contestService: IContestService,
               private _teamsService: ITeamsService,
-              public _authService: AuthService) { }
+              public _authService: AuthService) { 
+		  if (DEBUG_MODE) {
+			  console.log("Executing IWebsocketService constructor...");
+		  }
+	}
 
   abstract startWebsocket(): void;
 
@@ -19,18 +27,33 @@ export abstract class IWebsocketService {
     switch (message.type) {
       case 'test':
       case 'judged': {
+    	  if (DEBUG_MODE) {
+    		  console.log ("Got '", message.type, "' websocket message in IWebsocketService.incomingMessage()");
+    	  }
         this._uiHelperService.incomingRun(message.id);
         this._teamsService.runsUpdated.next();
         break;
       }
       case 'clarification': {
+    	  if (DEBUG_MODE) {
+    		  console.log ("Got '", message.type, "' websocket message in IWebsocketService.incomingMessage()");
+    	  }
         this._uiHelperService.incomingClarification(message.id);
         this._contestService.clarificationsUpdated.next();
         break;
       }
       case 'contest_clock': {
+    	  if (DEBUG_MODE) {
+    		  console.log ("Got '", message.type, "' websocket message in IWebsocketService.incomingMessage()");
+    	  }
         this._contestService.getIsContestRunning()
           .subscribe((val: any) => {
+        	  if (DEBUG_MODE) {
+        		  console.log ("IWebsocketService.incomingMessage(): callback from ContestService.getIsContestRunning() returned '", val, "'");
+        		  console.log ("Setting ContestService.isContestRunning to '", val, "'") ;
+        		  console.log ("  and invoking ContestService.contestClock.next()") ;
+        	  }
+
             this._contestService.isContestRunning = val;
             this._contestService.contestClock.next();
           });
@@ -47,6 +70,8 @@ export abstract class IWebsocketService {
     	  if (DEBUG_MODE) {
     	      console.log("Got a connection_dropped websocket message:");
     	      console.log(message);
+    	      console.log ("Invoking UIHelperService.indefinitelyDisplayedAlert('connection lost'") ;
+    	      console.log ("  and invoking AuthService.logout() then AuthService.completeLogout()") ;
     	  }
           this._uiHelperService.indefinitelyDisplayedAlert("PC2 Server connection lost");
           this._authService.logout();  			//invokes teamsService.logout();
