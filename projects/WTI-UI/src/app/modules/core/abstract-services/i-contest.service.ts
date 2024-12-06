dbfba5eed911312598288f1a04282a77cc62679e
John Clevenger <clevenger@csus.edu>
2024-12-06 12:41:17 -0800

    i871: made abstract IContestService Injectable,providedIn:root.  Also,
    added static nextId, along with uniqueId to each instance.

--- projects/WTI-UI/src/app/modules/core/abstract-services/i-contest.service.ts
+++ projects/WTI-UI/src/app/modules/core/abstract-services/i-contest.service.ts
@@ -1,18 +1,28 @@
+import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { ContestLanguage } from '../models/contest-language';
 import { ContestProblem } from '../models/contest-problem';
 import { Clarification } from '../models/clarification';
 import { DEBUG_MODE } from 'src/constants';
 
+@Injectable({
+	providedIn: 'root'   //forces the service to be a singleton across all app components ('root' == "root injector")
+})
 export abstract class IContestService {
   clarificationsUpdated = new Subject<void>();
   contestClock = new Subject<void>();
   standingsUpdated = new Subject<void>();
   isContestRunning = false;
+
+  //give each instance of IContestService a unique ID for debugging purposes
+  private static nextId: number = 1;
+  public uniqueId: number;
+
   
   constructor () {
+	  this.uniqueId = IContestService.nextId++;
 	  if (DEBUG_MODE) {
-		  console.log ("Executing IContestService constructor") ;
+		  console.log ("Executing IContestService constructor for unique instance ", this.uniqueId) ;
 	  }
   }
 
