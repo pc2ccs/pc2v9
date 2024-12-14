4a0956865bc8ec340c5c6896b165bf166d19f0a5
johnc <john@LenovoP360>
2024-12-13 17:26:52 -0800

    i1025: bump WTI version to 1.2 in BuildWTI.xml and in user scripts.

--- projects/WTI-API/scripts/pc2wti.bat
+++ projects/WTI-API/scripts/pc2wti.bat
@@ -4,6 +4,6 @@ REM Copyright (C) 1989-2020 PC2 Development Team: John Clevenger, Douglas Lane, 
 rem Purpose: start the WebTeamInterface (WTI) project team webserver module
 rem Author : pc2@ecs.csus.edu
 
-java -Djdk.crypto.KeyAgreement.legacyKDF=true -Xms64M -Xmx2048M -jar WebTeamInterface-1.1.jar
+java -Djdk.crypto.KeyAgreement.legacyKDF=true -Xms64M -Xmx2048M -jar WebTeamInterface-1.2.jar
 
 rem eof pc2wti.bat 
