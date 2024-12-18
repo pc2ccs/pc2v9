31e5afccbf10db692f193f1b1bb42eb5abf7f805
John Clevenger <clevenger@csus.edu>
2024-12-18 14:26:06 -0800

    i1027: added method getContestClock() to ContestServices (to both...
    the abstract and concrete versions)

--- projects/WTI-UI/src/app/modules/core/abstract-services/i-contest.service.ts
+++ projects/WTI-UI/src/app/modules/core/abstract-services/i-contest.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { ContestLanguage } from '../models/contest-language';
 import { ContestProblem } from '../models/contest-problem';
+import { ContestClock } from '../models/contest-clock';
 import { Clarification } from '../models/clarification';
 import { DEBUG_MODE } from 'src/constants';
 
@@ -10,7 +11,8 @@ import { DEBUG_MODE } from 'src/constants';
 })
 export abstract class IContestService {
   clarificationsUpdated = new Subject<void>();
-  contestClock = new Subject<void>();
+  contestClock = new Subject<void>();  //note that this is a WTI-UI-specific object used in conjunction with Websocket "clock" messages; 
+									//it is NOT a "ContestClock" object in the sense of classes defined in core/models.
   standingsUpdated = new Subject<void>();
   isContestRunning = false;
 
@@ -35,6 +37,8 @@ export abstract class IContestService {
   abstract getClarifications(): Observable<Clarification[]>;
 
   abstract getIsContestRunning(): Observable<boolean>;
+
+  abstract getContestClock(): Observable<ContestClock>;		//note that this refers to a "ContestClock" model object, not the "contestClock" subject above
   
   abstract getStandings(): Observable<String>;
 
