3ff1b6ef02fe4b23064971e44332d3a56c6299ca
John Clevenger <clevenger@csus.edu>
2024-12-23 17:35:58 -0800

    i1027: added Timer class to enable/disable incr of display times.

---
+++ projects/WTI-UI/src/app/modules/core/services/contestTimer.service.ts
@@ -0,0 +1,80 @@
+import { DEBUG_MODE } from 'src/constants'
+
+export class ContestTimerService {
+
+  elapsedSecs = 0 ; 		//how many seconds the contest has been running (doesn't include any 'paused' time)
+  remainingSecs = 18000 ;   //how many seconds remain in the contest (default initial value: 5 hours = 18,000 secs)
+  intervalId: ReturnType<typeof setInterval> ;  //the id of the JavaScript "interval" used to generate timer ticks
+  isTimerRunning: boolean = false;
+  
+  constructor() {
+	if (DEBUG_MODE) {
+		console.log ("Executing ContestTimerService constructor");
+	}
+  }
+
+  getElapsedSecs(): number {
+    return this.elapsedSecs;
+  }
+
+  getRemainingSecs(): number {
+	return this.remainingSecs;
+  }
+  
+  setElapsedSecs(newElapsedSecs: number) {
+    if (this.intervalId) {
+	  console.error ("ContestTimerService.setElapsedSecs(): cannot set elapsed seconds while Timer is running; stop the timer first.")
+      return;
+	} else {
+      this.elapsedSecs = newElapsedSecs;
+	}
+  }
+ 
+  setRemainingSecs(newRemainingSecs: number) {
+    if (this.intervalId) {
+	  console.error ("ContestTimerService.setRemainingSecs(): cannot set remaining seconds while Timer is running; stop the timer first.")
+      return;
+	} else {
+      this.remainingSecs = newRemainingSecs>=0 ? newRemainingSecs : 0;
+	}
+  }
+
+  startTimer() {
+	if (this.intervalId) {
+      console.error("ContestTimerService.startTimer():  call to startTimer() when timer is already running");
+      return;
+	} else {
+      if (DEBUG_MODE) {
+        console.log ("ContestTimerService.startTimer():  starting 1-second interval timer");
+	  }
+      //Start a 1-second interval timer going, save the interval timer Id
+      this.intervalId = setInterval(
+        //execute this function at the specified interval:
+        () => {
+          //bump the elapsed & remaining counters since 1000msec (one sec) has passed
+          this.elapsedSecs += 1 ;
+		  this.remainingSecs -= 1
+          console.log(`Elapsed secs: ${this.elapsedSecs}; remaining secs: ${this.remainingSecs}`);
+        }, 
+        1000	// 1000 milliseconds = 1 second interval
+      ); 
+      this.isTimerRunning = true;
+	}
+  }
+
+  stopTimer() {	
+    if (!this.intervalId) {
+      console.error("ContestTimerService.stopTimer():  call to stopTimer() when timer is not running");
+      return;
+    } else {
+      if (DEBUG_MODE) {
+        console.log ("ContestTimerService.stopTimer():  stopping timer");
+	  }
+      //dispose of the existing interval timer (thus stopping it)
+      clearInterval(this.intervalId);
+      this.intervalId = undefined;
+      this.isTimerRunning = false;
+    }
+  }
+ 
+}
