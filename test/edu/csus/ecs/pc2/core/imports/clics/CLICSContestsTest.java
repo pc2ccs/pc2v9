7637182d7496c3dcd026c8d2fd972ad7cecf0a73
John Buck <johnbrvc@gmail.com>
2025-02-21 15:25:39 -0500

    i_843 i_1048 Allow full CLICS RELTIME values for contest.yaml durations
    The YAML parser will convert any value it sees with a fractional part (decimal point followed by numbers) to a double value, which is converted to a String in ContestSnakeYAMLLoader.  The causes a problem when that value was parsed as a Long (it caused a NFE).  This fix converts the value to a double using (Double.parseDouble()), then, it typecasts that to a long.  This is OK, since contest durations are in seconds, not fractional parts of  second.  We just ignore the fractional part.
    Also fixed JUnit test correctly since the freeze duration will now compare properly without stripping off the leading 0.
    This fix will be part of the PR for i843_CLICS_API_Endpoint_Compliance, but can be cherry-picked elsewhere if needed before this is merged.

--- test/edu/csus/ecs/pc2/core/imports/clics/CLICSContestsTest.java
+++ test/edu/csus/ecs/pc2/core/imports/clics/CLICSContestsTest.java
@@ -1,4 +1,4 @@
-// Copyright (C) 1989-2022 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
+// Copyright (C) 1989-2025 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.core.imports.clics;
 
 import java.io.File;
@@ -19,7 +19,7 @@ import edu.csus.ecs.pc2.imports.ccs.ContestSnakeYAMLLoader;
 
 /**
  * Unit tests.
- * 
+ *
  * @author Douglas A. Lane <pc2@ecs.csus.edu>
  */
 public class CLICSContestsTest extends AbstractTestCase {
@@ -35,20 +35,20 @@ public class CLICSContestsTest extends AbstractTestCase {
         ObjectMapper mapper = getMapper();
 //        String json = "{\"type\": \"problems\", \"id\": \"542cc6e2-104a-49bc-9fea-04752f9af5ad\", \"op\": \"delete\", \"data\": null }";
 
-        CLICSEventFeedEvent eventFeedEntry = (CLICSEventFeedEvent) mapper.readValue(json, CLICSEventFeedEvent.class);
+        CLICSEventFeedEvent eventFeedEntry = mapper.readValue(json, CLICSEventFeedEvent.class);
         assertNotNull(eventFeedEntry.getData());
 
         CLICSContests clicsContests = mapper.convertValue(eventFeedEntry.getData(), CLICSContests.class);
 
         String cdpPath = getDataDirectory(getName());
-        
+
 //        ensureDirectory(cdpPath + File.separator + IContestLoader.CONFIG_DIRNAME);
 //        startExplorer(cdpPath);
-        
+
         IInternalContest contest = loadContest(cdpPath);
-        
+
         ContestInformation info = contest.getContestInformation();
-     
+
         ContestTime contestTime = contest.getContestTime();
         assertNotNull("Contest Time is NULL", contestTime);
 
@@ -68,24 +68,24 @@ public class CLICSContestsTest extends AbstractTestCase {
 
         // TODO CCS penalty-time not loaded from contest.yaml
 //        assertEquals("penalty_time", penaltyTime, clicsContests.getPenalty_time());
- 
-        assertEquals("scoreboard_freeze_duration", info.getFreezeTime().substring(1), ContestCompareModel.clipMs(clicsContests.getScoreboard_freeze_duration()));
+
+        assertEquals("scoreboard_freeze_duration", info.getFreezeTime(), ContestCompareModel.clipMs(clicsContests.getScoreboard_freeze_duration()));
 
 
         // TODO uncomment this test when the logic to handle timezones in start time is fixed.
         // from contest.yaml - 2022-10-22T03:30:00-0700
         // from getStart_time - 2022-10-22T12:30:00+02:00
 //        assertEquals("start_time", startTimeStr, clicsContests.getStart_time());
-        
+
         assertEquals("2022-10-22T12:30:00+02:00", clicsContests.getStart_time());
-        
+
         assertNotNull(clicsContests);
 
     }
 
     private IInternalContest loadContest(String cdpPath) {
 
-//      File cdpConfigDir = new File(cdpPath); 
+//      File cdpConfigDir = new File(cdpPath);
         File cdpConfigDir = FileUtilities.findCDPConfigDirectory(new File(cdpPath));
 
         assertDirectoryExists(cdpConfigDir.getAbsolutePath(), "Expected contest CDP directory to exist ");
@@ -96,7 +96,7 @@ public class CLICSContestsTest extends AbstractTestCase {
 
     /**
      * Get an object mapper that ignores unknown properties.
-     * 
+     *
      * @return an object mapper that ignores unknown properties
      */
     // TODO REFACTOR move into JSONUtilities
