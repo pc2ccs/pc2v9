71a21fb9cf0b74e1a2638f1e1af49cb0cbd866c8
Kutay Karakaş <kk3528@columbia.edu>
2025-04-24 20:15:25 -0400

    bruh

--- projects/WTI-API/src/main/controllers/TeamsController.java
+++ projects/WTI-API/src/main/controllers/TeamsController.java
@@ -499,7 +499,6 @@ public class TeamsController extends MainController {
 					.entity(new ServerErrorResponseModel(Response.Status.UNAUTHORIZED, "Unauthorized user request"))
 					.type(MediaType.APPLICATION_JSON).build();
 		}
-		
 		Response submitClarResponse = this.submitClarification(teamsConn, clar);
 		logger.info("Submitted clarification to PC2 server for team " +  teamAcct + ", response = " + submitClarResponse.getStatus());
 		return submitClarResponse;
