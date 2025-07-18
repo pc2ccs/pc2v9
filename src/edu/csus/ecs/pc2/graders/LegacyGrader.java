5b091216cd42c273c65c455d8746e2c01ec14ebf
John Buck <johnbrvc@gmail.com>
2025-07-18 10:22:00 -0400

    i_1006 allow LegacyGrader to accept list of judgments
    If using the LegacyGrader internally (and not as an external process), the "String gradeTestCases(ArrayList<String> testCaseResults)" may be used directly.  The bypasses the extra steps of saving the list of judgment/score tuples to a file and having the grader open and read that file.  In addition, the gradTestCases method returns the net judgment string, eg. "AC 50" on success, and null on failure.
    
    (cherry picked from commit b50c9610d21d1dbda6b99c19cd8d4115084c21de)

--- src/edu/csus/ecs/pc2/graders/LegacyGrader.java
+++ src/edu/csus/ecs/pc2/graders/LegacyGrader.java
@@ -1,6 +1,7 @@
 // Copyright (C) 1989-2025 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 package edu.csus.ecs.pc2.graders;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -17,6 +18,13 @@ public class LegacyGrader {
 
     public static final String ACCEPT_IF_ANY_ACCEPTED_FLAG = "accept_if_any_accepted";
 
+    public static final int GRADER_ERROR_BAD_FORMAT = 1;
+    public static final int GRADER_ERROR_BAD_SCORE = 2;
+    public static final int GRADER_ERROR_BAD_JUDGMENT = 3;
+    public static final int GRADER_ERROR_NO_TEST_CASES = 4;
+    public static final int GRADER_ERROR_BAD_WORST_CODE = 5;
+    public static final int GRADER_ERROR_BAD_FIRST_CODE = 6;
+    public static final int GRADER_ERROR_BAD_VERDICT_MODE = 7;
 
     enum VerdictMode {
         worst_error,
@@ -41,6 +49,7 @@ public class LegacyGrader {
     private VerdictMode verdictMode = VerdictMode.worst_error;
     private ScoringMode scoringMode = ScoringMode.sum;
     private boolean acceptIfAnyAccepted = false;
+    private int graderError = 0;
 
     /**
      * See if the supplied string argument is a valid verdict mode.
@@ -117,13 +126,31 @@ public class LegacyGrader {
     }
 
     /**
-     * Read the stdin input for lines of test case judgments and evaluate according the specification.
+     * Read the stdin input for lines of test case judgments, create a list of them,
+     * and evaluate according the specification.
      *
      * @return 0 on success (this is the exit code for the program), non-zero indicates a judging error
      */
     public int processResults() {
-        int result = 0;
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> testCaseResults = new ArrayList<String>();
+
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine().trim();
+            if(!line.isEmpty()) {
+                testCaseResults.add(line);
+            }
+        }
+        graderError = 0;
+        String result = gradeTestCases(testCaseResults);
+
+        if(result != null) {
+            System.out.println(result);
+        }
+        return(graderError);
+    }
+
+    public String gradeTestCases(ArrayList<String> testCaseResults) {
         int nLine = 0;
         int idx;
         boolean found;
@@ -134,14 +161,15 @@ public class LegacyGrader {
         boolean [] sawJudgment = new boolean[JudgmentCodes.values().length];
         boolean anyFailures = false;
         String firstError = null;
+        String graderResult = null;
 
-        while(scanner.hasNextLine()) {
+        graderError = 0;
+        for(String line : testCaseResults) {
             nLine++;
-            String line = scanner.nextLine().trim();
-            String [] values = line.split("\\s+");
+            String [] values = line.trim().split("\\s+");
             if(values.length != 2) {
-                System.err.println("LegacyGrader: invalid input on line " + nLine);
-               result = 1;
+                System.err.println("LegacyGrader: invalid input test case " + nLine);
+               graderError = GRADER_ERROR_BAD_FORMAT;
                break;
             }
             // be generous and accept upper or lower case for judgment
@@ -150,8 +178,8 @@ public class LegacyGrader {
             try {
                 score = Double.parseDouble(values[1]);
             } catch(NumberFormatException e) {
-                System.err.println("LegacyGrader: invalid score value on line " + nLine);
-                result = 2;
+                System.err.println("LegacyGrader: invalid score for test case " + nLine);
+                graderError = GRADER_ERROR_BAD_SCORE;
                 break;
             }
             found = false;
@@ -184,22 +212,22 @@ public class LegacyGrader {
                 }
             }
             if(!found) {
-                System.err.println("LegacyGrader: Unknown judgment code " + code + " at line " + nLine);
-                result = 3;
+                System.err.println("LegacyGrader: Unknown judgment code " + code + " for test case " + nLine);
+                graderError = GRADER_ERROR_BAD_JUDGMENT;
                 break;
             }
-            if(result != 0) {
+            if(graderError != 0) {
                 break;
             }
         }
         // Only calculate judgment if there were no errors
-        if(result == 0) {
+        if(graderError == 0) {
             // Check for no failures or always accept mode or optional flag "any" accepted case
             if(!anyFailures || verdictMode == VerdictMode.always_accept || (acceptIfAnyAccepted && scoreCount > 0)) {
                 if(scoreCount == 0) {
                     // this there were no judgments in the input
                     System.err.println("LegacyGrader: No judgments in the input.");
-                    result = 4;
+                    graderError = GRADER_ERROR_NO_TEST_CASES;
                 } else {
                     double score = 0;
                     // In this case, we return a score
@@ -217,12 +245,12 @@ public class LegacyGrader {
                             score = scoreMax;
                             break;
                     }
-                    System.out.println(JudgmentCodes.AC.toString() + " " + score);
+                    graderResult = JudgmentCodes.AC.toString() + " " + score;
                 }
             } else {
                 // determine non-accepted judgment
                 // All cases should either print the correct output to stdout, or print
-                // an error to stderr and set result to a non-zero value.
+                // an error to stderr and set graderError to a non-zero value.
                 switch(verdictMode) {
                     case worst_error:
                         found = false;
@@ -237,15 +265,15 @@ public class LegacyGrader {
                         if(!found) {
                             // Uhm.  This is extremely bad, Tammy, since we KNOW anyFailures must be true
                             System.err.println("LegacyGrader: FATAL error - can not find judgment code for worst_error mode.");
-                            result = 5;
+                            graderError = GRADER_ERROR_BAD_WORST_CODE;
                         }
                         break;
 
                     case first_error:
                         if(firstError == null) {
                             // Uhm.  This is extremely bad took Tammy, since we know anyFailures must be true
-                            System.err.println("LegacyGrader: FATAL error - can not find judgment code for worst_error mode.");
-                            result = 6;
+                            System.err.println("LegacyGrader: FATAL error - can not find judgment code for first_error mode.");
+                            graderError = GRADER_ERROR_BAD_FIRST_CODE;
                         } else {
                             System.out.println(firstError + " 0");
                         }
@@ -254,12 +282,24 @@ public class LegacyGrader {
                     default:
                         // Very bad, Tammy.  This can't happen because alwaysAccept was handled above
                         System.err.println("LegacyGrader: FATAL error - invalid verdict mode " + verdictMode.toString());
-                        result = 7;
+                        graderError = GRADER_ERROR_BAD_VERDICT_MODE;
                         break;
                 }
             }
         }
-        return result;
+        // this will be null in the case of an error, in which case graderError will have the error code
+        // in the case of success, this will be the "judgment_acronym <space> score", eg. "AC 50"
+        return graderResult;
+    }
+
+    /**
+     * Returns the last error the grader saw, or 0 if no errors.
+     * This may be useful if the getTestCases() method returns null.
+     *
+     * @return the last grader error
+     */
+    int getGraderError() {
+        return graderError;
     }
 
     /**
