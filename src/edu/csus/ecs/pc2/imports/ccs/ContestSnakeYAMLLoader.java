ed1b5fb535095e2f20b74f6ab9639263e549b219
John Buck <johnbrvc@gmail.com>
2025-08-01 07:40:11 -0400

    i_1006 Junit found error in new code
    Do not set scoreboard if it's not specify in the YAML config file.  The default will be pass-fail if it's not specified.  It was being set to "null" which was causing an NPE in the JUnit.

--- src/edu/csus/ecs/pc2/imports/ccs/ContestSnakeYAMLLoader.java
+++ src/edu/csus/ecs/pc2/imports/ccs/ContestSnakeYAMLLoader.java
@@ -628,15 +628,17 @@ public class ContestSnakeYAMLLoader implements IContestLoader {
 
         // If the contest type is present in contest.yaml, verify it
         String scoreType = ContestImportUtilities.fetchValue(content, CLICS_CONTEST_SCOREBOARD_TYPE);
-        if(scoreType != null && !scoreType.equals(CLICS_CONTEST_SCOREBOARD_TYPE_PASSFAIL)
+        if(scoreType != null) {
+            if(!scoreType.equals(CLICS_CONTEST_SCOREBOARD_TYPE_PASSFAIL)
                 && !scoreType.equals(CLICS_CONTEST_SCOREBOARD_TYPE_SCORE)) {
-            throw new YamlLoadException("Invalid " + CLICS_CONTEST_SCOREBOARD_TYPE + ": "
-                + scoreType + ", expected "
-                + CLICS_CONTEST_SCOREBOARD_TYPE_PASSFAIL
-                + " or "
-                + CLICS_CONTEST_SCOREBOARD_TYPE_SCORE);
+                throw new YamlLoadException("Invalid " + CLICS_CONTEST_SCOREBOARD_TYPE + ": "
+                    + scoreType + ", expected "
+                    + CLICS_CONTEST_SCOREBOARD_TYPE_PASSFAIL
+                    + " or "
+                    + CLICS_CONTEST_SCOREBOARD_TYPE_SCORE);
+            }
+            setContestScoreboardType(contest, scoreType);
         }
-        setContestScoreboardType(contest, scoreType);
 
         Object privatehtmlOutputDirectory = ContestImportUtilities.fetchObjectValue(content, OUTPUT_PRIVATE_SCORE_DIR_KEY);
         if (privatehtmlOutputDirectory != null) {
