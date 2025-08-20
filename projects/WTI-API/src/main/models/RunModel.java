2dc4bf021690effaa17b2bd072cd8245c27a0d25
John Clevenger <clevenger@csus.edu>
2025-08-19 21:34:53 -0700

    i1154: update RunModel constructor to accept and save isSolved.

--- projects/WTI-API/src/main/models/RunModel.java
+++ projects/WTI-API/src/main/models/RunModel.java
@@ -7,17 +7,18 @@ public class RunModel {
 	public String teamID, language, problem, judgement;
 	public List<File> result;
 	public long time;
-	public boolean isTestRun, isPreliminary, isFinal;
+	public boolean isTestRun, isPreliminary, isFinal, isSolved;
 	public String id;
 
 	public RunModel(String teamID, String language, String problem, String judgement, List<File> result, long time, 
-			boolean isTestRun, boolean isPreliminary, boolean isFinal, String id) {
+			boolean isTestRun, boolean isPreliminary, boolean isFinal, boolean isSolved, String id) {
 		this.teamID = teamID;
 		this.language = language;
 		this.problem = problem;
 		this.isTestRun = isTestRun;
 		this.isPreliminary = isPreliminary;
 		this.isFinal = isFinal;
+		this.isSolved = isSolved;
 		this.time = time;
 		this.judgement = judgement;
 		this.result = result;
