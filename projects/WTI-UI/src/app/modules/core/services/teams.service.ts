8d71220cc0063c63a9cd66f33111f59db71fb3f2
John Clevenger <clevenger@csus.edu>
2025-12-26 16:14:02 -0800

    i1083: make browser console debug/trace output conditional on DEBUG_MODE

--- projects/WTI-UI/src/app/modules/core/services/teams.service.ts
+++ projects/WTI-UI/src/app/modules/core/services/teams.service.ts
@@ -14,45 +14,51 @@ import { DEBUG_MODE } from 'src/constants';
 	providedIn: 'root'   //forces the service to be a singleton across all app components ('root' == "root injector")
 })
 export class TeamsService extends ITeamsService {
-	
-  constructor(private _httpClient: HttpClient) {
-    super();
-    if (DEBUG_MODE) {
-    	console.log ("Executing TeamsService constructor...") ;
-		  const environmentCopy = JSON.parse(JSON.stringify(environment));
-		  console.log('...Environment Object (Deep Copy):', environmentCopy);
-    }
-  }
 
-  login(loginCredentials: LoginCredentials): Observable<TeamsLoginResponse> {
-	console.log ("Executing TeamsService.login()...") ;
-	console.log("...environment is:");
-		  let environmentCopy = JSON.parse(JSON.stringify(environment));
-		  console.log(environmentCopy);
+	constructor(private _httpClient: HttpClient) {
+		super();
+		if (DEBUG_MODE) {
+			console.log("Executing TeamsService constructor...");
+			const environmentCopy = JSON.parse(JSON.stringify(environment));
+			console.log('...Environment Object (Deep Copy):', environmentCopy);
+		}
+	}
 
-	console.log(`Invoking HttpClient.post() with URL ${environment.baseUrl}/teams/login`);
-	let resp = this._httpClient.post<TeamsLoginResponse>(`${environment.baseUrl}/teams/login`, loginCredentials);
-	console.log("Response from HttClient:");
-	console.log(resp);
-	console.log("Environment following HTTPClient response is:");
-		  environmentCopy = JSON.parse(JSON.stringify(environment));
-		  console.log(environmentCopy);
-	return resp;
-  }
+	login(loginCredentials: LoginCredentials): Observable<TeamsLoginResponse> {
+		if (DEBUG_MODE) {
+			console.log("Executing TeamsService.login()...");
+			console.log("...environment is:");
+			const environmentCopy = JSON.parse(JSON.stringify(environment));
+			console.log(environmentCopy);
+		}
 
-  logout(): Observable<any> {
-    return this._httpClient.delete<any>(`${environment.baseUrl}/teams/logout`);
-  }
+		if (DEBUG_MODE) {
+			console.log(`Invoking HttpClient.post() with URL ${environment.baseUrl}/teams/login`);
+		}
+		let resp = this._httpClient.post<TeamsLoginResponse>(`${environment.baseUrl}/teams/login`, loginCredentials);
+		if (DEBUG_MODE) {
+			console.log("Response from HttClient:");
+			console.log(resp);
+			console.log("Environment following HTTPClient response is:");
+			const environmentCopy = JSON.parse(JSON.stringify(environment));
+			console.log(environmentCopy);
+		}
+		return resp;
+	}
 
-  submitRun(submission: Submission): Observable<any> {
-    return this._httpClient.post<Submission>(`${environment.baseUrl}/teams/run`, submission);
-  }
+	logout(): Observable<any> {
+		return this._httpClient.delete<any>(`${environment.baseUrl}/teams/logout`);
+	}
 
-  getRuns(): Observable<Run[]> {
-    return this._httpClient.get<Run[]>(`${environment.baseUrl}/teams/run`);
-  }
+	submitRun(submission: Submission): Observable<any> {
+		return this._httpClient.post<Submission>(`${environment.baseUrl}/teams/run`, submission);
+	}
 
-  postClarification(clarification: NewClarification): Observable<any> {
-    return this._httpClient.post<NewClarification>(`${environment.baseUrl}/teams/clarification`, clarification);
-  }
+	getRuns(): Observable<Run[]> {
+		return this._httpClient.get<Run[]>(`${environment.baseUrl}/teams/run`);
+	}
+
+	postClarification(clarification: NewClarification): Observable<any> {
+		return this._httpClient.post<NewClarification>(`${environment.baseUrl}/teams/clarification`, clarification);
+	}
 }
